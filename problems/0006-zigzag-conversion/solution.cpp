26effbde488606b45d23a80133f0caa70ac6e1a2
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2022-06-02 04:46:37 +0000

    [LeetCode Sync] Runtime - 10 ms (24.34%), Memory - 14.7 MB (17.33%)

--- problems/0006-zigzag-conversion/solution.cpp
+++ problems/0006-zigzag-conversion/solution.cpp
@@ -3,25 +3,24 @@ public:
     string convert(string s, int numRows) {
         if (numRows == 1) { return s; }
         
-        
         vector<string> rows;
-        string solution;
         for (int i = 0; i < numRows; i++) {
             rows.push_back("");
         }
         
-        int curChar = 0;
+        int i = 0;
         int row = 0;
-        int increment = 1;
-        
-        while (curChar < s.size()) {
-            rows[row] += s[curChar];
-            curChar++;
-            if (row == numRows - 1) { increment = -1; }
-            if (row == 0) { increment = 1; }
-            row += increment;
+        int rowIncr = 1;
+        while(i < s.size()) {
+            rows[row] += s[i];
+            
+            row += rowIncr;
+            i++;
+            if (row == numRows - 1) { rowIncr = -1;}
+            if (row == 0) { rowIncr = 1;}
         }
         
+        string solution = "";
         for (int i = 0; i < numRows; i++) {
             solution += rows[i];
         }

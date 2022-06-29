1c252c7610a85a08ec25edf873a4d047a964be7c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2022-06-29 16:51:48 +0000

    [LeetCode Sync] Runtime - 60 ms (7.91%), Memory - 13.9 MB (100.00%)

--- problems/0837-most-common-word/solution.py
+++ problems/0837-most-common-word/solution.py
@@ -1,22 +1,23 @@
-import re
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        banned.append(" ")
+        paragraph = paragraph.lower()
+
+        words: List[str] = re.split('[ .,;!?\']', paragraph)
+
         banned.append("")
-        words: List[str] = re.sub("[,.!?;'\"]", " ", paragraph).lower().split(" ")
-        count: Dict[int] = {}
+        banned.append(" ")
         
+        maxStr: str = ""
+        maxCount: int = 0
+        count: Dict[str, int] = {}
         for word in words:
             if word not in banned:
                 if word not in count:
                     count[word] = 1
                 else:
                     count[word] = count[word] + 1
-        # print(count)         
-        max: tuple[str, int] = ("", 0)
-        for i, (k, v) in enumerate(count.items()):
-            if v > max[1]:
-                max = (k, v)
-        return max[0]
-        
-                
+                if count[word] > maxCount:
+                        maxCount = count[word]
+                        maxStr = word
+        print(count)
+        return maxStr

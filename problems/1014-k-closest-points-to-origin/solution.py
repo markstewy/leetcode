fd3ce970485d608104b9621a35457bfc8312da8c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2022-07-13 03:28:23 +0000

    [LeetCode Sync] Runtime - 858 ms (5.00%), Memory - 20.3 MB (100.00%)

--- problems/1014-k-closest-points-to-origin/solution.py
+++ problems/1014-k-closest-points-to-origin/solution.py
@@ -1,7 +1,5 @@
 class Solution:
     
-
-    
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         def get_key(point: List[int]):
             return sqrt(pow(point[0], 2) + pow(point[1], 2))

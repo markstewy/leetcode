e3ef0ae065ab186e025fb8da5c30c920b13ec4e2
Mark Stewart <markstewy@gmail.com>
2022-07-12 20:43:54 -0700

    Time: 0 ms (100.00%), Space: 8.9 MB (35.10%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                nums.erase(nums.begin() + i);
+                i--;
+            } else {
+                k++;
+            }
+        }
+        return k;
+    }
+};

deaac8a40fbe025837824c157ae69a09c7ac8f16
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-08 09:59:20 +0000

    [LeetCode Sync] Runtime - 95 ms (12.18%), Memory - 51.2 MB (100.00%)

---
+++ problems/0217-contains-duplicate/solution.cpp
@@ -0,0 +1,18 @@
+#include <unordered_map>
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        std::unordered_map<int, int> m;
+
+        for (int n : nums) {
+            if (m[n] == 1) {
+                return true;
+            } else {
+                m[n] = 1;
+            }
+        }
+        return false;
+    }
+};

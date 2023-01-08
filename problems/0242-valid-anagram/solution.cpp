d05bc8603b5ae73cce6c9135d1fe070711d98c68
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-08 10:00:02 +0000

    [LeetCode Sync] Runtime - 16 ms (6.84%), Memory - 7.4 MB (100.00%)

---
+++ problems/0242-valid-anagram/solution.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        std::sort(s.begin(), s.end(), [](char a, char b) { return a < b; });
+        std::sort(t.begin(), t.end(), [](char a, char b) { return a < b; });
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != t[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};

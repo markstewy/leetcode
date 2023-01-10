91becedb8f555c394d56d32e16fdfa8f01be2ac7
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-10 21:33:30 +0000

    [LeetCode Sync] Runtime - 24 ms (37.13%), Memory - 15.1 MB (12.23%)

--- problems/0001-two-sum/solution.cpp
+++ problems/0001-two-sum/solution.cpp
@@ -1,35 +1,37 @@
 #include <unordered_map>
+#include <algorithm>
+
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> solution;
-        // map all values <value: index>
-        unordered_map<int, vector<int>> m;
-        for (int i = 0; i < nums.size(); i++) {
-            if (m.find(nums[i]) == m.end()) {
-                pair<int, vector<int>>p;
-                vector<int> temp{i};
-                p.first = nums[i];
-                p.second = temp;
-                m.insert(p);
-            } else {
-                m[nums[i]].push_back(i);
-            }
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            int diff = target - nums[i];
-            if (m.find(diff) != m.end()) {
-                for (int idx : m[diff]) {
-                    if (idx != i) {
-                        solution.push_back(i);
-                        solution.push_back(idx);
-                        return solution;
-                    }
-                }
-            }
-        }
-        return solution;
-    }
 
+    
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, vector<int>> m;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int n = nums[i];
+                
+            if (m.find(n) != m.end()) {
+                m[n].push_back(i);
+            } else {
+                m[n] = std::vector<int>{i};
+            }
+        }
+        
+        for (int n : nums) {
+            int diff = target - n;
+            
+            if (n == diff) {
+                if (m[n].size() > 1) {
+                    return std::vector<int>{m[n][0], m[n][1]};
+                }
+            } else {
+                if (m[diff].size() > 0) {
+                    return std::vector<int>{m[n][0], m[diff][0]};
+                } 
+            }
+        }
+        return std::vector<int>{-1, -1};
+    }
 };

892c7fe680115aba59142f2f65b53c46c86735d1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-11 06:10:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.9 MB (100.00%)

--- problems/0027-remove-element/solution.cpp
+++ problems/0027-remove-element/solution.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    int removeElement(vector<int>& nums, int val) {
-        int k = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == val) {
-                nums.erase(nums.begin() + i);
-                i--;
-            } else {
-                k++;
-            }
+    void clearBack(vector<int>& nums, int val) {
+        while(nums.size() > 0 && nums.back() == val) {
+            nums.pop_back();
         }
-        return k;
+    }
+    
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        if (nums.size() == 1 && nums[0] == val) {
+            nums.pop_back();
+            return 0;
+        }
+        
+        
+        clearBack(nums, val);
+        
+        int i = 0;
+        while (i < nums.size() && nums.size() > 1) {
+            if(nums[i] == val) {
+                nums[i] = nums.back();
+                nums.pop_back();
+                clearBack(nums, val);   
+            }
+            i++;
+        }
+        return nums.size();
     }
 };

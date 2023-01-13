674fae8005dae718b6f8f202c7953309f2a617b6
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-13 07:44:38 +0000

    [LeetCode Sync] Runtime - 21 ms (8.01%), Memory - 19.6 MB (99.98%)

---
+++ problems/0169-majority-element/solution.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        return nums[nums.size() / 2];
+    }
+};

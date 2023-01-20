228814966a086b85ab24f4ec9fa116bed9f7cfe6
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-20 10:28:41 +0000

    [LeetCode Sync] Runtime - 9 ms (6.41%), Memory - 7.5 MB (100.00%)

--- problems/0205-isomorphic-strings/solution.cpp
+++ problems/0205-isomorphic-strings/solution.cpp
@@ -2,23 +2,21 @@
 
 class Solution {
 public:
-    
-    bool checkOneWayTranslation (string a, string b) {
-        unordered_map<char, char> m;
-        
-        // a dictionary for translating a to b
-        for (int i = 0; i < a.size(); i++) {
-            m[a[i]] = b[i];
-        }
-        
-        string a_to_b_translation;
-        for (char c : a) {
-            a_to_b_translation.push_back(m[c]);
-        }
-        return a_to_b_translation == b;
-    }
-    
     bool isIsomorphic(string s, string t) {
-        return checkOneWayTranslation(s, t) && checkOneWayTranslation(t, s);
+        if (s.size() != t.size()) return false;
+        
+        unordered_map<char, char> tm;
+        unordered_map<char, char> sm;
+        for (int i = 0; i < s.size(); i++) {
+            tm[s[i]] = t[i];
+            sm[t[i]] = s[i];
+        }
+        
+        string s_to_t, t_to_s;
+        for (int i = 0; i < s.size(); i++) {
+            s_to_t += tm[s[i]];
+            t_to_s += sm[t[i]];
+        }
+        return s_to_t == t && t_to_s == s;
     }
 };

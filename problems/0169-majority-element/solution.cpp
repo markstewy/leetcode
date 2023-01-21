63e4e5e90dcbdc95271dfcaaeac52e173eb56f14
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-01-21 06:36:36 +0000

    [LeetCode Sync] Runtime - 36 ms (7.97%), Memory - 19.6 MB (99.98%)

--- problems/0169-majority-element/solution.cpp
+++ problems/0169-majority-element/solution.cpp
@@ -1,9 +1,24 @@
-#include <algorithm>
-
+#include <unordered_map>
+// solution in 5 minutes 
+// O(n) worst case compated to 
+// sorting alternative O(n logn)
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        return nums[nums.size() / 2];
+        unordered_map<int, int> m;
+        int majority = (nums.size() / 2) + 1;
+        
+        for (int n : nums) {
+            if (m.find(n) == m.end()) {
+                m[n] = 1;
+            } else {
+                m[n] = m[n] + 1;
+            }
+            
+            if (m[n] == majority) {
+                return n;
+            }
+        }
+        return -1;
     }
 };

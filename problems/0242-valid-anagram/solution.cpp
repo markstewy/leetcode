4fd309df6fb9901791c779b5f89690f3c89d758a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-02-03 05:18:30 +0000

    [LeetCode Sync] Runtime - 16 ms (6.84%), Memory - 7.4 MB (100.00%)

--- problems/0242-valid-anagram/solution.cpp
+++ problems/0242-valid-anagram/solution.cpp
@@ -2,6 +2,7 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
         sort(t.begin(), t.end());
         sort(s.begin(), s.end()); 
         return t == s;

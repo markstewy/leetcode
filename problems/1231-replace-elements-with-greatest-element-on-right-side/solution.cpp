ea522597caeb3fdef5a18d09f6266b8c14a31b40
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-02-03 05:41:44 +0000

    [LeetCode Sync] Runtime - 119 ms (21.62%), Memory - 68.4 MB (100.00%)

--- problems/1231-replace-elements-with-greatest-element-on-right-side/solution.cpp
+++ problems/1231-replace-elements-with-greatest-element-on-right-side/solution.cpp
@@ -1,15 +1,15 @@
-// solution in 8 minutes
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         int last = arr.size() - 1;
         int max = arr[last];
         arr[last] = -1;
+        last--;
         
-        for (int i = last - 1; i >= 0; i--) {
+        for (int i = last; i >= 0; i--) {
             int temp = arr[i];
             arr[i] = max;
-            max = temp > max ? temp : max;
+            max = max > temp ? max : temp;
         }
         return arr;
     }

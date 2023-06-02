10b572df655b4aac29317e16c7532de61b263e11
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-06-02 03:08:15 +0000

    [LeetCode Sync] Runtime - 4 ms (52.08%), Memory - 10.7 MB (100.00%)

--- problems/0001-two-sum/solution.cpp
+++ problems/0001-two-sum/solution.cpp
@@ -1,28 +1,23 @@
-#include <unordered_map>
-// solution in 22 minutes
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, vector<int>> m; // <val, idxs>
-        for (int i = 0; i < nums.size(); i++) {
-           if (m.find(nums[i]) == m.end()) {
-               m[nums[i]] = vector<int>{i};
-           } else {
-               m[nums[i]].push_back(i);
-           }
-        }
         
-        for (int i = 0; i < nums.size(); i++) {
-            int diff = target - nums[i];
-            
-            if (m.find(diff) != m.end()) {
-                // can't add to itself unless 2 instances exist
-                if (diff == nums[i] && m[diff].size() < 2) {
-                    continue;
-                }
-                return {i, m[diff].back()};
+        // key = num and value = index
+        unordered_map<int, int> cache;
+
+        int i = 0;
+        for (int n : nums) {
+            int diff = target - n;
+
+            if (cache.find(diff) != cache.end()) {
+                return {i, cache[diff]};
             }
+
+            cache[n] = i;
+
+            i++;
         }
+        // no answer found
         return {-1, -1};
     }
 };

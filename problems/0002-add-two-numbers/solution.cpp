e07f51cb28e3c093474ef3610c573b5a9013d7b4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-06-07 05:11:55 +0000

    [LeetCode Sync] Runtime - 32 ms (6.96%), Memory - 71.4 MB (99.95%)

--- problems/0002-add-two-numbers/solution.cpp
+++ problems/0002-add-two-numbers/solution.cpp
@@ -11,36 +11,32 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum = l1->val + l2->val;
+        ListNode* head_ptr = new ListNode();
+        ListNode* tail_ptr = head_ptr;
         int carry = 0;
-        
-        if ( sum > 9) {
-            carry = 1;
-            sum %= 10;
-        }
-        ListNode* solution = new ListNode(sum);
-        ListNode* solutionHead = solution;
-        l1 = l1->next;
-        l2 = l2->next;
-        
-        while (l1 || l2) {
-                int v1 = l1 ? l1->val : 0;
-                int v2 = l2 ? l2->val : 0;
-                int sum = v1 + v2 + carry;
-                if ( sum > 9) {
-                    carry = 1;
-                    sum %= 10;
-                 } else {
-                    carry = 0;
-                }
-                solution->next = new ListNode(sum);
-                l1 = l1 ? l1->next : nullptr;
-                l2 = l2 ? l2->next : nullptr;
-                solution = solution->next;
-        }
-        if (carry) {
-            solution->next = new ListNode(carry);
-        }
-        return solutionHead;
+
+        do {
+
+            int l1_val = l1 == nullptr ? 0 : l1->val;
+            int l2_val = l2 == nullptr ? 0 : l2->val;
+
+            int sum = l1_val + l2_val + carry;
+            tail_ptr->val = sum % 10;
+            carry = sum > 9 ? 1 : 0;
+
+            bool l1_has_next = (l1 != nullptr && l1->next != nullptr);
+            bool l2_has_next = (l2 != nullptr && l2->next != nullptr);
+
+            if (l1_has_next || l2_has_next || carry > 0) {
+                tail_ptr->next = new ListNode();
+                tail_ptr = tail_ptr->next;
+
+                l1 = l1 == nullptr ? nullptr : l1->next;
+                l2 = l2 == nullptr ? nullptr : l2->next;
+            } else {
+                return head_ptr;
+            }
+
+        } while (true);
     }
 };

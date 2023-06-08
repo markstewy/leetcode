f6742bf9b86e2f61f7bc1199e78923d8ae3ac277
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-06-08 04:14:12 +0000

    [LeetCode Sync] Runtime - 12 ms (37.61%), Memory - 10.8 MB (100.00%)

--- problems/0001-two-sum/solution.cpp
+++ problems/0001-two-sum/solution.cpp
@@ -1,23 +1,23 @@
+#include<unordered_map>
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        // key = num and value = index
-        unordered_map<int, int> cache;
+        unordered_map<int,int> cache; // <num, index>
 
         int i = 0;
         for (int n : nums) {
+
             int diff = target - n;
 
             if (cache.find(diff) != cache.end()) {
                 return {i, cache[diff]};
+            } else {
+                cache[n] = i;
             }
-
-            cache[n] = i;
-
             i++;
         }
-        // no answer found
+
         return {-1, -1};
     }
 };

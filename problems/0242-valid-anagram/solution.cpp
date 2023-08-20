6003fa8534d7cb28270d515f7925af3bcc0ec446
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-08-20 20:36:58 +0000

    [LeetCode Sync] Runtime - 8 ms (6.84%), Memory - 7.6 MB (100.00%)

--- problems/0242-valid-anagram/solution.cpp
+++ problems/0242-valid-anagram/solution.cpp
@@ -1,9 +1,42 @@
+#include<unordered_map>
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        std::sort(s.begin(), s.end());
-        std::sort(t.begin(), t.end());
-        return s == t;
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        unordered_map<char, int> sMap;
+        unordered_map<char, int> tMap;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (sMap.find(s[i]) == sMap.end()) {
+                sMap.insert(std::make_pair(s[i], 1));
+            } else {
+                sMap[s[i]]++;
+            }
+
+            if (tMap.find(t[i]) == tMap.end()) {
+                tMap.insert(std::make_pair(t[i], 1));
+            } else {
+                tMap[t[i]]++;
+            }
+        }
+
+        for (auto& pair : sMap) {
+            char sKey = pair.first;
+            int sCount = pair.second;
+
+            bool both_have_key = (tMap.find(sKey) != tMap.end());
+            bool same_count = sCount == tMap[sKey];
+
+            if (!both_have_key || !same_count) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };

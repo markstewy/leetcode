df3457a4a1a0971c8f8e7bd2ec7080f2c25cdd56
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2023-08-20 21:02:38 +0000

    [LeetCode Sync] Runtime - 24 ms (25.25%), Memory - 19.7 MB (100.00%)

--- problems/0049-group-anagrams/solution.cpp
+++ problems/0049-group-anagrams/solution.cpp
@@ -1,28 +1,24 @@
-#include<unordered_map>
-
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> sol;
-        unordered_map<string, vector<string>> cache; // <aplph_name key, vector of original strings>
-
+        // create an unordered map to store arrays of strings by sorted key
+        unordered_map<string, vector<string>>m;
         for (string s : strs) {
-            string temp = s;
-            std::sort(s.begin(), s.end()); 
+            string key = s;
+            std::sort(key.begin(), key.end());
             
-            if (cache.find(s) != cache.end()) {
-                cache[s].push_back(temp);
-            } else {
-                cache[s] = {temp};
+            if (m.find(key) == m.end()) {
+                m[key] = {};
             }
+            m[key].push_back(s);
         }
 
-        for (auto p : cache) {
-            sol.push_back(p.second);
+// loop over the map and convert it into an array
+        vector<vector<string>> solution;
+        for (auto& pair : m) {
+            solution.push_back(pair.second);
         }
 
-        return sol;
-
-
+        return solution;
     }
 };

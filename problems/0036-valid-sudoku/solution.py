9d8591bafcdbb38d0328ebece4c4e52e240308cd
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-18 23:47:45 +0000

    [LeetCode Sync] Runtime - 87 ms (5.83%), Memory - 16.5 MB (100.00%)

---
+++ problems/0036-valid-sudoku/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        sqs = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    continue
+
+                if (
+                    val in rows[r]
+                    or val in cols[c]
+                    or val in sqs[(r//3, c//3)]
+                ):
+                    return False
+            
+                rows[r].add(val)
+                cols[c].add(val)
+                sqs[(r//3, c//3)].add(val)
+        
+        return True

fae729c2160de5fcb656926dc7a334c11a05f1d1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-19 20:46:11 +0000

    [LeetCode Sync] Runtime - 59 ms (35.60%), Memory - 17.7 MB (100.00%)

--- problems/0001-two-sum/solution.py
+++ problems/0001-two-sum/solution.py
@@ -1,23 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        solution: List[int] = []
-        # create a map of all indexes by their value
-        dict:Dict[int, List[int]] = {};
+        numsMap = {}
         
-        for i, val in enumerate(nums):
-            if val not in dict:
-                dict[val] = [i]
-            else:  
-                dict[val].append(i);      
-        # loop over each num and see if value exists in map
-        for i, val in enumerate(nums):
-            diff: int = target - val;
-            if diff in dict:
-                for idx in dict[diff]:
-                    # if exists make sure it's not the same as the current index
-                    if idx != i:
-                        solution.append(i)
-                        solution.append(idx)
-                        return solution
-        return solution              
-        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in numsMap:
+                return [i, numsMap[diff]]
+            
+            numsMap[n] = i

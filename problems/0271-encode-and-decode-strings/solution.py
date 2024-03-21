88a008b9c29145a95a2f61c9d3cee4b6893a7619
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-21 16:53:58 +0000

    [LeetCode Sync] Runtime - 73 ms (54.81%), Memory - 17 MB (100.00%)

---
+++ problems/0271-encode-and-decode-strings/solution.py
@@ -0,0 +1,29 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        i = 0
+        decoded = []
+
+        while i < (len(s) - 1):
+            l = ""
+            while s[i] != "#":
+                l += s[i]
+                i += 1
+            i += 1
+            end = i + int(l)
+            decoded.append(s[i : end])
+            i = end
+        return decoded
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))

0f1c9d3eaefcb299ffc163f3a11270c7cbfd29f4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-21 16:47:33 +0000

    [LeetCode Sync] Runtime - 87 ms (5.83%), Memory - 16.5 MB (100.00%)

--- problems/0036-valid-sudoku/solution.py
+++ problems/0036-valid-sudoku/solution.py
@@ -1,24 +1,25 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
         sqs = collections.defaultdict(set)
 
         for r in range(9):
             for c in range(9):
-                val = board[r][c]
-                if val == ".":
+                v = board[r][c]
+                if v == ".":
                     continue
 
                 if (
-                    val in rows[r]
-                    or val in cols[c]
-                    or val in sqs[(r//3, c//3)]
+                    v in rows[r]
+                    or v in cols[c]
+                    or v in sqs[(r // 3, c // 3)]
                 ):
                     return False
-            
-                rows[r].add(val)
-                cols[c].add(val)
-                sqs[(r//3, c//3)].add(val)
-        
+
+                cols[c].add(v)
+                rows[r].add(v)
+                sqs[(r//3, c//3)].add(v)
         return True
+
+    

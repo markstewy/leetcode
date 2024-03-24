b0b0f346f4e6e32a494959b06ec0f47648b2a510
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-24 20:03:40 +0000

    [LeetCode Sync] Runtime - 71 ms (6.02%), Memory - 16.8 MB (100.00%)

--- problems/0242-valid-anagram/solution.py
+++ problems/0242-valid-anagram/solution.py
@@ -2,10 +2,12 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-
-        sMap, tMap = {}, {}
+            
+        sMap, tMap = {}, {} # n -> count
 
         for i in range(len(s)):
+            print(i)
             sMap[s[i]] = sMap.get(s[i], 0) + 1
             tMap[t[i]] = tMap.get(t[i], 0) + 1
+        
         return sMap == tMap

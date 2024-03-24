0c0bc9f0202b9fdae2b432fd145de008153e7c2f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-24 19:47:43 +0000

    [LeetCode Sync] Runtime - 407 ms (5.23%), Memory - 32.1 MB (87.80%)

--- problems/0217-contains-duplicate/solution.py
+++ problems/0217-contains-duplicate/solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         cache = set()
-
+        
         for n in nums:
             if n in cache:
                 return True
             cache.add(n)
+
         return False
+        

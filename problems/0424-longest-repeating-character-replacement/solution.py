604c7297a9d942052b48ee0cf480bdabdec44a31
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-03-28 16:40:02 +0000

    [LeetCode Sync] Runtime - 126 ms (33.70%), Memory - 16.6 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,21 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # "AABABBA"
-        #   l    r
-        # valid sub if l - most common char <= k
+        # len(window) - most common char count <=k: return len(window)
+        winCount = {}
+        l, r = 0, 0
+        ml = 0
 
-        subStrCharCount = {} # track the most common char
-        longestSub = 0
-        l = 0
+        while r < len(s):
+            winCount[s[r]] = winCount.get(s[r], 0) + 1
 
-        for r in range(len(s)):
-            #add to counter
-            subStrCharCount[s[r]] = subStrCharCount.get(s[r], 0) + 1
-            #check if string is valid, if not move left ptr
-            while r - l + 1 - max(subStrCharCount.values()) > k:
-                subStrCharCount[s[l]] -= 1
+            # while substr is invalid
+            while (r - l) + 1 - max(winCount.values()) > k:
+                winCount[s[l]] -= 1
                 l += 1
-            #update longestSub
-            subLength = r - l + 1
-            longestSub = max(longestSub, subLength)
-        return longestSub
+            
+            ml = max(ml, r - l + 1)
+            r += 1
+        return ml
+
+

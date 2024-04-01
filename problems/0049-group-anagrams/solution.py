48a8bada240b8fbd4f045d9243cd153aeb4a84f9
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-01 21:28:50 +0000

    [LeetCode Sync] Runtime - 103 ms (5.08%), Memory - 22.9 MB (31.92%)

--- problems/0049-group-anagrams/solution.py
+++ problems/0049-group-anagrams/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # make a universal key
         ans = collections.defaultdict(list)
 
         for s in strs:
             s = s.lower()
-            uKey = [0] *26
-
+            # create a uinversal key from the string
+            uKey = [0] * 26
             for c in s:
-                i = ord(c) - ord("a")
-                uKey[i] += 1
+                uKey[ord(c) - ord("a")] += 1
+            
             ans[tuple(uKey)].append(s)
         return ans.values()
 

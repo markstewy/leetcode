ec4d5e5d3d0e1cd3571443ad8dab6631fc3595ff
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-01 21:02:57 +0000

    [LeetCode Sync] Runtime - 94 ms (5.83%), Memory - 16.7 MB (100.00%)

--- problems/0036-valid-sudoku/solution.py
+++ problems/0036-valid-sudoku/solution.py
@@ -1,15 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
-        cols = collections.defaultdict(set)
-        sqs = collections.defaultdict(set)
+        # track duplicates in row, col and squares with a set each
+
+        rows = collections.defaultdict(set) # key = row number
+        cols = collections.defaultdict(set) # key = col number
+        sqs = collections.defaultdict(set) # key = tuple of integer math ie. (row//3, col//3)  >>> rows 1-2 // 3 = 1, 3-5 // 3 = 1
 
         for r in range(9):
             for c in range(9):
                 v = board[r][c]
+
                 if v == ".":
                     continue
                 
+                # check if duplicate exists, if so fail <return False>
                 if (
                     v in rows[r]
                     or v in cols[c]
@@ -20,5 +24,6 @@ class Solution:
                 rows[r].add(v)
                 cols[c].add(v)
                 sqs[(r//3, c//3)].add(v)
+        
         return True
 

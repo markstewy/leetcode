5a8af85f915d76cffbcdcca5ce4f55ee78e9dd08
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-03 01:24:40 +0000

    [LeetCode Sync] Runtime - 585 ms (5.41%), Memory - 29.8 MB (14.31%)

--- problems/0011-container-with-most-water/solution.py
+++ problems/0011-container-with-most-water/solution.py
@@ -1,15 +1,20 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        mVol = 0
+        lMax = 0
+        rMax = 0
+
         l = 0
         r = len(height) - 1
-        maxVol = 0
-
         while l < r:
-            vol = min(height[l], height[r]) * (r - l)
-            maxVol = max(maxVol, vol)
+            lMax = max(lMax, height[l])
+            rMax = max(rMax, height[r])
+            vol = (r - l) * min(lMax, rMax)
+            mVol = max(mVol, vol)
 
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        return maxVol
+        
+        return mVol

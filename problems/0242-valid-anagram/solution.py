80117d41ab92d6051aee3491e8e1bcfddeae71f2
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-05 05:09:30 +0000

    [LeetCode Sync] Runtime - 48 ms (6.02%), Memory - 16.9 MB (100.00%)

--- problems/0242-valid-anagram/solution.py
+++ problems/0242-valid-anagram/solution.py
@@ -7,5 +7,5 @@ class Solution:
         for i in range(len(s)):
             sMap[s[i]] = sMap.get(s[i], 0) + 1
             tMap[t[i]] = tMap.get(t[i], 0) + 1
-
+        
         return sMap == tMap

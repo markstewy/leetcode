a2250128cf04ab5c0f5f5c646606647c283036e3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-11 03:06:40 +0000

    [LeetCode Sync] Runtime - 117 ms (35.12%), Memory - 17.3 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,36 +1,45 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        countT, window = {}, {}
+        if len(s) < len(t): 
+            return ""
 
-        for c in t:
-            countT[c] = countT.get(c, 0) + 1
+        tCount, window = {}, {}
 
-        have = 0
-        need = len(countT)
+        for i in range(len(t)):
+            tCount[t[i]] = tCount.get(t[i], 0) + 1
+        
+            # have is the number of chars completely included in the window (inluding any duplicates)
+            # need is the total number of unique chars in t
+            have = 0
+            need = len(tCount)
+            sub = [-1, -1]
+            subLen = float('infinity')
 
-        sub = [-1, -1]
-        subLen = float('infinity')
-
+        # sliding window, expand r until all chars
+        # move up l while all chars
         l = 0
         for r in range(len(s)):
-            c = s[r]
-            window[c] = window.get(c, 0) + 1
-
-            if c in countT and window[c] == countT[c]:
+            window[s[r]] = window.get((s[r]), 0) + 1
+            if s[r] in tCount and window[s[r]] == tCount[s[r]]:
                 have += 1
-            
+
             while have == need:
                 if r - l + 1 < subLen:
                     subLen = r - l + 1
                     sub = [l, r]
-
+                
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] == countT[s[l]] - 1:
+                if s[l] in tCount and window[s[l]] == tCount[s[l]] - 1:
                     have -= 1
                 l += 1
+                
+        left = sub[0]
+        right = sub[1]
 
-        l = sub[0]
-        r = sub[1] 
-        return s[l:r+1] if subLen < float('infinity') else ""
+        if subLen == float('infinity'):
+            return ""
+
+        return s[left : right + 1]
 
 
+        

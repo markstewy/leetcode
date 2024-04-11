517c3850642eb3ddc3bda95b75db4761682a6e4a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-11 03:45:40 +0000

    [LeetCode Sync] Runtime - 76 ms (82.86%), Memory - 16.7 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,23 +1,22 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # keep track of most common character, use a map and get max(map.items())
-        # two pointers l & r both start at index 0
-        if len(s) == 0: return 0
-
+        count = {} # char -> count
         longest = 0
-        count = {}
-
+        maxCharCount = 0
+        
         l = 0
-        for r, rChar in enumerate(s):
-            # add r to the count:
-            count[rChar] = count.get(rChar, 0) + 1
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r], 0) + 1
+            maxCharCount = max(maxCharCount, count[s[r]])
 
-            # while is not valid move up left
-            while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] = count[s[l]] - 1 # should never need to be initialized to zero
+            # while is NOT valid substring
+            while (r - l + 1) - maxCharCount > k:
+                count[s[l]] -= 1
                 l += 1
-            
-            # now that its valid, update longest
-            longest = max(longest, (r - l + 1))
-            
+
+            longest = max(longest, r - l + 1)
+        
         return longest
+            
+
+

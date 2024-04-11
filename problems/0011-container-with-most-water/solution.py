a938331c9ebad704a7edfb89a8802e209b2c1a04
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-11 04:28:09 +0000

    [LeetCode Sync] Runtime - 538 ms (5.41%), Memory - 29.7 MB (14.31%)

--- problems/0011-container-with-most-water/solution.py
+++ problems/0011-container-with-most-water/solution.py
@@ -2,17 +2,15 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         l = 0
         r = len(height) - 1
-
-        maxWater = 0
+        maxA = 0
 
         while l < r:
-            length = r - l
-            h = min (height[l], height[r])
-            maxWater = max(maxWater, length * h)
+            a = min(height[l], height[r]) * (r - l)
+            maxA = max(maxA, a)
 
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-            
-        return maxWater
+        return maxA
+

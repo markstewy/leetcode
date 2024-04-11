78336c6ff279e7c1d557269c35408bcf41032e61
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-11 03:23:25 +0000

    [LeetCode Sync] Runtime - 69 ms (21.38%), Memory - 16.7 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,41 +1,48 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2): return False
+        if len(s1) > len(s2):
+            return False
 
-        s1, s2 = s1.lower(), s2.lower()
-        s1Count, s2WinCount = [0] * 26, [0] * 26
+        # sliding window of fixed length
+        # the window should have exactly same number of each char, no more no less
+
+        s1Count = {}
+        window = {}
 
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] += 1
-            s2WinCount[ord(s2[i]) - ord("a")] += 1
+            s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
+            window[s2[i]] = window.get(s2[i], 0) + 1
         
+        # track if we have all chars satisfied with "matches"
+        # a match is when a char has no more, no less than the char count in s1Count
         matches = 0
-        for i in range(len(s1Count)):
-            if s1Count[i] == s2WinCount[i]:
+        need = len(s1Count)
+
+        # initialize matches
+        for c, count in window.items():
+            if c not in s1Count:
+                continue
+            if window[c] == s1Count[c]:
                 matches += 1
-        
+
         l = 0
         for r in range(len(s1), len(s2)):
-            if matches == 26:
+            if matches == need:
                 return True
-        
-            # remove l from count (adj matches)
-            letter_index = ord(s2[l]) - ord("a")
-            s2WinCount[letter_index] -= 1
-            if s2WinCount[letter_index] == s1Count[letter_index]:
+            
+            window[s2[r]] = window.get(s2[r], 0) + 1
+            if s2[r] in s1Count and window[s2[r]] == s1Count[s2[r]]:
                 matches += 1
-            if s2WinCount[letter_index] + 1 == s1Count[letter_index]:
+            if s2[r] in s1Count and window[s2[r]] == s1Count[s2[r]] + 1:
+                matches -= 1
+
+            window[s2[l]] -= 1
+            if s2[l] in s1Count and window[s2[l]] == s1Count[s2[l]]:
+                matches += 1
+            if s2[l] in s1Count and window[s2[l]] == s1Count[s2[l]] - 1:
                 matches -= 1
             l += 1
-
-            # add r to count (adj matches)
-            letter_index = ord(s2[r]) - ord("a")
-            s2WinCount[letter_index] += 1
-            if s2WinCount[letter_index] == s1Count[letter_index]:
-                matches += 1
-            if s2WinCount[letter_index] == s1Count[letter_index] + 1:
-                matches -= 1
-            
-        return matches == 26
+        
+        return matches == need
 
 

0bef872e40fc85aea41a356b8683787a8992b49d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-15 04:30:39 +0000

    [LeetCode Sync] Runtime - 122 ms (6.43%), Memory - 18.4 MB (100.00%)

--- problems/0042-trapping-rain-water/solution.py
+++ problems/0042-trapping-rain-water/solution.py
@@ -1,26 +1,26 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         ltr, rtl = [], []
-        totalWater = 0
 
         m = 0
         for n in height:
             m = max(m, n)
             ltr.append(m)
-
+        
         m = 0
         for i in range(len(height) - 1, -1, -1):
             m = max(m, height[i])
             rtl.append(m)
         rtl.reverse()
-    
-        # iterate over array excluding first and last positions (these can't hold water)
+
+        totalWater = 0
         i = 1
         while i < len(height) - 1:
-            waterLevel = min(ltr[i - 1], rtl[i + 1])
-            waterDepth = max(0, waterLevel - height[i])
-            totalWater += waterDepth
+            waterHeight = min(ltr[i - 1], rtl[i + 1])
+            depth = max(0, waterHeight - height[i])
+            totalWater += depth
             i += 1
+        
         return totalWater
 
 

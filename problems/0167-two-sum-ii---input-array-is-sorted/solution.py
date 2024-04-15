be0b15f35df89ac788760552e948193eb7392834
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-15 04:05:11 +0000

    [LeetCode Sync] Runtime - 105 ms (7.07%), Memory - 17.5 MB (100.00%)

--- problems/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ problems/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -5,9 +5,12 @@ class Solution:
 
         while l < r:
             sum = nums[l] + nums[r]
+
             if sum < target:
                 l += 1
             elif sum > target:
                 r -= 1
             else:
                 return [l + 1, r + 1]
+        
+        return []

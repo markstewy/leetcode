e29ea30185eaaea7f4337ea92336eb8d5ae07eb9
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-17 21:50:33 +0000

    [LeetCode Sync] Runtime - 110 ms (36.69%), Memory - 17.4 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,45 +1,45 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t): 
+        if len(s) < len(t):
             return ""
 
-        tCount, window = {}, {}
+        ans = [-1, -1]
+        shortest = float('infinity')
 
-        for i in range(len(t)):
-            tCount[t[i]] = tCount.get(t[i], 0) + 1
+        tCount = {}
+        for c in t:
+            tCount[c] = tCount.get(c, 0) + 1
         
-            # have is the number of chars completely included in the window (inluding any duplicates)
-            # need is the total number of unique chars in t
-            have = 0
-            need = len(tCount)
-            sub = [-1, -1]
-            subLen = float('infinity')
+        have = 0
+        need = len(tCount)
 
-        # sliding window, expand r until all chars
-        # move up l while all chars
+        window = {}
         l = 0
         for r in range(len(s)):
-            window[s[r]] = window.get((s[r]), 0) + 1
-            if s[r] in tCount and window[s[r]] == tCount[s[r]]:
+            c = s[r]
+            window[c] = window.get(c, 0) + 1
+            
+            if c in tCount and tCount[c] == window[c]:
                 have += 1
 
-            while have == need:
-                if r - l + 1 < subLen:
-                    subLen = r - l + 1
-                    sub = [l, r]
-                
+            while have >= need:
+                if (r - l + 1) < shortest:
+                    shortest = min(shortest, r - l + 1)
+                    ans = [l, r]
+
                 window[s[l]] -= 1
-                if s[l] in tCount and window[s[l]] == tCount[s[l]] - 1:
+
+                if s[l] in tCount and tCount[s[l]] - 1 == window[s[l]]:
                     have -= 1
+
                 l += 1
                 
-        left = sub[0]
-        right = sub[1]
-
-        if subLen == float('infinity'):
+        if shortest == float('infinity'):
             return ""
-
-        return s[left : right + 1]
-
-
         
+        l = ans[0]
+        r = ans[1]
+        return s[l : r + 1]
+        
+
+

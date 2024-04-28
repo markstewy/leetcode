a16e93aed5585d0e88a5c8c3232d2dd51f18955b
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-28 14:47:20 +0000

    [LeetCode Sync] Runtime - 89 ms (49.25%), Memory - 16.9 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,26 +1,36 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         longest = 0
-        count = {} # c -> count
-
-        maxInCount = 0
+        cache = {}
+        mChar = 0
+        mCount = 0
+        # max occurance - k <= 0 then the substring is valid and record length
 
         l = 0
         for r in range(len(s)):
-            count[s[r]] = count.get(s[r], 0) + 1
-            maxInCount = max(maxInCount, count[s[r]])
+            cache[s[r]] = cache.get(s[r], 0) + 1
+            if cache[s[r]] > mCount:
+                mCount = cache[s[r]]
+                mChar = s[r]
 
-            while (r - l + 1) - maxInCount > k:
-                count[s[l]] -= 1
+            # make substring valid: len - most common char <= number of changes we can make
+            while (r - l + 1) - mCount > k:
+                cache[s[l]] -= 1
                 l += 1
 
-                if count[s[l - 1]] + 1 == maxInCount:
-                    maxInCount = max(count.values())
+                # if the char we removed was the max count char, we need to 0(n) recalc the max
+                if s[l - 1] == mChar:
+                    mChar = 0
+                    mCount = 0
+                    for c , count in cache.items():
+                        if count > mCount:
+                            mCount = count
+                            mChar = c
+
             
-            longest = max(longest, (r - l + 1))
+            longest = max(longest, r - l + 1)
         
         return longest
-            
 
-        
+
 

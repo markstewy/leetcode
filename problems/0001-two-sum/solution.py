3e3c98d4e258a0f5bcba462ef7676a251ef0c81c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-30 21:54:08 +0000

    [LeetCode Sync] Runtime - 51 ms (35.60%), Memory - 17.8 MB (100.00%)

--- problems/0001-two-sum/solution.py
+++ problems/0001-two-sum/solution.py
@@ -2,10 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         cache = {}
 
-        for i, n in enumerate(nums):
-            diff = target - n
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            
             if diff in cache:
                 return [i, cache[diff]]
             
-            cache[n] = i
-
+            cache[nums[i]] = i

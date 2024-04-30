8e9f4bd71a13beaa87cb9032f86e9ac20937e46d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-04-30 22:05:46 +0000

    [LeetCode Sync] Runtime - 84 ms (5.30%), Memory - 22 MB (99.99%)

--- problems/0347-top-k-frequent-elements/solution.py
+++ problems/0347-top-k-frequent-elements/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {} # n -> count
+        count = {} # n -> c
         ans = []
 
         for n in nums:
             count[n] = count.get(n, 0) + 1
 
-        
-        order = [[] for _ in range(len(nums) + 1)]
+        sorted = [[] for _ in range(len(nums) + 1)]
 
         for n, c in count.items():
-            order[c].append(n)
+            sorted[c].append(n)
         
-
-        for i in range(len(order) - 1, -1, -1):
-            for n in order[i]:
+        for i in range(len(sorted) - 1, -1, -1):
+            for n in sorted[i]:
                 ans.append(n)
                 if len(ans) == k:
                     return ans

7ccefc7280e7974022842f2936d35b59539a853f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-05-01 14:48:05 +0000

    [LeetCode Sync] Runtime - 89 ms (5.08%), Memory - 22.5 MB (40.72%)

--- problems/0049-group-anagrams/solution.py
+++ problems/0049-group-anagrams/solution.py
@@ -1,14 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # get a ukey from each word
-        anagrams = collections.defaultdict(list)
+        ans = collections.defaultdict(list)
 
         for s in strs:
-            s = s.lower()
             ukey = [0] * 26
             for c in s:
                 ukey[ord(c) - ord("a")] += 1
-            anagrams[tuple(ukey)].append(s)
+            ans[tuple(ukey)].append(s)
         
-        return anagrams.values()
-
+        return ans.values()

d525cfc9467497b616751b38bf6621d3025c07af
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-05-20 22:59:22 +0000

    [LeetCode Sync] Runtime - 106 ms (7.07%), Memory - 17.5 MB (100.00%)

--- problems/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ problems/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
         l = 0
         r = len(nums) - 1
 
-        while l < r: 
+        while l < r:
             sum = nums[l] + nums[r]
+
             if sum < target:
                 l += 1
             elif sum > target:
@@ -12,4 +14,4 @@ class Solution:
             else:
                 return [l + 1, r + 1]
 
-
+        return []

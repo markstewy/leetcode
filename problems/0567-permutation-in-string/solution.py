4d6dc78d0f94b92028ace3e32522660e65a0b028
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-05-22 19:19:44 +0000

    [LeetCode Sync] Runtime - 54 ms (24.31%), Memory - 16.7 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,46 +1,48 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
+        if len(s1) > len(s2):
             return False
 
-        s1Count = {} # a char counf of the entire s1 string
-        s2Count = {} # a char count of the sub array within s2
+        count = {}
+        win = {}
 
-        # initialize counts
+        # initialize s1 count and s2 sliding window
         for i in range(len(s1)):
-            s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
-            s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
+            count[s1[i]] = count.get(s1[i], 0) + 1
+            win[s2[i]] = win.get(s2[i], 0) + 1
         
+        # initialize matches between s1 and s2 window
         matches = 0
-        need = len(s1Count) # matches for every char in s1
+        needed = len(count.keys())
 
-        # initialize matches count
-        for c in s2Count.keys():
-            if c in s1Count and s2Count[c] == s1Count[c]:
+        for s1c, s1count in count.items():
+            if s1c in win and win[s1c] == s1count:
                 matches += 1
 
-        # sliding window of length s1: add r and remove l then update matches
         l = 0
         for r in range(len(s1), len(s2)):
-            if matches == need:
+            # if matches correct return true
+            if matches == needed:
                 return True
-                
-            # add r and update matches
-            rc = s2[r]
-            s2Count[rc] = s2Count.get(rc, 0 ) + 1
-            if rc in s1Count and s2Count[rc] == s1Count[rc]:
-                matches += 1
-            if rc in s1Count and s2Count[rc] == s1Count[rc] + 1:
-                matches -= 1 # one too many, no longer a match
 
-            # remove l and update matches
-            lc = s2[l]
-            s2Count[lc] -= 1
-            if lc in s1Count and s2Count[lc] == s1Count[lc]:
-                matches += 1
-            if lc in s1Count and s2Count[lc] == s1Count[lc] - 1:
-                matches -= 1 # one too few, no longer a match
+            # remove l, update matches
+            c = s2[l]
+            win[c] -= 1
             l += 1
-            
-        return matches == need
+            if c in count and win[c] == count[c]:
+                matches += 1
+            if c in count and win[c] + 1 == count[c]:
+                matches -= 1
+
+            # add r, update matches
+            c = s2[r]
+            win[c] = win.get(c, 0) + 1
+            if c in count and win[c] == count[c]:
+                matches += 1
+            if c in count and win[c] - 1 == count[c]:
+                matches -= 1
+
+
+        return matches == needed
+
 

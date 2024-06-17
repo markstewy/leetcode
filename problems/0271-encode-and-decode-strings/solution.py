2b9d30ccf74a4acbfc59cdac0002ced031a65163
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-06-17 19:12:49 +0000

    [LeetCode Sync] Runtime - 70 ms (69.84%), Memory - 16.8 MB (100.00%)

--- problems/0271-encode-and-decode-strings/solution.py
+++ problems/0271-encode-and-decode-strings/solution.py
@@ -3,17 +3,19 @@ class Codec:
         """Encodes a list of strings to a single string.
         """
         encoded = ""
+
         for s in strs:
             encoded += str(len(s)) + "#" + s
         
         return encoded
         
+
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
         ans = []
-        i = 0
 
+        i = 0
         while i < len(s):
             l = ""
             while s[i] != "#":
@@ -25,7 +27,7 @@ class Codec:
             end = i + int(l)
             ans.append(s[start : end])
             i = end
-        
+
         return ans
 
 

bfd80b1e3bf2123d4debd192b1eed490f61ff843
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-06-17 19:24:29 +0000

    [LeetCode Sync] Runtime - 311 ms (5.66%), Memory - 26.2 MB (37.42%)

--- problems/0238-product-of-array-except-self/solution.py
+++ problems/0238-product-of-array-except-self/solution.py
@@ -14,17 +14,16 @@ class Solution:
         rtl.reverse()
 
         ans = []
+
         for i in range(len(nums)):
-            print(i)
             l = 1
             r = 1
-
+            
             if i > 0:
                 l = ltr[i - 1]
-
             if i < len(nums) - 1:
                 r = rtl[i + 1]
             
             ans.append(l * r)
-        
+
         return ans

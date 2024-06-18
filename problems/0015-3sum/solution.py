75fd6ca223c3190e5ea4ccdaca3de1d3cef6383b
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-06-18 22:05:38 +0000

    [LeetCode Sync] Runtime - 586 ms (74.68%), Memory - 20.6 MB (99.96%)

--- problems/0015-3sum/solution.py
+++ problems/0015-3sum/solution.py
@@ -3,27 +3,28 @@ class Solution:
         nums.sort()
         ans = []
 
+
         for i in range(len(nums)):
             l = i + 1
             r = len(nums) - 1
 
+            if nums[i] > 0:
+                continue # no 3 pos ints will sum to zero
+            
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            if nums[i] > 0:
-                break
-            
+
             while l < r:
                 sum = nums[i] + nums[l] + nums[r]
-
-                if sum < 0:
-                    l += 1
-                elif sum > 0:
+                
+                if sum > 0:
                     r -= 1
+                elif sum < 0:
+                    l += 1
                 else:
                     ans.append([nums[i], nums[l], nums[r]])
                     l += 1
+                
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        
         return ans

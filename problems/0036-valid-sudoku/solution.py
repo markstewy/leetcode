8a8019c250d3f28f60eb330aa680f7f756c63dc1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-12 10:53:38 +0000

    [LeetCode Sync] Runtime - 88 ms (5.83%), Memory - 16.4 MB (100.00%)

--- problems/0036-valid-sudoku/solution.py
+++ problems/0036-valid-sudoku/solution.py
@@ -9,16 +9,15 @@ class Solution:
                 n = board[r][c]
                 if n == ".":
                     continue
-
+                
                 if (
                     n in rows[r] or
                     n in cols[c] or
-                    n in sqs[(r//3, c//3)]
-                ):
-                    return False
-                else:
-                    rows[r].add(n)
-                    cols[c].add(n)
-                    sqs[(r//3, c//3)].add(n)
+                    n in sqs[r//3, c//3]
+                ): return False
+
+                rows[r].add(n)
+                cols[c].add(n)
+                sqs[r//3, c//3].add(n)
         
         return True

c9e3e3f0b7f2b504a705ee172e3de96eb9384d56
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-12 11:00:11 +0000

    [LeetCode Sync] Runtime - 4727 ms (5.01%), Memory - 31.8 MB (98.79%)

--- problems/0128-longest-consecutive-sequence/solution.py
+++ problems/0128-longest-consecutive-sequence/solution.py
@@ -3,13 +3,15 @@ class Solution:
         numSet = set(nums)
         ans = 0
 
-        for n in numSet:
-            if n - 1 not in numSet:
+        for n in nums:
+            if n - 1 not in numSet: # is start of sequence
                 l = 0
+                
                 while n in numSet:
                     l += 1
                     n += 1
-                if ans < l:
-                    ans = l
-        
+                
+                ans = max(ans, l)
         return ans
+
+

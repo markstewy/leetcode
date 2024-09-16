f8be2be92dc1a9b61c046b3f2fd67134dc2cf698
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-16 16:45:28 +0000

    [LeetCode Sync] Runtime - 66 ms (48.65%), Memory - 16.7 MB (100.00%)

--- problems/0003-longest-substring-without-repeating-characters/solution.py
+++ problems/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,16 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         cache = set()
-        ml = 0
 
         l = 0
+        r = 0
+        maxLength = 0
 
-        for r in range(len(s)):
+        while r < len(s):
             while s[r] in cache:
                 cache.remove(s[l])
                 l += 1
-            
-            cache.add(s[r])
-            ml = max(ml, r - l + 1)
 
-        return ml
+            cache.add(s[r])
+            maxLength = max(maxLength, r - l + 1)
+
+            r += 1
+        return maxLength
+

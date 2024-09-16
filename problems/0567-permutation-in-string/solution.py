8edbd69c69a1909e2264f6e73d74f4993505e8fb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-16 18:54:30 +0000

    [LeetCode Sync] Runtime - 54 ms (24.31%), Memory - 16.7 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -2,47 +2,42 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
+        
+        win, count = {}, {}
 
-        count = {}
-        win = {}
-
-        # initialize s1 count and s2 sliding window
+        # initialize counters
         for i in range(len(s1)):
             count[s1[i]] = count.get(s1[i], 0) + 1
             win[s2[i]] = win.get(s2[i], 0) + 1
-        
-        # initialize matches between s1 and s2 window
+
         matches = 0
         needed = len(count.keys())
 
-        for s1c, s1count in count.items():
-            if s1c in win and win[s1c] == s1count:
+        # initialize matches
+        for n, c in count.items():
+            if n in win and win[n] == c:
                 matches += 1
-
+        
         l = 0
         for r in range(len(s1), len(s2)):
-            # if matches correct return true
             if matches == needed:
                 return True
-
-            # remove l, update matches
+            
+            # update l
             c = s2[l]
             win[c] -= 1
             l += 1
-            if c in count and win[c] == count[c]:
+            if c in count and count[c] == win[c]:
                 matches += 1
-            if c in count and win[c] + 1 == count[c]:
+            if c in count and count[c] - 1 == win[c]:
                 matches -= 1
-
-            # add r, update matches
+            
+            # update r
             c = s2[r]
             win[c] = win.get(c, 0) + 1
-            if c in count and win[c] == count[c]:
+            if c in count and count[c] == win[c]:
                 matches += 1
-            if c in count and win[c] - 1 == count[c]:
+            if c in count and count[c] + 1 == win[c]:
                 matches -= 1
-
-
+            
         return matches == needed
-
-

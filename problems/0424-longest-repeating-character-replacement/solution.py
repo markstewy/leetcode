9d8f2d3a17101929ed4dc0f9ec5f0e4a2e2d15b5
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-16 17:33:30 +0000

    [LeetCode Sync] Runtime - 125 ms (33.83%), Memory - 16.8 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,23 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {} # char - > count
-        maxsub = 0
-        # is valid if length of sub - max value in count map <= k
+        substringCache = {} # char: count
+        maxSubstringLength = 0
 
         l = 0
+        
         for r in range(len(s)):
-            count[s[r]] = count.get(s[r], 0) + 1
+            # expand and add to cache
+            substringCache[s[r]] = substringCache.get(s[r], 0) + 1
 
-            while (r - l + 1) - max(count.values()) > k:
-                count[s[l]] -= 1
+            # shrink and remove from cache (while k exceeded)
+            while (r - l + 1) - max(substringCache.values()) > k:
+                substringCache[s[l]] -= 1
                 l += 1
             
-            maxsub = max(maxsub, r - l + 1)
-        
-        return maxsub
+            maxSubstringLength = max(maxSubstringLength, r - l + 1)
 
-
-
-        # expand to the right
-        # if not valid bring up left until is valid
-        # record the valid lengths to keep max (we only need the length not the substr idxs)
+        return maxSubstringLength

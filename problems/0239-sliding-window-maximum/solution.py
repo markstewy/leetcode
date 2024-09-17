30c9757fc2dbf1567fe1b8bb425cbcca1c0b142f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-17 04:57:38 +0000

    [LeetCode Sync] Runtime - 1162 ms (5.01%), Memory - 37.6 MB (13.76%)

--- problems/0239-sliding-window-maximum/solution.py
+++ problems/0239-sliding-window-maximum/solution.py
@@ -1,33 +1,20 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        
-        # keep a list of max
-        # if a newly added n is greater than nums to the left, remove the nums on the left
-        # the max for a window will always be the n in position 0
-        # remove the position 0 if it's index is no longer in the window
-
-        # track index to make sure it's removed from the window
-        # track val to knwo if it should overwrite values to the left
-        # use a double sided queue so you can pop from the front or the back at constant time
-
         dq = collections.deque()
         ans = []
 
-        l = 0
-        for r, n in enumerate(nums):
-            while len(dq) > 0 and dq[-1]["num"] < n:
+        for i, n in enumerate(nums):
+            while len(dq) > 0 and n > dq[-1]["num"]:
                 dq.pop()
             
-            # add r index and val
-            dq.append({"idx": r, "num": n})
+            while len(dq) > 0 and dq[0]["idx"] <= (i - k):
+                dq.popleft()
             
-            if (r - l + 1) >= k:
-                while dq[0]["idx"] < l:
-                    dq.popleft()
-                
+            dq.append({"idx": i, "num": n})
+            
+            if i >= k - 1:
                 ans.append(dq[0]["num"])
-                l += 1
-        
+
         return ans
 
-        
+

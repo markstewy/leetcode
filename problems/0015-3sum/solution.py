a3090fd613257f6f7a2267d96252ecbf6f9e3efb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-22 02:43:36 +0000

    [LeetCode Sync] Runtime - 677 ms (83.02%), Memory - 15.2 MB (100.00%)

--- problems/0015-3sum/solution.py
+++ problems/0015-3sum/solution.py
@@ -1,22 +1,23 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         nums.sort()
         ans = []
 
         for i in range(len(nums)):
             if nums[i] > 0:
-                break # no 3 pos will sum to zero
-            
+                break
             if i > 0 and nums[i - 1] == nums[i]:
-                continue # avoid duplicate solutions
-            
+                continue
+
             l = i + 1
             r = len(nums) - 1
 
             while l < r:
-
                 sum = nums[i] + nums[l] + nums[r]
-
                 if sum > 0:
                     r -= 1
                 elif sum < 0:
@@ -24,11 +25,7 @@ class Solution:
                 else:
                     ans.append([nums[i], nums[l], nums[r]])
                     l += 1
-                    while l > 0 and l < r and nums[l] == nums[l - 1]:
-                        l += 1 # avoid duplicate solutions
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
         return ans
-                
-
-
-
-
+        

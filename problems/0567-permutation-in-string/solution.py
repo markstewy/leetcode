7f20e6b2dc53684dc5ec510ff8355dd0f4c5a349
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-23 18:50:50 +0000

    [LeetCode Sync] Runtime - 49 ms (26.21%), Memory - 16.7 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,43 +1,44 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s2) < len(s1):
             return False
         
-        win, count = {}, {}
 
-        # initialize counters
+        s1Count, s2Count = {}, {}
+
         for i in range(len(s1)):
-            count[s1[i]] = count.get(s1[i], 0) + 1
-            win[s2[i]] = win.get(s2[i], 0) + 1
-
+            s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
+            s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
+        
+        needed = len(s1Count)
         matches = 0
-        needed = len(count.keys())
 
-        # initialize matches
-        for n, c in count.items():
-            if n in win and win[n] == c:
+        for n, c in s1Count.items():
+            if n in s2Count and s2Count[n] == c:
                 matches += 1
+        if matches == needed:
+            return True
         
         l = 0
         for r in range(len(s1), len(s2)):
+
+            c = s2[r]
+            s2Count[c] = s2Count.get(c, 0) + 1
+            if c in s1Count and s2Count[c] == s1Count[c]:
+                matches += 1
+            if c in s1Count and s2Count[c] == s1Count[c] + 1:
+                matches -= 1
+            
+
+            c = s2[l]
+            s2Count[c] -= 1
+            l += 1
+            if c in s1Count and s2Count[c] == s1Count[c]:
+                matches += 1
+            if c in s1Count and s2Count[c] == s1Count[c] - 1:
+                matches -= 1
+            
             if matches == needed:
                 return True
-            
-            # update l
-            c = s2[l]
-            win[c] -= 1
-            l += 1
-            if c in count and count[c] == win[c]:
-                matches += 1
-            if c in count and count[c] - 1 == win[c]:
-                matches -= 1
-            
-            # update r
-            c = s2[r]
-            win[c] = win.get(c, 0) + 1
-            if c in count and count[c] == win[c]:
-                matches += 1
-            if c in count and count[c] + 1 == win[c]:
-                matches -= 1
-            
-        return matches == needed
+
+        return False

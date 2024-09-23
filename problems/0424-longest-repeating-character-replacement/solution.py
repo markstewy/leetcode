f1b40440ed1677e229568fb0b4b311d4fb9895e3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-23 18:35:48 +0000

    [LeetCode Sync] Runtime - 119 ms (35.89%), Memory - 16.5 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,26 +1,15 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        sCount = {}
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {} # c: count
         maxL = 0
 
         l = 0
         for r in range(len(s)):
-            # expand right
-            sCount[s[r]] = sCount.get(s[r], 0) + 1
-            
-            # if not valid bring up left
-            while (r - l + 1) - max(sCount.values()) > k:
-                sCount[s[l]] -= 1
+            count[s[r]] = count.get(s[r], 0) + 1
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
                 l += 1
-            
-            # record valid max length
             maxL = max(maxL, r - l + 1)
-            
         
         return maxL
-

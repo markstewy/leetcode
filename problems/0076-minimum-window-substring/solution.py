97f41156532899e4fa097b968a12f10d9e03ed73
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-23 21:44:28 +0000

    [LeetCode Sync] Runtime - 88 ms (39.13%), Memory - 17.4 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,39 +1,41 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if len(t) > len(s):
             return ""
-        if s == t:
-            return t
-
-        count, win = {}, {}
-        # intialize count
-        for c in t:
-            count[c] = count.get(c, 0) + 1
         
-        matches = 0
-        needed = len(count.keys())
-
-        minLength = float('inf')
+        minLength = float("infinity")
         minl, minr = 0, 0
+        sCount, tCount = {}, {}
+
+        for i in range(len(t)):
+            tCount[t[i]] = tCount.get(t[i], 0) + 1
+        
+        needed = len(tCount)
+        matches = 0
 
         l = 0
-        for r in range(len(s)): 
-            # move up r pointer 
-            win[s[r]] = win.get(s[r], 0) + 1
-            if s[r] in count and count[s[r]] == win[s[r]]:
+        for r in range(len(s)):
+            # add r
+            c = s[r]
+            sCount[c] = sCount.get(c, 0) + 1
+            if c in tCount and sCount[c] == tCount[c]:
                 matches += 1
-            
+
+            # if enough matches, bring up l and reset min length if remains valid
             while matches >= needed:
-                if minLength > r - l + 1:
+                if (r - l + 1) < minLength:
                     minLength = r - l + 1
                     minl = l
-                    minr = r
-
-                # move up l pointer
-                win[s[l]] -= 1
-                if s[l] in count and win[s[l]] == count[s[l]] - 1:
-                    matches -= 1
+                    minr = r + 1 # right not inclusive in []
+                
+                c = s[l]
+                sCount[c] -= 1
                 l += 1
-
-        return str(s[minl : minr + 1]) if minLength < float('inf') else  ""
-
+                if c in tCount and sCount[c] == tCount[c] - 1:
+                    matches -= 1
+        
+        if minLength == float("infinity"):
+            return ""
+        else:
+            return s[minl : minr]
+            

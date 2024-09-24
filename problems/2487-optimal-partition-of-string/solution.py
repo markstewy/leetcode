51abb7057b0dc226791ef568714f2cef68056c4c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-24 01:26:21 +0000

    [LeetCode Sync] Runtime - 98 ms (8.58%), Memory - 17.4 MB (100.00%)

--- problems/2487-optimal-partition-of-string/solution.py
+++ problems/2487-optimal-partition-of-string/solution.py
@@ -1,21 +1,14 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        cache = set()
-        ans = []
+        cSet = set()
 
-        start, end = 0, 0
-        while end < len(s):
-            # if end is in cache add sub and set start to end
-            if s[end] in cache:
-                ans.append(s[start: end])
-                start = end
-                cache.clear()
-            # else expand the subtring to the right
-            cache.add(s[end])
-            end += 1
-
-        return len(ans) + 1
-
+        numSubs = 1
+        for c in s:
+            if c in cSet:
+                numSubs += 1
+                cSet.clear()
+            
+            cSet.add(c)
         
-
+        return numSubs
 

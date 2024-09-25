732e94579d8d6ce43a9834e7dfbb862afb85aed9
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-25 21:56:03 +0000

    [LeetCode Sync] Runtime - 53 ms (23.37%), Memory - 12.1 MB (99.94%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,44 +1,48 @@
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        if len(s1) > len(s2):
             return False
-        
 
-        s1Count, s2Count = {}, {}
+        s1count, s2count = {}, {}
 
         for i in range(len(s1)):
-            s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
-            s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
+            s1count[s1[i]] = s1count.get(s1[i], 0) + 1
+            s2count[s2[i]] = s2count.get(s2[i], 0) + 1
         
-        needed = len(s1Count)
         matches = 0
+        needed = len(s1count)
 
-        for n, c in s1Count.items():
-            if n in s2Count and s2Count[n] == c:
+        for n, c in s1count.items():
+            if n in s2count and s2count[n] == c:
                 matches += 1
-        if matches == needed:
-            return True
-        
+
         l = 0
         for r in range(len(s1), len(s2)):
-
-            c = s2[r]
-            s2Count[c] = s2Count.get(c, 0) + 1
-            if c in s1Count and s2Count[c] == s1Count[c]:
-                matches += 1
-            if c in s1Count and s2Count[c] == s1Count[c] + 1:
-                matches -= 1
-            
-
-            c = s2[l]
-            s2Count[c] -= 1
-            l += 1
-            if c in s1Count and s2Count[c] == s1Count[c]:
-                matches += 1
-            if c in s1Count and s2Count[c] == s1Count[c] - 1:
-                matches -= 1
-            
             if matches == needed:
                 return True
+            # add right
+            c = s2[r]
+            s2count[c] = s2count.get(c, 0) + 1
 
-        return False
+            if c in s1count and s2count[c] == s1count[c]:
+                matches += 1
+            if c in s1count and s2count[c] == s1count[c] + 1:
+                matches -= 1
+            
+            # remove left
+            c = s2[l]
+            l += 1
+            s2count[c] -= 1
+
+            if c in s1count and s2count[c] == s1count[c]:
+                matches += 1
+            if c in s1count and s2count[c] == s1count[c] - 1:
+                matches -= 1
+        
+        return matches == needed
+         

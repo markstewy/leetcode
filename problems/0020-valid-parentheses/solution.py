f00c21861698157701d9ae4cea7a05df5942d1d7
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-25 23:42:24 +0000

    [LeetCode Sync] Runtime - 12 ms (5.79%), Memory - 11.5 MB (100.00%)

--- problems/0020-valid-parentheses/solution.py
+++ problems/0020-valid-parentheses/solution.py
@@ -1,21 +1,31 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        closeToOpen = {
-            "}": "{",
-            "]": "[",
-            ")": "("
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        openToClose = {
+            "(": ")",
+            "{": "}",
+            "[": "]"
         }
 
+        if s[0] not in openToClose:
+            return False
+
+        stack = []
+
         for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
+            isOpen = c in openToClose
+            # if it is a open, add the closed to the stack
+            if isOpen:
+                stack.append(openToClose[c])
+            # while it's a closed, pop it off of the stack
+            else:
+                if stack and stack[-1] == c:
                     stack.pop()
                 else:
                     return False
-            else:
-                stack.append(c)
-        
-        return True if not stack else False
-
-
+        return len(stack) == 0
+                
+            # when done the stack should be empty

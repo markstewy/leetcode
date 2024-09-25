0ac00599b5023667e11338e5d060d2cd5ebabad6
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-25 21:10:20 +0000

    [LeetCode Sync] Runtime - 660 ms (83.86%), Memory - 14.9 MB (100.00%)

--- problems/0015-3sum/solution.py
+++ problems/0015-3sum/solution.py
@@ -1,25 +1,28 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         nums.sort()
         ans = []
 
         for i in range(len(nums)):
-
-            if nums[i] > 0:
-                break
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
+            if nums[i] > 0:
+                break
+            
             l = i + 1
-            r = len(nums) -1
+            r = len(nums) - 1 
 
             while l < r:
                 sum = nums[i] + nums[l] + nums[r]
 
-                if sum < 0:
-                    l += 1
-                elif sum > 0:
+                if sum > 0:
                     r -= 1
+                elif sum < 0:
+                    l += 1
                 else:
                     ans.append([nums[i], nums[l], nums[r]])
                     l += 1

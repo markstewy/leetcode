2f6e4fb53b320a411a64a28bbb87be3cab83ec3f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-27 20:26:16 +0000

    [LeetCode Sync] Runtime - 1196 ms (5.01%), Memory - 38.2 MB (12.08%)

--- problems/0239-sliding-window-maximum/solution.py
+++ problems/0239-sliding-window-maximum/solution.py
@@ -1,22 +1,21 @@
-class Solution(object):
-    def maxSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         dq = deque()
         ans = []
 
         for i, n in enumerate(nums):
-            while dq and dq[-1]["val"] < n:
+            while dq and n > dq[-1]["val"]:
                 dq.pop()
+            
             dq.append({"val": n, "idx": i})
+            
 
             if i >= k - 1:
-                while dq and dq[0]["idx"] <= i - k:
+                while dq[0]["idx"] <= i - k:
                     dq.popleft()
-            
                 ans.append(dq[0]["val"])
-        return ans  
+        return ans
+            
+
+
 

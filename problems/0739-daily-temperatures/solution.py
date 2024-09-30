60581b2a16efc095283455a42bd465cb47c2cefb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-09-30 21:52:42 +0000

    [LeetCode Sync] Runtime - 932 ms (5.14%), Memory - 43 MB (5.42%)

--- problems/0739-daily-temperatures/solution.py
+++ problems/0739-daily-temperatures/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = [] # {temp: number, idx: number}
-        solution = [0] * len(temperatures)
+        stack = []
+        ans = [0] * len(temperatures)
 
         for i, t in enumerate(temperatures):
-            # if t is greater than prior, set idx differences in solution
-            # else append to stack
-
-            while stack and t > stack[-1]["temp"]:
-                solution[stack[-1]["idx"]] = i - stack[-1]["idx"]
-                stack.pop()
             
+            while stack and t > stack[-1]["temp"]:
+                topIdx = stack[-1]["idx"]
+                ans[topIdx] = i - topIdx
+                stack.pop()
+
             stack.append({"temp": t, "idx": i})
         
-        return solution
+        return ans

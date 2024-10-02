6826eba071078b6f3c60598d78e003bb8223aab1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-02 22:17:22 +0000

    [LeetCode Sync] Runtime - 26 ms (3.82%), Memory - 16.5 MB (100.00%)

--- problems/0020-valid-parentheses/solution.py
+++ problems/0020-valid-parentheses/solution.py
@@ -1,22 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        openToClosed = {
+        openToClose = {
             "(": ")",
-            "{": "}",
             "[": "]",
+            "{": "}"
         }
+        stack = []
 
-        if s[0] not in openToClosed:
+        if s[0] not in openToClose:
             return False
 
-
-        stack = []
         for c in s:
-            if c in openToClosed:
-                stack.append(openToClosed[c])
-            elif stack and c == stack[-1]:
-                stack.pop()
+            if c in openToClose:
+                stack.append(openToClose[c])
             else:
-                return False
-                     
+                if stack and c == stack[-1]:
+                    stack.pop()
+                else: return False
+        
         return len(stack) == 0

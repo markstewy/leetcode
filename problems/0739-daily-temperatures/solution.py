1db28ab329a59546eb8ebb470636a40c016c3ec3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-02 21:22:15 +0000

    [LeetCode Sync] Runtime - 1015 ms (5.14%), Memory - 47.2 MB (5.42%)

--- problems/0739-daily-temperatures/solution.py
+++ problems/0739-daily-temperatures/solution.py
@@ -1,15 +1,22 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []
-        ans = [0] * len(temperatures)
-
+        temps = []
         for i, t in enumerate(temperatures):
-            
-            while stack and t > stack[-1]["temp"]:
-                topIdx = stack[-1]["idx"]
-                ans[topIdx] = i - topIdx
-                stack.pop()
-
-            stack.append({"temp": t, "idx": i})
+            temps.append({"temp": t, "idx": i})
         
-        return ans
+        days = [0] * len(temps)
+
+        # add temp to stack, while temp is higher than the top of the stack pop and assign the diff in idx to the days array
+
+        stack = []
+        for currDay in temps:
+            
+            while stack and stack[-1]["temp"] < currDay["temp"]:
+                dayCount = currDay["idx"] - stack[-1]["idx"]
+                priorDayIdx = stack[-1]["idx"]
+                days[priorDayIdx] = dayCount
+                stack.pop()
+            
+            stack.append(currDay)
+        
+        return days

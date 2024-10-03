f66261d79475b7a2dbc06183b46747b8ab3e423e
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-03 23:03:14 +0000

    [LeetCode Sync] Runtime - 65 ms (6.13%), Memory - 17.2 MB (100.00%)

--- problems/0150-evaluate-reverse-polish-notation/solution.py
+++ problems/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        # if it's an int push to the stack
-        # if not perform the operation on the ints
-        # a, b, c,   for the '-' operation it woudl be b - c (rearrange the pops accordingly)
-
+        signs = "+-?*"
         stack = []
-        for t in tokens:                
+
+        for t in tokens:
             if t == "+":
                 stack.append(stack.pop() + stack.pop())
+            elif t == "*":
+                stack.append(stack.pop() * stack.pop())
             elif t == "-":
                 a = stack.pop()
                 b = stack.pop()
                 stack.append(b - a)
-            elif t == "*":
-                stack.append(stack.pop() * stack.pop())
             elif t == "/":
                 a = stack.pop()
                 b = stack.pop()
@@ -22,3 +20,5 @@ class Solution:
                 stack.append(int(t))
         
         return stack[0]
+
+

4bcf4b297c9720b22fbdef6b320327f8a557242a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-03 23:09:22 +0000

    [LeetCode Sync] Runtime - 40 ms (5.30%), Memory - 17 MB (100.00%)

--- problems/0022-generate-parentheses/solution.py
+++ problems/0022-generate-parentheses/solution.py
@@ -2,18 +2,20 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
 
-        def helper(s, lCount, rCount):
-
-            # return / break statement
-            if len(s) == n * 2:
+        def helper(s, l, r):
+            # base case
+            if len(s) == 2 * n:
                 ans.append(s)
                 return
-
-            # scenarios
-            if rCount < lCount:
-                helper(s + ")", lCount, rCount + 1)
-            if lCount < n:
-                helper(s + "(", lCount + 1, rCount)
             
+            if l > r:
+                helper(s + ")", l, r + 1)
+            if l < n:
+                helper(s + "(", l + 1, r)
+        
         helper("(", 1, 0)
+
         return ans
+
+
+

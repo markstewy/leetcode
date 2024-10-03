73797eec87763581d7d397570896cf19dc6eb0ce
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-03 21:51:13 +0000

    [LeetCode Sync] Runtime - 731 ms (5.48%), Memory - 27.5 MB (99.95%)

--- problems/0121-best-time-to-buy-and-sell-stock/solution.py
+++ problems/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        mp = 0
-
+        profit = 0
         buy = prices[0]
-        for sell in prices:
-            profit = sell - buy
-            mp = max(profit, mp)
         
+        for sell in prices:
+            profit = max(profit, sell - buy)
+
             if sell < buy:
                 buy = sell
-            
-        return mp
+        return profit
+

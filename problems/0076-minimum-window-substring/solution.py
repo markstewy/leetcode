76e17f58a5428c085d97107666a2594a14d3f654
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-03 22:35:00 +0000

    [LeetCode Sync] Runtime - 96 ms (37.64%), Memory - 17.4 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -4,50 +4,35 @@ class Solution:
             return ""
 
         sCount, tCount = {}, {}
+        minLength = float("infinity")
+        minl, minr = -1, -1
 
-        for i in range(len(t)):
-            tCount[t[i]] = tCount.get(t[i], 0) + 1
+        for c in t:
+            tCount[c] = tCount.get(c, 0) + 1
         
-        matches = 0
         needed = len(tCount)
-        minL = float("infinity")
-        minl = -1
-        minr = -1
-
-        # for c, count in tCount.items():
-        #     if c in sCount and sCount[c] == tCount[c]:
-        #         matches += 1
-
-
+        matches = 0
+        
         l = 0
         for r in range(len(s)):
             # add r
             c = s[r]
             sCount[c] = sCount.get(c, 0) + 1
-            if c in tCount and tCount[c] == sCount[c]:
+
+            if c in tCount and sCount[c] == tCount[c]:
                 matches += 1
-            
-            # update min if matches
-            if matches >= needed:
-                if (r - l + 1) < minL:
-                    minL = r - l + 1
+
+            # remove l
+            while matches >= needed:
+                if r - l + 1 < minLength:
+                    minLength = r - l + 1
                     minl = l
                     minr = r
-
-            # slid up l 
-            while matches >= needed:
+                
                 c = s[l]
                 sCount[c] -= 1
-                l += 1
-
                 if c in tCount and sCount[c] == tCount[c] - 1:
                     matches -= 1
-                
-                if matches >= needed:
-                    if (r - l + 1) < minL:
-                        minL = r - l + 1
-                        minl = l
-                        minr = r
-
-            
+                l += 1
+        
         return s[minl : minr + 1]

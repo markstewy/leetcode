f162899823daa9d92086a2dabd2dd23f20d3ee13
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-03 21:14:51 +0000

    [LeetCode Sync] Runtime - 39 ms (6.02%), Memory - 17 MB (100.00%)

--- problems/0242-valid-anagram/solution.py
+++ problems/0242-valid-anagram/solution.py
@@ -3,12 +3,10 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        sCount, tCount = {}, {}
 
+        scount, tcount = {}, {}
         for i in range(len(s)):
-            sCount[s[i]] = sCount.get(s[i], 0) + 1
-            tCount[t[i]] = tCount.get(t[i], 0) + 1
-        
-        return sCount == tCount
+            scount[s[i]] = scount.get(s[i], 0) + 1
+            tcount[t[i]] = tcount.get(t[i], 0) + 1
 
-            
+        return scount == tcount

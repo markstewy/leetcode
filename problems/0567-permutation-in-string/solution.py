ebe042a5c082b890468025b5622342065446f5eb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-03 22:26:34 +0000

    [LeetCode Sync] Runtime - 56 ms (23.86%), Memory - 16.8 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
-            return False
-
+            return False    
+    
         s1Count, s2Count = {}, {}
 
         for i in range(len(s1)):
             s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
             s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
-        
+
         needed = len(s1Count)
         matches = 0
 
-        for n, c in s1Count.items():
-            if n in s2Count and s2Count[n] == c:
+        for c in s1Count:
+            if c in s2Count and s2Count[c] == s1Count[c]:
                 matches += 1
         
         l = 0
@@ -21,21 +21,22 @@ class Solution:
             if matches == needed:
                 return True
             
-            # shift r
+            # add r
             c = s2[r]
             s2Count[c] = s2Count.get(c, 0) + 1
             if c in s1Count and s2Count[c] == s1Count[c]:
                 matches += 1
             if c in s1Count and s2Count[c] == s1Count[c] + 1:
                 matches -= 1
- 
-            # shift l
+
+            # remove l
             c = s2[l]
             s2Count[c] -= 1
-            l += 1
             if c in s1Count and s2Count[c] == s1Count[c]:
                 matches += 1
             if c in s1Count and s2Count[c] == s1Count[c] - 1:
                 matches -= 1
+            l += 1
         
         return matches == needed
+

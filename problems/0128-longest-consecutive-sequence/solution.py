8337950ca09a563d69be98efcf80848c81d21f6d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-04 15:49:23 +0000

    [LeetCode Sync] Runtime - 4276 ms (5.01%), Memory - 31.9 MB (98.65%)

--- problems/0128-longest-consecutive-sequence/solution.py
+++ problems/0128-longest-consecutive-sequence/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nSet = set(nums)
-        mL = 0
+        ml = 0
 
         for n in nums:
-            if n - 1 not in nSet:
+            if n - 1 not in nSet and n in nSet:
                 l = 0
                 while n + l in nSet:
                     l += 1
-                mL = max(mL, l)
-        return mL
+                ml = max(l, ml)
+        return ml

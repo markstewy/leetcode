e2ef0d5697e11a2a812c0e223ddf629c9233c245
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-22 03:23:24 +0000

    [LeetCode Sync] Runtime - 56 ms (87.78%), Memory - 17.1 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,38 +1,42 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
-
+        
         sCount, tCount = {}, {}
-        minLength = float("infinity")
-        minl, minr = -1, -1
 
         for c in t:
             tCount[c] = tCount.get(c, 0) + 1
         
-        needed = len(tCount)
         matches = 0
-        
+        needed = len(tCount.keys())
+
+        minLength = float("infinity")
+        minl = -1
+        minr = -1
+
         l = 0
         for r in range(len(s)):
             # add r
             c = s[r]
             sCount[c] = sCount.get(c, 0) + 1
-
-            if c in tCount and sCount[c] == tCount[c]:
+            if c in tCount and tCount[c] == sCount[c]:
                 matches += 1
-
-            # remove l
-            while matches >= needed:
-                if r - l + 1 < minLength:
+            if matches >= needed:
+                if (r - l + 1) < minLength:
                     minLength = r - l + 1
                     minl = l
                     minr = r
-                
+
+            # while enough matches bring up l
+            while matches >= needed:
                 c = s[l]
                 sCount[c] -= 1
                 if c in tCount and sCount[c] == tCount[c] - 1:
                     matches -= 1
                 l += 1
-        
+
+                if matches >= needed:
+                    if (r - l + 1) < minLength:
+                        minLength = r - l + 1
+                        minl = l
+                        minr = r
         return s[minl : minr + 1]

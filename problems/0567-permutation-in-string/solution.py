e84408ba23f3c5a8ec6d7215786ee6bb31b35981
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-22 03:32:03 +0000

    [LeetCode Sync] Runtime - 27 ms (41.62%), Memory - 16.5 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,42 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
-            return False    
-    
+            return False
+
         s1Count, s2Count = {}, {}
 
-        for i in range(len(s1)):
-            s1Count[s1[i]] = s1Count.get(s1[i], 0) + 1
-            s2Count[s2[i]] = s2Count.get(s2[i], 0) + 1
-
-        needed = len(s1Count)
-        matches = 0
-
-        for c in s1Count:
-            if c in s2Count and s2Count[c] == s1Count[c]:
-                matches += 1
+        for c in s1:
+            s1Count[c] = s1Count.get(c, 0) + 1
         
+        matches = 0
+        needed = len(s1Count.keys())
+
         l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == needed:
-                return True
-            
+        for r in range(len(s2)):
             # add r
             c = s2[r]
             s2Count[c] = s2Count.get(c, 0) + 1
             if c in s1Count and s2Count[c] == s1Count[c]:
                 matches += 1
             if c in s1Count and s2Count[c] == s1Count[c] + 1:
-                matches -= 1
+                matches -= 1 # one too many
 
-            # remove l
-            c = s2[l]
-            s2Count[c] -= 1
-            if c in s1Count and s2Count[c] == s1Count[c]:
-                matches += 1
-            if c in s1Count and s2Count[c] == s1Count[c] - 1:
-                matches -= 1
-            l += 1
+            # if len of string is 1 too long then bring up l
+            if (r - l + 1) > len(s1):
+                c = s2[l]
+                s2Count[c] -= 1
+                if c in s1Count and s2Count[c] == s1Count[c]:
+                    matches += 1
+                if c in s1Count and s2Count[c] == s1Count[c] - 1:
+                    matches -= 1 # one too few
+                l += 1
+
+            # if matches are exact return true
+            if matches == needed:
+                return True
         
-        return matches == needed
+        return False
+
+
+
+
 

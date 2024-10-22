e13c2f9d4dcd4fd2f05b69bae42456259d0b1272
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-22 06:44:32 +0000

    [LeetCode Sync] Runtime - 3 ms (55.24%), Memory - 18.3 MB (100.00%)

--- problems/0150-evaluate-reverse-polish-notation/solution.py
+++ problems/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,24 +1,33 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        signs = "+-?*"
         stack = []
+        signs = "-+/*"
 
-        for t in tokens:
-            if t == "+":
-                stack.append(stack.pop() + stack.pop())
-            elif t == "*":
-                stack.append(stack.pop() * stack.pop())
-            elif t == "-":
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(b - a)
-            elif t == "/":
-                a = stack.pop()
-                b = stack.pop()
-                stack.append(trunc(b / a))
+        def strToInt(n):
+            sign = 1
+            if n[0] == "-":
+                sign = -1
+            n = n.lstrip("-")
+            return int(n) * sign
+
+        for n in tokens:
+            if n not in signs:
+                stack.append(strToInt(n))
             else:
-                stack.append(int(t))
-        
-        return stack[0]
+                r = stack.pop()
+                l = stack.pop()
+                if n == "+":
+                    stack.append(l + r)
+                elif n == "*":
+                    stack.append(l * r)
+                elif n == "-":
+                    stack.append(l - r)
+                elif n == "/":
+                    sign = 1
+                    if l * r < 0:
+                        sign = -1
+                    stack.append(abs(l) // abs(r) * sign) 
+        return stack[-1]
+
 
 

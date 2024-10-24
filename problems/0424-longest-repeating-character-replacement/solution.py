1c2b3cd0c7bf53a44a7ecf835ccbfe424b61ad58
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-24 23:41:07 +0000

    [LeetCode Sync] Runtime - 79 ms (76.12%), Memory - 16.8 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,19 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        maxLength = 0
+        maxLen = 0
 
         l = 0
         for r in range(len(s)):
-            # add r
             count[s[r]] = count.get(s[r], 0) + 1
 
             while (r - l + 1) - max(count.values()) > k:
-                # move up l while more than 2 letters or both letters greater than k
                 count[s[l]] -= 1
                 l += 1
             
-            maxLength = max(maxLength, r - l + 1)
+            maxLen = max(maxLen, r - l + 1)
         
-        return maxLength
+        return maxLen
 

b6618c6028d6850361faa41fa7de8168b6f7075b
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-24 21:26:03 +0000

    [LeetCode Sync] Runtime - 6 ms (46.01%), Memory - 16.7 MB (100.00%)

--- problems/0013-roman-to-integer/solution.py
+++ problems/0013-roman-to-integer/solution.py
@@ -1,37 +1,38 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        total = 0
-        vals = {
+        numerals = {
             "I": 1,
+            "II": 2,
+            "III": 3,
+            "IV": 4,
             "V": 5,
+            "VI": 6,
+            "VII": 7,
+            "VIII": 8,
+            "IX": 9,
             "X": 10,
             "L": 50,
             "C": 100,
             "D": 500,
             "M": 1000,
-            "IV": 4,
-            "IX": 9,
             "XL": 40,
             "XC": 90,
             "CD": 400,
             "CM": 900
         }
-        
+
+        total = 0
         i = 0
         while i < len(s):
-            # check for 2 character values
-            if i < len(s) - 1 and s[i : i + 2] in vals:
-                total += vals[s[i : i + 2]]
+            if  i < len(s) - 1 and s[i : i + 2] in numerals:
+                numeral = s[i : i + 2]
+                total += numerals[numeral]
                 i += 2
-            else:       
-                total += vals[s[i]]
+            else:
+                numeral = s[i : i + 1]
+                total += numerals[numeral]
                 i += 1
         
         return total
-                
-                    
-                    
-                    
-                    
-                    
-                    
+
+            

5b452efb2e8cc40829a3ee3f37d1dc078caa1258
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-26 18:18:52 +0000

    [LeetCode Sync] Runtime - 11 ms (95.45%), Memory - 17.1 MB (100.00%)

--- problems/1483-rank-teams-by-votes/solution.py
+++ problems/1483-rank-teams-by-votes/solution.py
@@ -1,27 +1,25 @@
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
-        count = {} # ch: [0, 0, 0]
 
-        for v in votes:
-            for i, ch in enumerate(v):
-                if ch not in count:
-                    count[ch] = [0] * len(v)
-                count[ch][i] += 1
+        candidateCount = len(votes[0])
+        count = {} # [] size of candidates
 
-        countArr = list(count.items()) # [("A", [0, 0, 0]), ...]
-        countArr.sort()
-        countArr.sort(key=lambda x: x[1], reverse=True)
+        for candidate in votes[0]:
+            count[candidate] = [0] * candidateCount # number of potential ranks
         
+        for ballot in votes:
+            for place, candidate in enumerate(ballot):
+                count[candidate][place] += 1
+        
+        candidateVotes = list(count.items())
+        candidateVotes.sort()
+        candidateVotes.sort(key=lambda x:x[1], reverse=True)
+
         ans = ""
-        for tup in countArr:
-            ans += tup[0]
+        for c in candidateVotes:
+            ans += c[0]
         
         return ans
-
-
-
-
-            
 
         
 

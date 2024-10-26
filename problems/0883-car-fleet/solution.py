a79cf7f942d99aa5dc28570d0f96a3e67e45eb62
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-26 16:19:00 +0000

    [LeetCode Sync] Runtime - 125 ms (97.57%), Memory - 39.5 MB (96.76%)

--- problems/0883-car-fleet/solution.py
+++ problems/0883-car-fleet/solution.py
@@ -1,20 +1,18 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        combined = list(zip(position, speed))
-        combined.sort()
+        cars = list(zip(position, speed))
+        cars.sort()
 
-        time = []
-
-        for c in combined:
-            arrival = (target - c[0]) / c[1]
-            time.append(arrival)
+        times = []
+        for c in cars:
+            # append travel time (arrival time)
+            times.append((target - c[0]) / c[1])
         
-
         fleetArrivalTime = 0
         fleetCount = 0
-        for i in range(len(time) - 1, -1, -1):
-            if time[i] > fleetArrivalTime:
+        for i in range(len(times) - 1, -1, -1):
+            if times[i] > fleetArrivalTime:
                 fleetCount += 1
-                fleetArrivalTime = time[i]
+                fleetArrivalTime = times[i]
         
         return fleetCount

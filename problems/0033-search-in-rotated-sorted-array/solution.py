ae50861ad99ff0c7408497d026d7a368c8831d21
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-28 17:54:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17 MB (100.00%)

--- problems/0033-search-in-rotated-sorted-array/solution.py
+++ problems/0033-search-in-rotated-sorted-array/solution.py
@@ -1,26 +1,30 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        l = 0
+        r = len(nums) - 1
 
         while l <= r:
             m = l + (r - l) // 2
-            if target == nums[m]:
+
+            if nums[l] == target:
+                return l
+            if nums[r] == target:
+                return r
+            if nums[m] == target:
                 return m
             
-            if nums[m] >= nums[0]:
-                # we are on the left side continuous array
-                if target >= nums[0] and target <= nums[m]:
+
+            # find the continuous section to check
+            if nums[l] < nums[m]:
+                if nums[l] < target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             else:
-                # we are on the right side continuous array
-                if target >= nums[m] and target <= nums[r]:
+                if nums[m] < target < nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
-
+        
         return -1
-
-
-# [4, 5, 6, 7, 1, 2, 3]
+        

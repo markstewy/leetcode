ca2cc6610a726c1be79b93e588d999d0f4ce90b1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-28 19:02:46 +0000

    [LeetCode Sync] Runtime - 221 ms (5.13%), Memory - 81 MB (5.59%)

--- problems/1023-time-based-key-value-store/solution.py
+++ problems/1023-time-based-key-value-store/solution.py
@@ -1,33 +1,38 @@
 class TimeMap:
 
     def __init__(self):
-        self.values = collections.defaultdict(list)
+        self.store = collections.defaultdict(list)
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.values[key].append({"val": value, "time": timestamp})
-
+        self.store[key].append({"val": value, "time": timestamp})
 
     def get(self, key: str, timestamp: int) -> str:
-        values = self.values[key]
-        l, r = 0, len(values) - 1
-        ans = ""
+        ans = {"val": "", "time": 0}
 
-        while l <= r:
-            m = l + (r - l) // 2
-            if values[m]["time"] < timestamp:
-                ans = values[m]["val"]
-                l = m + 1
-            elif values[m]["time"] > timestamp:
-                r = m - 1
-            else:
-                return values[m]["val"]
+        if key in self.store and self.store[key]:
+            entries = self.store[key]
+            l, r = 0, len(entries) - 1
+            
+            while l <= r:
+                m = l + (r - l) // 2
 
-        return ans      
-
- 
-
-
+                if entries[m]["time"] > timestamp: # lower half
+                    r = m - 1
+                elif entries[m]["time"] < timestamp: # upper half
+                    l = m + 1
+                    # record m as prev option
+                    if entries[m]["time"] >= ans["time"]:
+                        ans = entries[m]
+                else:
+                    return entries[m]["val"]
         
+        return ans["val"]
+            
+
+
+
+
 
 
 # Your TimeMap object will be instantiated and called as such:

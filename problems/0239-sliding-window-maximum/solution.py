aa5c7ecb7c83a1501c1022984e484940e42f2748
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-29 05:40:50 +0000

    [LeetCode Sync] Runtime - 263 ms (15.93%), Memory - 38.8 MB (10.95%)

--- problems/0239-sliding-window-maximum/solution.py
+++ problems/0239-sliding-window-maximum/solution.py
@@ -1,27 +1,19 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # dq where the right collpases on any values less than and the left loses any values out of the window
-        # the max wil always be in position[0] ie. dq[0]
-
         dq = deque()
         ans = []
 
-        # add
         for i, n in enumerate(nums):
-            while dq and dq[-1]["val"] <= n:
+            while dq and n > dq[-1]["val"]:
                 dq.pop()
-            dq.append({"idx": i, "val": n})
+            
+            dq.append({"val": n, "idx": i})
 
-            while dq and dq[0]["idx"] <= i - k:
+            lastIdxInWindow = i - k + 1
+            while dq and dq[0]["idx"] < lastIdxInWindow:
                 dq.popleft()
-        
-            kFirstIdx = k - 1
-            if i >= kFirstIdx:
+            
+            if lastIdxInWindow >= 0:
                 ans.append(dq[0]["val"])
-        
+            
         return ans
-
-
-
-
-        # start recroding max only after you get to window size

b567775bf5c60c12da2a0d515a374c19a09fa005
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-29 23:44:17 +0000

    [LeetCode Sync] Runtime - 3 ms (55.24%), Memory - 18.4 MB (100.00%)

--- problems/0150-evaluate-reverse-polish-notation/solution.py
+++ problems/0150-evaluate-reverse-polish-notation/solution.py
@@ -1,33 +1,35 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
-        signs = "-+/*"
+        signs = "+-*/"
+        total = 0
 
-        def strToInt(n):
-            sign = 1
-            if n[0] == "-":
-                sign = -1
-            n = n.lstrip("-")
-            return int(n) * sign
-
-        for n in tokens:
-            if n not in signs:
-                stack.append(strToInt(n))
+        for t in tokens:
+            if t not in signs:
+                if t[0] == "-":
+                    n = int(t[1:]) * -1
+                    stack.append(n)
+                else:
+                    stack.append(int(t))
             else:
-                r = stack.pop()
-                l = stack.pop()
-                if n == "+":
+                if t == "+":
+                    r = stack.pop()
+                    l = stack.pop()
                     stack.append(l + r)
-                elif n == "*":
+                if t == "*":
+                    r = stack.pop()
+                    l = stack.pop()
                     stack.append(l * r)
-                elif n == "-":
+                if t == "-":
+                    r = stack.pop()
+                    l = stack.pop()
                     stack.append(l - r)
-                elif n == "/":
-                    sign = 1
+                if t == "/":
+                    r = stack.pop()
+                    l = stack.pop()
+
                     if l * r < 0:
-                        sign = -1
-                    stack.append(abs(l) // abs(r) * sign) 
-        return stack[-1]
-
-
-
+                        stack.append(math.ceil(l / r))
+                    else:
+                        stack.append(math.floor(l / r))
+        return stack[0]

2d58d6988387670793dcab15fcb30d98f6783b0c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-29 06:27:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.6 MB (100.00%)

--- problems/0974-reorder-data-in-log-files/solution.py
+++ problems/0974-reorder-data-in-log-files/solution.py
@@ -1,10 +1,15 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        dLogs = [log for log in logs if log.split(" ")[1].isdigit()]
-        aLogs = [log for log in logs if log.split(" ")[1].isalpha()]
-        
-        print(aLogs)
-        print(dLogs)
-        
-        aLogs.sort(key = lambda x : (x.split(" ")[1:], x.split(" ")[0]))
-        return aLogs + dLogs
+        letters = []
+        digits = []
+
+        for l in logs:
+            if l[-1].isdigit():
+                digits.append(l)
+            else:
+                letters.append(l)
+
+        letters.sort(key=lambda x : (x.split(" ")[1:], x.split(" ")[0]))
+
+        return letters + digits
+

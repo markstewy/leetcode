9b4c81cf404e397509c94aa2d59cf633aa30cec0
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-29 17:39:41 +0000

    [LeetCode Sync] Runtime - 59 ms (84.72%), Memory - 17 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,42 +1,40 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
         
-        sCount, tCount = {}, {}
-
-        for c in t:
-            tCount[c] = tCount.get(c, 0) + 1
-        
-        matches = 0
-        needed = len(tCount.keys())
-
-        minLength = float("infinity")
+        tcount = {}
+        scount = {}
+        minLen = float("infinity")
         minl = -1
         minr = -1
 
+        for c in t:
+            tcount[c] = tcount.get(c, 0) + 1
+        
+        matches = 0
+        needed = len(tcount.keys())
+
         l = 0
         for r in range(len(s)):
-            # add r
             c = s[r]
-            sCount[c] = sCount.get(c, 0) + 1
-            if c in tCount and tCount[c] == sCount[c]:
+            scount[c] = scount.get(c, 0) + 1
+            if c in tcount and scount[c] == tcount[c]:
                 matches += 1
-            if matches >= needed:
-                if (r - l + 1) < minLength:
-                    minLength = r - l + 1
+            
+            while matches >= needed:
+                if r - l + 1 < minLen:
+                    minLen = r - l + 1
                     minl = l
                     minr = r
-
-            # while enough matches bring up l
-            while matches >= needed:
+                
                 c = s[l]
-                sCount[c] -= 1
-                if c in tCount and sCount[c] == tCount[c] - 1:
+                scount[c] -= 1
+                if c in tcount and scount[c] == tcount[c] - 1:
                     matches -= 1
-                l += 1
 
-                if matches >= needed:
-                    if (r - l + 1) < minLength:
-                        minLength = r - l + 1
-                        minl = l
-                        minr = r
+                l += 1
+        
         return s[minl : minr + 1]
+            
+

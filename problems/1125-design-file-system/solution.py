a2957b47014d054a6a4e1ba9e706be85f85f6b6a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-31 20:15:54 +0000

    [LeetCode Sync] Runtime - 47 ms (55.03%), Memory - 24.6 MB (100.00%)

--- problems/1125-design-file-system/solution.py
+++ problems/1125-design-file-system/solution.py
@@ -1,39 +1,37 @@
 class FileSystem:
 
     def __init__(self):
-        self.root = {"children": {}, "val": None}
-        
+        self.root = {} #key: sub name value: {"value":val, [subkeys...]}
 
     def createPath(self, path: str, value: int) -> bool:
-        path = path.lstrip("/").rstrip("/").split("/")
-        newDir = path.pop() #!
-        
         curr = self.root
-        for dir in path:
-            if dir in curr["children"]:
-                curr = curr["children"][dir]
+        path = path.lstrip("/").rstrip("/").split(("/"))
+        newDir = path.pop()
+        
+        for p in path:
+            if p in curr:
+                curr = curr[p]
             else:
                 return False
-        
-        if newDir in curr["children"]:
+        if newDir in curr:
             return False
-        else:
-            curr["children"][newDir] = {"val": value, "children": {}}
         
+        curr[newDir] = {}
+        curr[newDir]["value"] = value
         return True
-        
 
     def get(self, path: str) -> int:
-        path = path.lstrip("/").rstrip("/").split("/")
         curr = self.root
-
-        for dir in path:
-            if dir in curr["children"]:
-                curr = curr["children"][dir]
+        path = path.lstrip("/").rstrip("/").split(("/"))
+        
+        for p in path:
+            if p in curr:
+                curr = curr[p]
             else:
                 return -1
-        
-        return curr["val"]
+
+        return curr["value"]
+
         
 
 

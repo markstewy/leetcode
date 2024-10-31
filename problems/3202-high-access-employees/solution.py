16a051f41729b0dd09a7a4b6fdd2483f6d29ac94
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-31 20:39:20 +0000

    [LeetCode Sync] Runtime - 183 ms (16.79%), Memory - 16.8 MB (100.00%)

--- problems/3202-high-access-employees/solution.py
+++ problems/3202-high-access-employees/solution.py
@@ -1,31 +1,35 @@
 class Solution:
     def findHighAccessEmployees(self, access_times: List[List[str]]) -> List[str]:
-        access_times.sort(key=lambda x: x[1])
-        employees = collections.defaultdict(list)
+        # sliding window of 1 hour
 
-        for access in access_times:
-            name = access[0]
-            time = access[1]
-            employees[name].append(time)
-
-        ans = []
-        for name, times in employees.items():
-            if self.isHighFrequency(times):
-                ans.append(name)
+        # remove timed out
         
-        return ans
-    
-    def isHighFrequency(self, accessTimes):
-        if len(accessTimes) < 3:
-            return False
+        # add new (if 3 or more) add to the ans list
+        for i in range(len(access_times)):
+            access_times[i][1] = int(access_times[i][1])
+
+        access_times.sort(key=lambda x : x[1])
+        empCounter = collections.defaultdict(deque)
+        ans = []
+
         l = 0
-        r = 2
-        while r < len(accessTimes):
-            if int(accessTimes[r]) - int(accessTimes[l]) < 100:
-                return True
-            l += 1
-            r += 1
+        for r in range(len(access_times)):
+            # move up left if outside the new hour window
+            while access_times[r][1] - access_times[l][1] >= 100:
+                emp = access_times[l][0]
+                empCounter[emp].popleft()
+                l += 1
             
-        return False
+            # add the new emp access, if it is >= 3 add to high access list
+            emp = access_times[r][0]
+            time = access_times[r][1]
+            empCounter[emp].append(time)
+            
+            if len(empCounter[emp]) >= 3 and emp not in ans:
+                ans.append(emp)
+        return ans
+
+
+        
 
 

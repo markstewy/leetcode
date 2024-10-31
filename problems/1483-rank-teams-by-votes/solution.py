d8f4f1d379d0b56559fbea1ebfd99195a7440855
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-10-31 19:21:45 +0000

    [LeetCode Sync] Runtime - 13 ms (80.19%), Memory - 17 MB (100.00%)

--- problems/1483-rank-teams-by-votes/solution.py
+++ problems/1483-rank-teams-by-votes/solution.py
@@ -1,23 +1,20 @@
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
         candidates = {}
+
         for c in votes[0]:
             candidates[c] = [0] * len(votes[0])
 
-
         for v in votes:
             for i, c in enumerate(v):
-                candidates[c][i] += 1
+                candidates[c][i] -= 1
         
+        cArr = list(candidates.items())
 
-        candidates = list(candidates.items())
-        candidates.sort()
-        candidates.sort(key=lambda x : (x[1]), reverse=True)
-        print(candidates)
+        cArr.sort(key=lambda x : (x[1], x[0]))
 
         ans = ""
-
-        for c in candidates:
+        for c in cArr:
             ans += c[0]
         
         return ans

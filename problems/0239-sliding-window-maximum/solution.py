576f5194178696e4240240d96972961443aa8945
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-02 17:03:54 +0000

    [LeetCode Sync] Runtime - 254 ms (16.72%), Memory - 39 MB (10.89%)

--- problems/0239-sliding-window-maximum/solution.py
+++ problems/0239-sliding-window-maximum/solution.py
@@ -4,16 +4,19 @@ class Solution:
         ans = []
 
         for i, n in enumerate(nums):
-            while dq and n > dq[-1]["val"]:
+            while dq and dq[-1]["val"] < n:
                 dq.pop()
             
             dq.append({"val": n, "idx": i})
-
+        
             lastIdxInWindow = i - k + 1
             while dq and dq[0]["idx"] < lastIdxInWindow:
                 dq.popleft()
             
             if lastIdxInWindow >= 0:
                 ans.append(dq[0]["val"])
-            
+        
         return ans
+
+            
+

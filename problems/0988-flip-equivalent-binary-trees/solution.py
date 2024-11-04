ab6c6c320060e5b1ce7269deedd786e932b3aa80
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-04 21:49:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.5 MB (100.00%)

--- problems/0988-flip-equivalent-binary-trees/solution.py
+++ problems/0988-flip-equivalent-binary-trees/solution.py
@@ -10,10 +10,10 @@ class Solution:
         def helper(n1, n2):
             if n1 == None and n2 == None:
                 return True
+            
             if (n1 == None) != (n2 == None) or n1.val != n2.val:
                 return False
-
-            return ((helper(n1.left, n2.left) and helper(n1.right, n2.right)) or (helper(n1.left, n2.right) and helper(n1.right, n2.left)))
+            
+            return (helper(n1.left, n2.left) and helper(n1.right, n2.right)) or (helper(n1.left, n2.right) and helper(n1.right, n2.left)) 
         
         return helper(root1, root2)
-            

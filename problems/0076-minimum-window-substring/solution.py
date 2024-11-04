882a09fa9fe0383f676ab17aa5898e735ac5d576
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-04 01:05:28 +0000

    [LeetCode Sync] Runtime - 61 ms (76.98%), Memory - 16.9 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -4,35 +4,35 @@ class Solution:
             return ""
 
         scount, tcount = {}, {}
-
-        for c in t:
-            tcount[c] = tcount.get(c, 0) + 1
-        
-        matches = 0
-        needed = len(tcount.keys())
         minLen = float("infinity")
         minl, minr = -1, -1
 
+        for c in t:
+            tcount[c] = tcount.get(c, 0) + 1
+
+        needed = len(tcount.keys())
+        matches = 0
+
         l = 0
         for r in range(len(s)):
-            # add r
             c = s[r]
             scount[c] = scount.get(c, 0) + 1
             if c in tcount and scount[c] == tcount[c]:
                 matches += 1
-
-            # move up l
-                while matches >= needed:
-                    if r - l + 1 < minLen:
-                        minLen = r - l + 1
-                        minl = l
-                        minr = r
-                    
-                    c = s[l]
-                    scount[c] -= 1
-                    if c in tcount and scount[c] == tcount[c] - 1:
-                        matches -= 1
-                    l += 1
+            
+            while matches >= needed:
+                if r - l + 1 < minLen:
+                    minLen = r - l + 1
+                    minl = l
+                    minr = r
+                
+                c = s[l]
+                scount[c] -= 1
+                if c in tcount and scount[c] == tcount[c] - 1:
+                    matches -= 1
+                
+                l += 1
         
         return s[minl : minr + 1]
+
 

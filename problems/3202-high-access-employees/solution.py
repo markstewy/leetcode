cb6b68e74019663a809c3b3d145779b20edfef9d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-07 22:55:56 +0000

    [LeetCode Sync] Runtime - 191 ms (7.63%), Memory - 16.6 MB (100.00%)

--- problems/3202-high-access-employees/solution.py
+++ problems/3202-high-access-employees/solution.py
@@ -1,35 +1,28 @@
 class Solution:
     def findHighAccessEmployees(self, access_times: List[List[str]]) -> List[str]:
-        # sliding window of 1 hour
+        # sort by time and use a sliding window
+        # track employee access in a map
+        # if any have 3 or more access add them to the ans array
 
-        # remove timed out
-        
-        # add new (if 3 or more) add to the ans list
+        ans = set()
         for i in range(len(access_times)):
             access_times[i][1] = int(access_times[i][1])
-
+        
         access_times.sort(key=lambda x : x[1])
-        empCounter = collections.defaultdict(deque)
-        ans = []
 
+        emp = {}
         l = 0
         for r in range(len(access_times)):
-            # move up left if outside the new hour window
+            name = access_times[r][0]
+            emp[name] = emp.get(name, 0) + 1
+
             while access_times[r][1] - access_times[l][1] >= 100:
-                emp = access_times[l][0]
-                empCounter[emp].popleft()
+                name = access_times[l][0]
+                emp[name] -= 1
                 l += 1
             
-            # add the new emp access, if it is >= 3 add to high access list
-            emp = access_times[r][0]
-            time = access_times[r][1]
-            empCounter[emp].append(time)
-            
-            if len(empCounter[emp]) >= 3 and emp not in ans:
-                ans.append(emp)
-        return ans
-
-
+            for e, c in emp.items():
+                if c >= 3:
+                    ans.add(e)
         
-
-
+        return list(ans)

3f00b682b634b7f2e4392fc68adf5490e55363e3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-10 04:16:06 +0000

    [LeetCode Sync] Runtime - 148 ms (32.95%), Memory - 74.2 MB (5.59%)

--- problems/1023-time-based-key-value-store/solution.py
+++ problems/1023-time-based-key-value-store/solution.py
@@ -4,31 +4,29 @@ class TimeMap:
         self.store = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key].append({"time": timestamp, "val": value})
+        self.store[key].append([timestamp, value])
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.store or not self.store[key]:
+        if key not in self.store:
             return ""
-
+        maxPrev = [-float("infinity"), ""]
         values = self.store[key]
         l = 0
         r = len(values) - 1
-        maxprev = {"time": -1, "val": ""}
-         
+
         while l <= r:
             m = l + (r - l) // 2
 
-            if values[m]["time"] < timestamp:
-                if values[m]["time"] > maxprev["time"]:
-                    maxprev = values[m]
+            if values[m][0] < timestamp:
+                if values[m][0] > maxPrev[0]: # record the max prev
+                    maxPrev = values[m]
                 l = m + 1
-            elif values[m]["time"] > timestamp:
+            elif values[m][0] > timestamp:
                 r = m - 1
             else:
-                return values[m]["val"]
+                return values[m][1] # perfect match return val
         
-        return maxprev["val"]
-        
+        return maxPrev[1]
 
 
 # Your TimeMap object will be instantiated and called as such:

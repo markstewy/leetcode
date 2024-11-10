7804a81b1a549500c428405c744a40145a0455e8
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-10 21:30:42 +0000

    [LeetCode Sync] Runtime - 14 ms (10.63%), Memory - 16.9 MB (100.00%)

--- problems/1044-find-common-characters/solution.py
+++ problems/1044-find-common-characters/solution.py
@@ -1,30 +1,21 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        mainCount = Counter(words[0])
+        
+        mergedCount = Counter(words[0])
 
-        for word in words: # {"ch": n, "ch2": n2}
+        for word in words:
             wordCount = Counter(word)
-            commonKeys = wordCount.keys() and mainCount.keys()
-            mainCount = {
-                key: min(wordCount[key], mainCount[key])
+            commonKeys = wordCount.keys() and mergedCount.keys()
+
+            mergedCount = {
+                key: min(wordCount[key], mergedCount[key])
                 for key in commonKeys
-           }
+            }
+
 
         ans = []
-        for ch, n in mainCount.items():
+        for ch, n in mergedCount.items():
             for _ in range(n):
                 ans.append(ch)
-
+        
         return ans
-
-
-
-            
-                
-            
-            
-        
-
-        
-
-        

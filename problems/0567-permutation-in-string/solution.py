2d5c6a4ccf721c62a0d637aeb8f77be0e6f2f0ab
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-13 07:18:45 +0000

    [LeetCode Sync] Runtime - 29 ms (39.12%), Memory - 16.9 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
-            return False
-        
-
         s1count, s2count = {}, {}
 
         for c in s1:
             s1count[c] = s1count.get(c, 0) + 1
-
+        
         matches = 0
         needed = len(s1count.keys())
 
         l = 0
         for r in range(len(s2)):
-            # add r
+
             c = s2[r]
             s2count[c] = s2count.get(c, 0) + 1
             if c in s1count and s2count[c] == s1count[c]:
@@ -22,8 +18,7 @@ class Solution:
             if c in s1count and s2count[c] == s1count[c] + 1:
                 matches -= 1
             
-            # remove l
-            while r - l + 1 > len(s1):
+            if r - l + 1 > len(s1):
                 c = s2[l]
                 s2count[c] -= 1
                 if c in s1count and s2count[c] == s1count[c]:
@@ -34,4 +29,6 @@ class Solution:
             
             if matches == needed:
                 return True
+            
         return False
+

8b8bb7161dc249dc28dfa683949adabc89f9d824
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-13 06:37:22 +0000

    [LeetCode Sync] Runtime - 11 ms (25.02%), Memory - 23.2 MB (27.63%)

--- problems/0347-top-k-frequent-elements/solution.py
+++ problems/0347-top-k-frequent-elements/solution.py
@@ -5,17 +5,15 @@ class Solution:
         for n in nums:
             count[n] = count.get(n, 0) + 1
         
-        freqArr = [[] for _ in range(len(nums) + 1)]
+        frequency = [[] for _ in range(len(nums) + 1)]
 
         for n, c in count.items():
-            freqArr[c].append(n)
+            frequency[c].append(n)
         
         ans = []
-        for i in range(len(freqArr) - 1 , -1 , -1):
-            values = freqArr[i]
+        for i in range(len(frequency) - 1, -1, -1):
+            values = frequency[i]
             for v in values:
                 ans.append(v)
                 if len(ans) == k:
                     return ans
-        
-        return []

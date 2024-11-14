84745923ae107636745081d8f1ee7751415ef2c2
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-14 08:16:11 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 21.7 MB (100.00%)

--- problems/0155-min-stack/solution.py
+++ problems/0155-min-stack/solution.py
@@ -1,28 +1,23 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = [] # {"min": , "val":}
-        
+        self.stack = []
 
     def push(self, val: int) -> None:
-        mn = float("infinity")
-        if self.stack:
-            mn = min(val, self.stack[-1]["min"])
-        else:
-            mn = val
-        self.stack.append({"min": mn, "val": val})
+        mn = val
+        if self.stack and self.stack[-1]["min"] < val:
+            mn = self.stack[-1]["min"]
         
+        self.stack.append({"val": val, "min": mn})
 
     def pop(self) -> None:
         self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]["val"]
-        
 
     def getMin(self) -> int:
         return self.stack[-1]["min"]
-        
 
 
 # Your MinStack object will be instantiated and called as such:

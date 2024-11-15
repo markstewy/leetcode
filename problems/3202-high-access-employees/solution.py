a3a392d89100e6f05580b14f4954881ce1b25b25
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-15 05:07:47 +0000

    [LeetCode Sync] Runtime - 182 ms (22.14%), Memory - 16.7 MB (100.00%)

--- problems/3202-high-access-employees/solution.py
+++ problems/3202-high-access-employees/solution.py
@@ -1,28 +1,26 @@
 class Solution:
-    def findHighAccessEmployees(self, access_times: List[List[str]]) -> List[str]:
-        # sort by time and use a sliding window
-        # track employee access in a map
-        # if any have 3 or more access add them to the ans array
-
-        ans = set()
-        for i in range(len(access_times)):
-            access_times[i][1] = int(access_times[i][1])
+    def findHighAccessEmployees(self, times: List[List[str]]) -> List[str]:
         
-        access_times.sort(key=lambda x : x[1])
+        # sort by time
+        # two pointers logic: remove l if outside the window, add r and check if 3 or more
+        # use a set of names to avoid duplicats 
 
-        emp = {}
+        times.sort(key=lambda x: int(x[1]))
+
+        highAccess= set()
+        empCount = {}
         l = 0
-        for r in range(len(access_times)):
-            name = access_times[r][0]
-            emp[name] = emp.get(name, 0) + 1
+        for r in range(len(times)):
 
-            while access_times[r][1] - access_times[l][1] >= 100:
-                name = access_times[l][0]
-                emp[name] -= 1
+            while int(times[r][1]) - int(times[l][1]) >= 100:
+                name = times[l][0]
+                empCount[name] -= 1
                 l += 1
             
-            for e, c in emp.items():
-                if c >= 3:
-                    ans.add(e)
+            name = times[r][0]
+            empCount[name] = empCount.get(name, 0) + 1
+            if empCount[name] == 3:
+                highAccess.add(name)
         
-        return list(ans)
+        return list(highAccess)
+        

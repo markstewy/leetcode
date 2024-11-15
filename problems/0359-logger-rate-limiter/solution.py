c4dd2e93ed83bb4a9b8f82d09641cc2f8b21c3a0
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-15 21:29:14 +0000

    [LeetCode Sync] Runtime - 23 ms (10.34%), Memory - 24.2 MB (100.00%)

--- problems/0359-logger-rate-limiter/solution.py
+++ problems/0359-logger-rate-limiter/solution.py
@@ -1,16 +1,17 @@
 class Logger:
 
     def __init__(self):
-        self.store = {} # key: log value: timestamp
+        self.msgTimes = {}
         
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message not in self.store or timestamp - self.store[message] >= 10:
-            self.store[message] = timestamp
+        if message not in self.msgTimes or timestamp - self.msgTimes[message] >= 10:
+            self.msgTimes[message] = timestamp
             return True
         else:
             return False
-        
+
+            
 
 
 # Your Logger object will be instantiated and called as such:

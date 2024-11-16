c0f7097076950c7ca85e6b28bfd1edbe567689f4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-16 06:02:32 +0000

    [LeetCode Sync] Runtime - 3 ms (79.85%), Memory - 16.7 MB (100.00%)

--- problems/0013-roman-to-integer/solution.py
+++ problems/0013-roman-to-integer/solution.py
@@ -1,50 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        nums = {
+        numerals = {
             "I": 1,
-            "II": 2,
-            "III": 3,
-            "IV": 4,
             "V": 5,
-            "VI": 6,
-            "VII": 7,
-            "VIII": 8,
-            "IX": 9,
             "X": 10,
             "L": 50,
             "C": 100,
             "D": 500,
-            "M": 1000,
-            "XL": 40,
-            "XC": 90,
-            "CD": 400,
-            "CM": 900
+            "M": 1000
         }
 
-        subtotal = 0
-        total = 0
-        i = 0
-        while i < len(s):
-
-            four = s[i : i + 4] if i + 4 <= len(s) else ""
-            three = s[i : i + 3] if i + 3 <= len(s) else ""
-            two = s[i : i + 2] if i + 2 <= len(s) else ""
-            one = s[i : i + 1] if i + 1 <= len(s) else ""
-
-            if four in nums:
-                total += nums[four]
-                i += 4
-            elif three in nums:
-                total += nums[three]
-                i += 3
-            elif two in nums:
-                total += nums[two]
-                i += 2
-            elif one in nums:
-                total += nums[one]
-                i += 1
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        number = 0
+        for c in s:
+            number += numerals[c]
         
-        return total
-
-            
+        return number
 

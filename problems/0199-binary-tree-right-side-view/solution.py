e514023593c93ac35aa951584d892e711732e0a3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-16 06:11:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.7 MB (100.00%)

--- problems/0199-binary-tree-right-side-view/solution.py
+++ problems/0199-binary-tree-right-side-view/solution.py
@@ -6,18 +6,18 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        self.rightView = []
-        
-        # do aright traversal that tracks level
-        def helper(node, level):
-            if node == None:
-                return
-            if len(self.rightView) - 1 < level:
-                self.rightView.append(node.val)
+        self.levels = []
 
+        def helper(node, level):
+            if not node:
+                return
+            if not self.levels or level > len(self.levels) - 1:
+                self.levels.append(node.val)
+            
             helper(node.right, level + 1)
             helper(node.left, level + 1)
         
         helper(root, 0)
-        return self.rightView
+        return self.levels
+
 

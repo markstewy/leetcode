fe352345d5ffe2bc51167461b94d43da38fb05e3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-18 21:00:55 +0000

    [LeetCode Sync] Runtime - 198 ms (7.33%), Memory - 74.6 MB (5.59%)

--- problems/1023-time-based-key-value-store/solution.py
+++ problems/1023-time-based-key-value-store/solution.py
@@ -1,38 +1,46 @@
+class Entry:
+    def __init__(self, value, timestamp):
+        self.val = value
+        self.time = timestamp
+
 class TimeMap:
 
     def __init__(self):
         self.store = collections.defaultdict(list)
+        
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.store[key].append({"time": timestamp, "val": value})
+        self.store[key].append(Entry(value, timestamp))
 
-    def get(self, key: str, targetTime: int) -> str:
-        mxprev = {"time": -1, "val": ""}
-
+    def get(self, key: str, timestamp: int) -> str:
         if key not in self.store:
             return ""
         values = self.store[key]
 
         l = 0
         r = len(values) - 1
+
+
+        targetTime = timestamp
+        maxPrev = Entry("", -float("infinity"))
         while l <= r:
             m = l + (r - l) // 2
-            mtime = values[m]["time"]
 
-            if targetTime < mtime:
-                r = m - 1
-            elif mtime < targetTime:
-                if mtime > mxprev["time"]:
-                    mxprev = values[m]
+            if targetTime > values[m].time:
+                if maxPrev.time < values[m].time:
+                    maxPrev = values[m]
                 l = m + 1
+            elif targetTime < values[m].time:
+                r = m - 1
             else:
-                return values[m]["val"]
+                return values[m].val
         
-        return mxprev["val"]
+        return maxPrev.val
+
         
 
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
-# obj.set(key,value,targetTime)
+# obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)

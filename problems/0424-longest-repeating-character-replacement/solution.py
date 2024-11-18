7e4431d0ec7d3db4912d899523f53afba7f592a1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-18 19:24:42 +0000

    [LeetCode Sync] Runtime - 79 ms (76.12%), Memory - 16.6 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -1,8 +1,7 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        longest = 0
         scount = {}
-        maxLen = 0
-
         l = 0
         for r in range(len(s)):
             c = s[r]
@@ -13,6 +12,6 @@ class Solution:
                 scount[c] -= 1
                 l += 1
             
-            maxLen = max(maxLen, r - l + 1)
-        
-        return maxLen
+            longest = max(longest, r - l + 1)
+    
+        return longest

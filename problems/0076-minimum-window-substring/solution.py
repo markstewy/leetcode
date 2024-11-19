3e741454fcfa5b070b7e9043b4534bd7dfd74caf
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-19 19:15:47 +0000

    [LeetCode Sync] Runtime - 61 ms (76.98%), Memory - 17 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -4,24 +4,23 @@ class Solution:
             return ""
         
         minLen = float("infinity")
-        minl, minr = -1 , -1
+        minl, minr = -1, -1
         scount, tcount = {}, {}
 
         for c in t:
             tcount[c] = tcount.get(c, 0) + 1
         
-        needed = len(tcount.keys())
         matches = 0
+        needed = len(tcount.keys())
 
         l = 0
         for r in range(len(s)):
-            # add r and update matches
             c = s[r]
             scount[c] = scount.get(c, 0) + 1
+
             if c in tcount and scount[c] == tcount[c]:
                 matches += 1
-
-            # remove l and update matches *** update the minLen
+            
             while matches >= needed:
                 if r - l + 1 < minLen:
                     minLen = r - l + 1
@@ -32,8 +31,8 @@ class Solution:
                 scount[c] -= 1
                 if c in tcount and scount[c] == tcount[c] - 1:
                     matches -= 1
+
                 l += 1
         
         return s[minl : minr + 1]
 
-        # BigO: time O(n) lineanr, space O(n) linear

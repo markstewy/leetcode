a7aaec75feef6609d4b3b4f092ae33e697a8ccb0
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-19 01:41:49 +0000

    [LeetCode Sync] Runtime - 2 ms (15.03%), Memory - 17 MB (100.00%)

--- problems/0362-design-hit-counter/solution.py
+++ problems/0362-design-hit-counter/solution.py
@@ -4,16 +4,16 @@ class HitCounter:
         self.dq = deque()
 
     def hit(self, timestamp: int) -> None:
-        self.dq.append(timestamp)
-        
         while self.dq and self.dq[0] <= timestamp - 300:
             self.dq.popleft()
+        self.dq.append(timestamp)
+        
 
     def getHits(self, timestamp: int) -> int:
         while self.dq and self.dq[0] <= timestamp - 300:
             self.dq.popleft()
-        
         return len(self.dq)
+        
 
 
 # Your HitCounter object will be instantiated and called as such:

854d75ee7e6a87f1afbbb0b902a0729167f9802f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-21 17:32:24 +0000

    [LeetCode Sync] Runtime - 185 ms (5.14%), Memory - 45.3 MB (5.42%)

--- problems/0739-daily-temperatures/solution.py
+++ problems/0739-daily-temperatures/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temps: List[int]) -> List[int]:
-        stack = []
         ans = [0] * len(temps)
+        stack = []
 
-        for i, t in enumerate(temps):
-            while stack and t > stack[-1]["temp"]:
-                ans[stack[-1]["idx"]] = i - stack[-1]["idx"] # record number of days to get higher temp
+        for i , t in enumerate(temps):
+            while stack and stack[-1]["temp"] < t:
+                ans[stack[-1]["idx"]] = i - stack[-1]["idx"]
                 stack.pop()
+
             stack.append({"temp": t, "idx": i})
-            
         
         return ans
 
-        # BigO: O(n) linear time and space

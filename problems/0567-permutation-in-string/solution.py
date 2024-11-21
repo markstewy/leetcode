ba913521ba7b3f61867d842f4396face95426da3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-21 07:03:50 +0000

    [LeetCode Sync] Runtime - 27 ms (41.62%), Memory - 16.9 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
         s1count, s2count = {}, {}
 
         for c in s1:
@@ -10,22 +13,24 @@ class Solution:
 
         l = 0
         for r in range(len(s2)):
-
+            # add to s2count
             c = s2[r]
             s2count[c] = s2count.get(c, 0) + 1
             if c in s1count and s2count[c] == s1count[c]:
                 matches += 1
             if c in s1count and s2count[c] == s1count[c] + 1:
                 matches -= 1
-            
-            if r - l + 1 > len(s1):
+
+            # remove l if len == s1
+            while r - l + 1 > len(s1):
                 c = s2[l]
+                l += 1
                 s2count[c] -= 1
+
                 if c in s1count and s2count[c] == s1count[c]:
                     matches += 1
                 if c in s1count and s2count[c] == s1count[c] - 1:
                     matches -= 1
-                l += 1
             
             if matches == needed:
                 return True

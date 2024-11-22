bba6a873fc08a5d7aa0a6b8d50dd69ef47b9988a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-22 22:22:19 +0000

    [LeetCode Sync] Runtime - 9 ms (8.93%), Memory - 16.7 MB (100.00%)

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -5,26 +5,30 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
         dhead = ListNode()
         curr = dhead
         carry = 0
 
         while l1 or l2 or carry:
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+            total = l1val + l2val + carry
 
-            l1v = l1.val if l1 else 0
-            l2v = l2.val if l2 else 0
+            carry = total // 10
+            total = total % 10
 
-            total = (l1v + l2v + carry) % 10
-            carry = (l1v + l2v + carry) // 10
             curr.next = ListNode(total)
-            
+            curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            curr = curr.next
         
+        self.printList(dhead.next)
         return dhead.next
 
-
-
-
+    def printList(self, head):
+        s = ""
+        while head:
+            s += f"{head.val} -->"
+            head = head.next
+        
+        print(s)

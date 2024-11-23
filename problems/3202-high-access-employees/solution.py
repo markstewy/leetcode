8eb32f44bee511035b38db124f2f607c67604c8b
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-23 04:19:27 +0000

    [LeetCode Sync] Runtime - 180 ms (27.48%), Memory - 16.7 MB (100.00%)

--- problems/3202-high-access-employees/solution.py
+++ problems/3202-high-access-employees/solution.py
@@ -1,26 +1,20 @@
 class Solution:
-    def findHighAccessEmployees(self, times: List[List[str]]) -> List[str]:
-        
-        # sort by time
-        # two pointers logic: remove l if outside the window, add r and check if 3 or more
-        # use a set of names to avoid duplicats 
+    def findHighAccessEmployees(self, access_times: List[List[str]]) -> List[str]:
+        access_times.sort(key=lambda x : int(x[1]))
+        count = {}
+        hfe = set()
 
-        times.sort(key=lambda x: int(x[1]))
-
-        highAccess= set()
-        empCount = {}
         l = 0
-        for r in range(len(times)):
-
-            while int(times[r][1]) - int(times[l][1]) >= 100:
-                name = times[l][0]
-                empCount[name] -= 1
+        for r in range(len(access_times)):
+            while int(access_times[l][1]) <= int(access_times[r][1]) - 100:
+                emp = access_times[l][0]
+                count[emp] -= 1
                 l += 1
             
-            name = times[r][0]
-            empCount[name] = empCount.get(name, 0) + 1
-            if empCount[name] == 3:
-                highAccess.add(name)
+            emp = access_times[r][0]
+            count[emp] = count.get(emp, 0) + 1
+
+            if count[emp] >= 3:
+                hfe.add(emp)
         
-        return list(highAccess)
-        
+        return list(hfe)

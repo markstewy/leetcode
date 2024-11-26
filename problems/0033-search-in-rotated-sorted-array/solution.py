0fff1ab0451d2a8f34965f9bdc330e8f2c6adfad
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-26 05:01:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17 MB (100.00%)

--- problems/0033-search-in-rotated-sorted-array/solution.py
+++ problems/0033-search-in-rotated-sorted-array/solution.py
@@ -8,16 +8,17 @@ class Solution:
 
             if nums[m] == target:
                 return m
-            if nums[l] == target:
-                return l
             if nums[r] == target:
                 return r
+            if nums[l] == target:
+                return l
 
-            if nums[r] > nums[m]: # right side is ascending
+            if nums[m] < nums[r]: # right side is ascending
                 if nums[m] < target < nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
+
             else: # left side is ascending
                 if nums[l] < target < nums[m]:
                     r = m - 1
@@ -25,6 +26,3 @@ class Solution:
                     l = m + 1
         
         return -1
-                
-            
-

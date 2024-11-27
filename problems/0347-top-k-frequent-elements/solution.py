3cd4e1c1c4a1d65dcf91e7e8a1b87f8bf082a6ec
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-27 00:47:06 +0000

    [LeetCode Sync] Runtime - 15 ms (10.19%), Memory - 23.8 MB (27.50%)

--- problems/0347-top-k-frequent-elements/solution.py
+++ problems/0347-top-k-frequent-elements/solution.py
@@ -5,15 +5,20 @@ class Solution:
         for n in nums:
             count[n] = count.get(n, 0) + 1
 
-        sortedArr = [[] for _ in range(len(nums) + 1)]
+        countArr = [[] for _ in range(len(nums) + 1)]
 
-        for n, c in count.items():
-            sortedArr[c].append(n)
-        
+        for n, cnt in count.items():
+            countArr[cnt].append(n)
+
+
         ans = []
-        for i in range(len(sortedArr) - 1, -1, -1):
-            values = sortedArr[i]
+        for i in range(len(countArr) - 1, -1, -1):
+            values = countArr[i]
             for v in values:
                 ans.append(v)
                 if len(ans) == k:
                     return ans
+        
+        return []
+
+    

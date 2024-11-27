ec89c12e894971f55612645fe6e6bb49615d5c34
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-27 00:50:04 +0000

    [LeetCode Sync] Runtime - 5 ms (36.03%), Memory - 17.3 MB (100.00%)

--- problems/0036-valid-sudoku/solution.py
+++ problems/0036-valid-sudoku/solution.py
@@ -4,19 +4,20 @@ class Solution:
         cols = collections.defaultdict(set)
         sqs = collections.defaultdict(set)
 
+
         for r in range(len(board)):
             for c in range(len(board[0])):
                 v = board[r][c]
-
+                
                 if v == ".":
                     continue
                 
-                if v in rows[r] or v in cols[c] or v in sqs[(r//3,c//3)]:
+                if v in rows[r] or v in cols[c] or v in sqs[(r//3, c//3)]:
                     return False
                 
                 rows[r].add(v)
                 cols[c].add(v)
                 sqs[(r//3, c//3)].add(v)
-            
-        
+    
         return True
+                

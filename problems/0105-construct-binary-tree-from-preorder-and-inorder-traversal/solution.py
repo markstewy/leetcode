225e167d301a1098f809b46404f7635559e7d4d6
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-27 00:29:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (100.00%)

--- problems/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ problems/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -6,28 +6,31 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        
         inorderIdx = {}
+
         for i, n in enumerate(inorder):
             inorderIdx[n] = i
-
+        
         self.preIdx = 0
+        l = 0
+        r = len(preorder) - 1
 
         def dfs(l, r):
             if l > r:
                 return None
-                
+
             rootVal = preorder[self.preIdx]
             self.preIdx += 1
-            root = TreeNode(rootVal)
 
             partitionIdx = inorderIdx[rootVal]
-
+            
+            root = TreeNode(rootVal)
             root.left = dfs(l, partitionIdx - 1)
             root.right = dfs(partitionIdx + 1, r)
 
             return root
         
-        l = 0
-        r = len(preorder) - 1
         return dfs(l, r)
+
+
+            

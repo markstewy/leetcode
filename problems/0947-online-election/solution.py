4b00eb87a745399368c9601ce65ee549cd518584
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-28 03:05:02 +0000

    [LeetCode Sync] Runtime - 216 ms (24.54%), Memory - 23.6 MB (100.00%)

--- problems/0947-online-election/solution.py
+++ problems/0947-online-election/solution.py
@@ -2,44 +2,49 @@ class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
         self.times = times
+        self.maxVotes = -1
+        self.leader = None
+        self.count = {}
         self.leaders = []
-        count = {}
-        maxVotes = 0
-        leader = None
 
         for p in persons:
-            count[p] = count.get(p, 0) + 1
-            if count[p] >= maxVotes:
-                maxVotes = count[p]
-                leader = p
+            self.count[p] = self.count.get(p, 0) + 1
+
+            if self.count[p] >= self.maxVotes:
+                self.maxVotes = self.count[p]
+                self.leader = p
             
-            self.leaders.append(leader)
-        
+            self.leaders.append(self.leader)
 
-    def q(self, t: int) -> int:
-        idx = self.findNearestTimeIdx(t)
-        return self.leaders[idx]
-        
-
-    def findNearestTimeIdx(self, time):
-        values = self.times
+    def findTimeIdx(self, time):
         target = time
+        values = self.times
         l = 0
         r = len(values) - 1
-        closestPrev = -1
+        nearestPrev = -1
+
 
         while l <= r:
             m = l + (r - l) // 2
 
-            if values[m] < target:
-                closestPrev = m
-                l = m + 1
-            elif values[m] > target:
+            if values[m] > target:
                 r = m - 1
+            elif values[m] < target:
+                nearestPrev = m
+                l = m + 1
             else:
                 return m
         
-        return closestPrev
+        return nearestPrev
+
+
+
+    def q(self, t: int) -> int:
+        idx = self.findTimeIdx(t)
+        return self.leaders[idx]
+
+        
+
 
 # Your TopVotedCandidate object will be instantiated and called as such:
 # obj = TopVotedCandidate(persons, times)

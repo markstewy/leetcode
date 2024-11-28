49d3df3553cc17383d45a664383d27a2e92cfc55
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-28 19:29:17 +0000

    [LeetCode Sync] Runtime - 29 ms (39.12%), Memory - 17.2 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -4,36 +4,35 @@ class Solution:
             return False
 
         s1count, s2count = {}, {}
-
         for c in s1:
             s1count[c] = s1count.get(c, 0) + 1
         
         matches = 0
         needed = len(s1count.keys())
 
+        print(s1count)
+
         l = 0
         for r in range(len(s2)):
-            # add to s2count
+            if matches == needed:
+                return True
+
             c = s2[r]
             s2count[c] = s2count.get(c, 0) + 1
+
             if c in s1count and s2count[c] == s1count[c]:
                 matches += 1
             if c in s1count and s2count[c] == s1count[c] + 1:
                 matches -= 1
-
-            # remove l if len == s1
-            while r - l + 1 > len(s1):
+        
+            if r - l + 1 > len(s1):
                 c = s2[l]
-                l += 1
                 s2count[c] -= 1
 
                 if c in s1count and s2count[c] == s1count[c]:
                     matches += 1
                 if c in s1count and s2count[c] == s1count[c] - 1:
                     matches -= 1
-            
-            if matches == needed:
-                return True
-            
-        return False
-
+                l += 1
+        
+        return matches == needed

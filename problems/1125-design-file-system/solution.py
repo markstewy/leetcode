f1b5bd038be5e6ab6c10eb7baf1175c4dccfa549
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-30 07:03:14 +0000

    [LeetCode Sync] Runtime - 36 ms (70.93%), Memory - 24.9 MB (100.00%)

--- problems/1125-design-file-system/solution.py
+++ problems/1125-design-file-system/solution.py
@@ -1,25 +1,23 @@
 class FileSystem:
 
     def __init__(self):
-        self.store = {}
-        self.store[""] = -1
+        self.paths = {}
+        self.paths[""] = -1
         
 
     def createPath(self, path: str, value: int) -> bool:
-        paths = path.split("/")
-        parent = "/".join(paths[:-1])
-        newDir = paths[-1]
-        if parent not in self.store or path in self.store:
+        parent = "/".join(path.split("/")[:-1])
+        print(f"path: {path}, parent:{parent}")
+        
+        if parent not in self.paths or path in self.paths:
             return False
         else:
-            self.store[path] = value
+            self.paths[path] = value
             return True
+        
 
     def get(self, path: str) -> int:
-        if path not in self.store:
-            return -1
-        else:
-            return self.store[path]
+        return self.paths[path] if path in self.paths else -1
         
 
 

d62637f68ee1e8fad667305d366d9d4381d8ff19
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-11-30 05:09:41 +0000

    [LeetCode Sync] Runtime - 123 ms (15.69%), Memory - 36.2 MB (99.78%)

--- problems/2483-task-scheduler-ii/solution.py
+++ problems/2483-task-scheduler-ii/solution.py
@@ -1,15 +1,14 @@
 class Solution:
     def taskSchedulerII(self, tasks: List[int], space: int) -> int:
-        
-        wait = {} # key: task val: next date task can be done
-
         day = 0
+        wait = {}
+
         for t in tasks:
             day += 1
-
             if t in wait:
-                day = max(wait[t], day)
-
+                day = max(day, wait[t])
+            
             wait[t] = day + space + 1
-        
+
         return day
+

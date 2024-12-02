f75c63a10ff7c7a8b396d608b268410661a6ba56
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-02 20:51:51 +0000

    [LeetCode Sync] Runtime - 175 ms (40.73%), Memory - 23.6 MB (100.00%)

--- problems/0947-online-election/solution.py
+++ problems/0947-online-election/solution.py
@@ -1,48 +1,43 @@
 class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
+        self.persons = persons
         self.times = times
-        self.maxVotes = -1
-        self.leader = None
-        self.count = {}
         self.leaders = []
 
+        count = {}
+        leader = None
+        leaderCount = -1
+
         for p in persons:
-            self.count[p] = self.count.get(p, 0) + 1
-
-            if self.count[p] >= self.maxVotes:
-                self.maxVotes = self.count[p]
-                self.leader = p
+            count[p] = count.get(p, 0) + 1
+            if count[p] >= leaderCount:
+                leader = p
+                leaderCount = count[p]
             
-            self.leaders.append(self.leader)
+            self.leaders.append(leader)
 
-    def findTimeIdx(self, time):
-        target = time
+
+    def q(self, t: int) -> int:
+        target = t
         values = self.times
         l = 0
         r = len(values) - 1
-        nearestPrev = -1
-
+        closestPrev = -1
 
         while l <= r:
             m = l + (r - l) // 2
 
-            if values[m] > target:
-                r = m - 1
-            elif values[m] < target:
-                nearestPrev = m
+            if values[m] < target:
+                closestPrev = m
                 l = m + 1
+            elif values[m] > target:
+                r = m - 1
             else:
-                return m
+                closestPrev = m
+                break
         
-        return nearestPrev
-
-
-
-    def q(self, t: int) -> int:
-        idx = self.findTimeIdx(t)
-        return self.leaders[idx]
-
+        return self.leaders[closestPrev]
         
 
 

e8ce12bbb21841efc8634c3634a7ddc99736329f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-04 04:25:21 +0000

    [LeetCode Sync] Runtime - 11 ms (31.38%), Memory - 23.1 MB (17.08%)

--- problems/0125-valid-palindrome/solution.py
+++ problems/0125-valid-palindrome/solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         sArr = []
-
         for c in s:
             if c.isdigit() or c.isalpha():
                 sArr.append(c.lower())
-        
+    
         return sArr == sArr[::-1]

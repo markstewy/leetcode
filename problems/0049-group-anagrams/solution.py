dcd37a8c62604f936a1a5457082cc5bc3704ace5
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-04 04:10:19 +0000

    [LeetCode Sync] Runtime - 19 ms (19.38%), Memory - 22.2 MB (45.15%)

--- problems/0049-group-anagrams/solution.py
+++ problems/0049-group-anagrams/solution.py
@@ -1,12 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = collections.defaultdict(list)
+        
+        count = collections.defaultdict(list)
 
         for s in strs:
             ukey = [0] * 26
             for c in s:
                 ukey[ord(c) - ord("a")] += 1
+            count[tuple(ukey)].append(s)
         
-            anagrams[tuple(ukey)].append(s)
+        return list(count.values())
         
-        return list(anagrams.values())
+

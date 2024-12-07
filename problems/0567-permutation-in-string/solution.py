74a55100718b16173c62254e3dd3c292ca593205
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-07 22:31:30 +0000

    [LeetCode Sync] Runtime - 29 ms (39.12%), Memory - 17.6 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,34 +1,36 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1Count, s2Count = {}, {}
+        s1count, s2count = {}, {}
 
         for c in s1:
-            s1Count[c] = s1Count.get(c, 0) + 1
+            s1count[c] = s1count.get(c, 0) + 1
         
         matches = 0
-        needed = len(s1Count.keys())
+        needed = len(s1count.keys())
 
         l = 0
         for r in range(len(s2)):
             if matches == needed:
                 return True
-                        
-            c = s2[r]
-            s2Count[c] = s2Count.get(c, 0) + 1
 
-            if c in s1Count and s2Count[c] == s1Count[c]:
+            c = s2[r]
+            s2count[c] = s2count.get(c, 0) + 1
+
+            if c in s1count and s2count[c] == s1count[c]:
                 matches += 1
-            if c in s1Count and s2Count[c] == s1Count[c] + 1:
+            if c in s1count and s2count[c] == s1count[c] + 1:
                 matches -= 1
-        
+            
             if r - l + 1 > len(s1):
                 c = s2[l]
-                s2Count[c] -= 1
+                s2count[c] -= 1
 
-                if c in s1Count and s2Count[c] == s1Count[c]:
+                if c in s1count and s2count[c] == s1count[c]:
                     matches += 1
-                if c in s1Count and s2Count[c] == s1Count[c] - 1:
+                if c in s1count and s2count[c] == s1count[c] - 1:
                     matches -= 1
+
                 l += 1
-    
+        
         return matches == needed
+

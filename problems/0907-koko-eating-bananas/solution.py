02a65cca3e65b8de3ac2233e318cee9c3984a67e
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-08 05:40:12 +0000

    [LeetCode Sync] Runtime - 163 ms (76.89%), Memory - 18.6 MB (100.00%)

--- problems/0907-koko-eating-bananas/solution.py
+++ problems/0907-koko-eating-bananas/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l = 1
-        r = max(piles)
-        slowestCompletion = float("infinity")
+        lk = 1
+        rk = max(piles)
+        completedSpeed = -1
 
-        while l <= r:
-            m = l + (r - l) // 2
+        while lk <= rk:
+            mk = lk + (rk - lk) // 2
 
             hrs = 0
             for p in piles:
-                hrs += math.ceil(p / m)
-            
-            if hrs <= h: # was fast enough (save)
-                slowestCompletion = m
-                r = m - 1
-            elif hrs > h: # took too long, go faster
-                l = m + 1
-        
-        return slowestCompletion
+                hrs += math.ceil(p / mk)
+
+            if hrs <= h: # fast enough try slower
+                completedSpeed = mk
+                rk = mk - 1
+            elif hrs > h: # too slow, try faster
+                lk = mk + 1
+
+        return completedSpeed

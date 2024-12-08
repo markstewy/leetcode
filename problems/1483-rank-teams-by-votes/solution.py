e006d88d2b40eba21d31c102e0e7c035443d6813
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-08 05:24:36 +0000

    [LeetCode Sync] Runtime - 9 ms (97.08%), Memory - 17.9 MB (100.00%)

--- problems/1483-rank-teams-by-votes/solution.py
+++ problems/1483-rank-teams-by-votes/solution.py
@@ -5,17 +5,17 @@ class Solution:
         for c in votes[0]:
             candidates[c] = [0] * len(votes[0])
 
-        
         for v in votes:
             for i, c in enumerate(v):
                 candidates[c][i] -= 1
         
         candidatesArr = list(candidates.items())
+
         candidatesArr.sort(key=lambda x : (x[1], x[0]))
+
 
         ans = ""
         for c in candidatesArr:
             ans += c[0]
         
         return ans
-

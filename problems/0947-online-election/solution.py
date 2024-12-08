64255a6d9c2bfde7db4bf60027df44d9f68c530e
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-08 04:57:06 +0000

    [LeetCode Sync] Runtime - 207 ms (28.98%), Memory - 23.5 MB (100.00%)

--- problems/0947-online-election/solution.py
+++ problems/0947-online-election/solution.py
@@ -1,43 +1,42 @@
 class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
-        self.persons = persons
         self.times = times
         self.leaders = []
-
-        count = {}
+        leaderCount = {}
         leader = None
-        leaderCount = -1
+        maxCount = -1
 
         for p in persons:
-            count[p] = count.get(p, 0) + 1
-            if count[p] >= leaderCount:
+            leaderCount[p] = leaderCount.get(p, 0) + 1
+            if leaderCount[p] >= maxCount:
+                maxCount = leaderCount[p]
                 leader = p
-                leaderCount = count[p]
-            
             self.leaders.append(leader)
 
-
-    def q(self, t: int) -> int:
-        target = t
+    def getClosestPrevIdx(self, timestamp):
+        target = timestamp
         values = self.times
+
         l = 0
         r = len(values) - 1
-        closestPrev = -1
+        nearestPrev = -1
 
         while l <= r:
             m = l + (r - l) // 2
 
             if values[m] < target:
-                closestPrev = m
+                nearestPrev = m
                 l = m + 1
             elif values[m] > target:
                 r = m - 1
             else:
-                closestPrev = m
-                break
+                return m
         
-        return self.leaders[closestPrev]
+        return nearestPrev
+
+    def q(self, t: int) -> int:
+        return self.leaders[self.getClosestPrevIdx(t)]
         
 
 

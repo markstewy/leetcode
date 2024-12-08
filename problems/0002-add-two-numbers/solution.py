20c23393115f0c7e522af81785e0b8c113660ffb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-08 23:33:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.4 MB (100.00%)

--- problems/0002-add-two-numbers/solution.py
+++ problems/0002-add-two-numbers/solution.py
@@ -12,12 +12,12 @@ class Solution:
         while l1 or l2 or carry:
             l1val = l1.val if l1 else 0
             l2val = l2.val if l2 else 0
-            total = carry + l1val + l2val
+
+            total = l1val + l2val + carry
 
             carry = total // 10
-            remainder = total % 10
-            curr.next = ListNode(remainder)
-            
+            total %= 10
+            curr.next = ListNode(total)
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None

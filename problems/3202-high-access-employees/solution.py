af6f512bb7f43a48bd472c631dd9306d8bca5dd3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2024-12-09 00:11:21 +0000

    [LeetCode Sync] Runtime - 182 ms (22.14%), Memory - 17.5 MB (100.00%)

--- problems/3202-high-access-employees/solution.py
+++ problems/3202-high-access-employees/solution.py
@@ -1,21 +1,18 @@
 class Solution:
-    def findHighAccessEmployees(self, access_times: List[List[str]]) -> List[str]:
+    def findHighAccessEmployees(self, atimes: List[List[str]]) -> List[str]:
+        atimes.sort(key=lambda x : int(x[1]))
+        emps = collections.defaultdict(deque)
+        ans = set()
+        
+        for a in atimes:
+            name = a[0]
+            time = a[1]
+            emps[name].append(int(time))
 
-        access_times.sort(key=lambda x : int(x[1]))
-        count = {}
-        empSet = set()
+            while emps[name][0] <= int(time) - 100:
+                emps[name].popleft()
+            if len(emps[name]) >= 3:
+                ans.add(name)
+        
+        return list(ans)
 
-        l = 0
-        for r in range(len(access_times)):
-            while int(access_times[r][1]) - int(access_times[l][1]) >= 100:
-                name = access_times[l][0]
-                count[name] -= 1
-                l += 1
-            
-            name = access_times[r][0]
-            count[name] = count.get(name, 0) + 1
-            if count[name] >= 3:
-                empSet.add(name)
-
-        return list(empSet)
-            

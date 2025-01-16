ead593091923b8140f68750221eaa8af7ae9c4af
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-16 20:48:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- problems/0021-merge-two-sorted-lists/solution.py
+++ problems/0021-merge-two-sorted-lists/solution.py
@@ -8,21 +8,21 @@ class Solution:
         dhead = ListNode()
         curr = dhead
 
-        while list1 or list2:
-            if not list1:
-                curr.next = list2
-                break
-            if not list2:
-                curr.next = list1
-                break
-
+        while list1 and list2:
             if list1.val < list2.val:
                 curr.next = list1
+                curr = curr.next
                 list1 = list1.next
             else:
                 curr.next = list2
+                curr = curr.next
                 list2 = list2.next
-            curr = curr.next
+        
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
 
-        return dhead.next
+
+        return dhead.next 
 

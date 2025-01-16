8647563f6bcfa75046dfaad3b69c2bf47b8c3f61
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-16 03:19:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- problems/0100-same-tree/solution.py
+++ problems/0100-same-tree/solution.py
@@ -6,13 +6,20 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-
-        def isSameTree(r1, r2):
-            if not r1 and not r2:
+        
+        def isSameTree(root1, root2):
+            if not root1 and not root2:
                 return True
-            if r1 and r2 and r1.val == r2.val:
-                return isSameTree(r1.left, r2.left) and isSameTree(r1.right, r2.right)
-            else:
+
+            if root1 and not root2 or root2 and not root1 or root1.val != root2.val:
                 return False
+            
+            return isSameTree(root1.left, root2.left) and isSameTree(root1.right, root2.right)
         
         return isSameTree(p, q)
+
+
+            
+
+        
+

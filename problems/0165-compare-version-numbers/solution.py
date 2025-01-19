6687f4d3d7592e0b8cc44bb9781acefe26e49e95
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-19 18:50:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- problems/0165-compare-version-numbers/solution.py
+++ problems/0165-compare-version-numbers/solution.py
@@ -1,15 +1,27 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1, v2 = version1.split("."), version2.split(".")
-        
-        length = max(len(v1), len(v2))
-        
-        for i in range(length):
-            v1num = int(v1[i].lstrip("0")) if i < len(v1) and v1[i].lstrip("0") != '' else 0
-            v2num = int(v2[i].lstrip("0")) if i < len(v2) and v2[i].lstrip("0") != '' else 0
-            
-            if v1num > v2num:
-                return 1
-            elif v1num < v2num:
+        v1 = deque([int(n) for n in version1.split(".")])
+        v2 = deque([int(n) for n in version2.split(".")])
+
+        while v1 and v1[-1] == 0:
+            v1.pop()
+        while v2 and v2[-1] == 0:
+            v2.pop()
+
+
+        while v1 and v2:
+            if v1[0] < v2[0]:
                 return -1
-        return 0
+            elif v1[0] > v2[0]:
+                return 1
+            else:
+                v1.popleft()
+                v2.popleft()
+        
+        if v1 and not v2:
+            return 1
+        if v2 and not v1:
+            return -1
+        if not v2 and not v1:
+            return 0
+

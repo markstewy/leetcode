fe1f163755b54f4473a02a563ffc931968a77fb4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-22 18:24:21 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- problems/0988-flip-equivalent-binary-trees/solution.py
+++ problems/0988-flip-equivalent-binary-trees/solution.py
@@ -7,13 +7,14 @@
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         
-        def helper(r1, r2):
-            if not r1 and not r2:
+        def helper(root1, root2):
+            if root1 == None and root2 == None:
                 return True
-            if r1 and r2 and r1.val == r2.val:
-                return (helper(r1.left, r2.left) and helper(r1.right, r2.right)) or (helper(r1.left, r2.right) and helper(r1.right, r2.left))
-            else:
+            
+            if root1 and not root2 or root2 and not root1 or root1.val != root2.val:
                 return False
             
-
+            return (helper(root1.left, root2.right) and helper(root1.right, root2.left)) or (helper(root1.right, root2.right) and helper(root1.left, root2.left))
+        
         return helper(root1, root2)
+

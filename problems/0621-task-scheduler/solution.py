6f291550a6bcf7f8208d645b48081c6ba35b45fe
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-22 18:04:51 +0000

    [LeetCode Sync] Runtime - 158 ms (44.41%), Memory - 19.3 MB (100.00%)

--- problems/0621-task-scheduler/solution.py
+++ problems/0621-task-scheduler/solution.py
@@ -1,27 +1,37 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        tCount = [-count for count in Counter(tasks).values()]
-        heapq.heapify(tCount)
+        taskCounts = [-count for count in Counter(tasks).values()] # maxheap
+        taskCounts.sort(reverse=True)
         
-        waitq = deque() # (interval, count)
-        interval = 0
+        time = 0
+        heapq.heapify(taskCounts)
+        dq = deque()
+        
+        while taskCounts or dq:
+            time += 1
 
-        while tCount or waitq:
-            interval = interval + 1
-
-            while waitq and interval - waitq[0][0] > n:
-                count = waitq.popleft()[1]
-                heapq.heappush(tCount, count)
+            # add back all items from dq if wait is over
+            while dq and dq[0][0] < time:
+                heapq.heappush(taskCounts, dq.popleft()[1])
             
-            if tCount:
-                count = heapq.heappop(tCount)
-                if count < -1:
-                    waitq.append((interval, count + 1))
+            # execute the current task with the max count
+            if not taskCounts:
+                continue
+            taskCount = heapq.heappop(taskCounts) + 1
+
+            # if still counts left on that task, put back in the dq
+            if taskCount:
+                dq.append((time + n, taskCount))
         
-        return interval
+        return time
 
 
-        
-        
+            # for each time increment move add back all from timeout that are done waiting
+            # operate on the largest in the maxheap
+            # if it still has counts left move to timeout queue
+            
+            # move to queue
 
-        return 0
+
+
+

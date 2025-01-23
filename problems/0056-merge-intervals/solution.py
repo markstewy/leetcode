880fd8b41e54dcf216d8b02483154f2f5b1e080d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-23 23:01:11 +0000

    [LeetCode Sync] Runtime - 17 ms (6.85%), Memory - 20.9 MB (100.00%)

--- problems/0056-merge-intervals/solution.py
+++ problems/0056-merge-intervals/solution.py
@@ -1,24 +1,25 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        
-        def isOverlap(n1, n2):
-            l1, r1 = n1[0], n1[1]
-            l2, r2 = n2[0], n2[1]
+        if not intervals:
+            return []
 
-            if l1 <= l2 <= r1 or l1 <= r2 <= r1 or l2 <= l1 <= r2 or l2 <= r1 <= r2:
-                return True
-            return False
-        
-        def merge(n1, n2):
-            return [min(n1[0], n2[0]), max(n1[1], n2[1])]
-
-        merged = [intervals[0]]
-
-        for i in intervals:
-            if isOverlap(merged[-1], i):
-                merged[-1] = merge(i, merged[-1])
+        ans = [intervals[0]]
+        for interval in intervals:
+            if self.isOverlap(interval, ans[-1]):
+                ans[-1] = self.mergeInts(interval, ans[-1])
             else:
-                merged.append(i)
-        
-        return merged
+                ans.append(interval)
+
+        return ans
+
+    def mergeInts(self, int1, int2):
+        return [min(int1[0], int2[0]), max(int1[1], int2[1])]
+
+    def isOverlap(self, int1, int2) -> bool:
+        l1, r1, l2, r2 = int1[0], int1[1], int2[0], int2[1]
+
+        if l1 <= l2 <= r1 or l1 <= r2 <= r1 or l2 <= l1 <= r2 or l2 <= r1 <= r2:
+            return True
+        else:
+            return False

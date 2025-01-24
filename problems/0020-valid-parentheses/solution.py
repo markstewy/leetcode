421a0312a8d9d21c52f49a855b2cccc49b3d0129
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-24 23:34:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- problems/0020-valid-parentheses/solution.py
+++ problems/0020-valid-parentheses/solution.py
@@ -5,14 +5,16 @@ class Solution:
             "[": "]",
             "{": "}"
         }
+
         stack = []
 
         for c in s:
             if c in openToClose:
                 stack.append(openToClose[c])
-            elif stack and c == stack[-1]:
-                stack.pop()
             else:
-                return False
-            
+                if stack and stack[-1] == c:
+                    stack.pop()
+                else:
+                    return False
+        
         return not stack

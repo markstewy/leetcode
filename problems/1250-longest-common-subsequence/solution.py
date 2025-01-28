66cd5a790991e344361a70e9081d5bae89bc87c3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-28 20:05:50 +0000

    [LeetCode Sync] Runtime - 419 ms (35.25%), Memory - 42.7 MB (82.36%)

--- problems/1250-longest-common-subsequence/solution.py
+++ problems/1250-longest-common-subsequence/solution.py
@@ -1,10 +1,28 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        matrix = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
         for r, ch1 in enumerate(text1):
             for c, ch2 in enumerate(text2):
                 if ch1 == ch2:
-                    dp[r + 1][c + 1] = 1 + dp[r][c]
+                    matrix[r + 1][c + 1] = 1 + matrix[r][c]
                 else:
-                    dp[r + 1][c + 1] = max(dp[r][c + 1], dp[r + 1][c])
-        return dp[-1][-1]
+                    matrix[r + 1][c + 1] = max(matrix[r][c + 1], matrix[r + 1][c])
+        return matrix[-1][-1]
+
+
+# add extra zero to beginning of each row/col
+# if row char == col char then diagonally equals increment current by 1
+# else diagonally equals max of values to right or down
+
+#       | u   b   m   r   a   p   g
+#     --|------------------------------
+#     e | 0   0   0   0   0   0   0   0
+#     z | 0   0   0   0   0   0   0   0
+#     u | 0   0   0   0   0   0   0   0
+#     p | 0   1   1   1   1   1   1   1
+#     k | 0   1   1   1   1   1   2   2
+#     r | 0   1   1   1   1   1   2   2
+#       | 0   1   1   1   2   2   2   2
+
+
+# answer is "ur" or 2

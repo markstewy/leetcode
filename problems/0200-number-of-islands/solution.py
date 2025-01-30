bf30bc85054ded6dc5fcfe64e4f389acfcf4f5bb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-30 22:19:21 +0000

    [LeetCode Sync] Runtime - 248 ms (46.42%), Memory - 20 MB (99.99%)

--- problems/0200-number-of-islands/solution.py
+++ problems/0200-number-of-islands/solution.py
@@ -1,33 +1,27 @@
-class Solution:        
+class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        xMaxIdx = len(grid[0]) - 1
-        yMaxIdx = len(grid) - 1
-        
-        def isLand(x, y):
-            # out of bounds
-            if x < 0 or x > xMaxIdx or y < 0 or y > yMaxIdx:
-                return False
-            if grid[y][x] == '1':
-                return True
+        self.grid = grid
+        count = 0
+
+        def removeIsland(r, c):
+            if r >= len(self.grid) or r < 0:
+                return
+            if c >= len(self.grid[0]) or c < 0:
+                return
+            if self.grid[r][c] == "1":
+                self.grid[r][c] = "0"
+                removeIsland(r + 1, c)
+                removeIsland(r - 1, c)
+                removeIsland(r, c + 1)
+                removeIsland(r, c - 1)
             else:
-                return False
-    
-        def deleteIsland(x, y):
-            # don't over out of bounds, recursive delete all adjacent land '1'
-            if isLand(x, y):
-                grid[y][x] = '0'
-                deleteIsland(x + 1, y)
-                deleteIsland(x - 1, y)
-                deleteIsland(x, y + 1)
-                deleteIsland(x, y - 1)
-                
+                return
         
-        counter = 0
-        # if there is an island, increment the counter and then delete the island
-        for (iy, y) in enumerate(grid):
-            print(iy)
-            for (ix, x) in enumerate(grid[iy]):
-                if grid[iy][ix] == '1':
-                    counter = counter + 1
-                    deleteIsland(ix, iy)
-        return counter
+        for r in range(len(self.grid)):
+            for c in range(len(self.grid[0])):
+                if self.grid[r][c] == "1":
+                    count += 1
+                    removeIsland(r, c)
+    
+        return count
+

86be5e2323a8a1c01e222bac9734684104851653
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-31 20:25:50 +0000

    [LeetCode Sync] Runtime - 5 ms (32.53%), Memory - 21.9 MB (99.99%)

--- problems/0890-lemonade-change/solution.py
+++ problems/0890-lemonade-change/solution.py
@@ -1,45 +1,26 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        solution = []
-        cash = [0, 0, 0]
+        fives = 0
+        tens = 0
+
         for b in bills:
-            ans = self.hasChange(b, cash)
-            if not ans[0]:
+            if b == 5:
+                fives += 1
+            if b == 10:
+                tens += 1
+
+            changeDue = b - 5
+
+            while changeDue >= 10 and tens:
+                    changeDue -= 10
+                    tens -= 1
+            while changeDue >= 5 and fives:
+                    changeDue -= 5
+                    fives -= 1
+            
+            if changeDue:
                 return False
-            else:
-                cash = ans[1]
-                print(cash)
         
         return True
 
-
-    def hasChange(self, pmt: int, cash: List[int]) -> tuple:
-        changeAmt: int = pmt - 5
-
-        if pmt == 5:
-            cash[0] += 1
-        if pmt == 10:
-            cash[1] += 1
-        if pmt == 20:
-            cash[2] += 1
-
-        priorCash = cash
-        while changeAmt > 0:
-            progress = False
-            if changeAmt >= 20 and cash[2] > 0:
-                changeAmt -= 20
-                cash[2] -= 1
-                progress = True
-            if changeAmt >= 10 and cash[1] > 0:
-                changeAmt -= 10
-                cash[1] -= 1
-                progress = True
-            if changeAmt >= 5 and cash[0] > 0:
-                changeAmt -= 5
-                cash[0] -= 1
-                progress = True
-            if not progress:
-                return((False, [])) # no change solultion found
-
-        return (True, cash)
 

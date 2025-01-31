51e28d44897614636a70a493d2c32303752aa0bb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-31 21:33:11 +0000

    [LeetCode Sync] Runtime - 91 ms (9.38%), Memory - 21.8 MB (100.00%)

--- problems/1107-minimum-swaps-to-group-all-1s-together/solution.py
+++ problems/1107-minimum-swaps-to-group-all-1s-together/solution.py
@@ -1,16 +1,19 @@
 class Solution:
-    def minSwaps(self, nums: List[int]) -> int:
-        total_ones = nums.count(1)
-        window_ones = max_window_ones = 0
-
+    def minSwaps(self, data: List[int]) -> int:
+        windowSize = sum(data)
+        maxCount = 0
+        count = 0
+        
         l = 0
-        for r in range(len(nums)):
-            window_ones += nums[r]
-            if r - l + 1 > total_ones:
-                window_ones -= nums[l]
+        for r in range(len(data)):
+            if r - l + 1 > windowSize:
+                count -= data[l]
                 l += 1
-            max_window_ones = max(max_window_ones, window_ones)
-
-        return total_ones - max_window_ones
             
+            count += data[r]
+            maxCount = max(maxCount, count)
+        
+        return windowSize - maxCount
+            
+                
 

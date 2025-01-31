10d334ec2aa198e93bcecd1d568a07649dbd4603
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-01-31 21:24:31 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.9 MB (100.00%)

--- problems/0110-balanced-binary-tree/solution.py
+++ problems/0110-balanced-binary-tree/solution.py
@@ -8,17 +8,17 @@ class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         self.isBalanced = True
 
-        def helper(node):
-            if not node:
+        def helper(root):
+            if not root:
                 return 0
-            
-            l = helper(node.left)
-            r = helper(node.right)
+
+            l = helper(root.left)
+            r = helper(root.right)
+
             if abs(l - r) > 1:
                 self.isBalanced = False
             
-            return 1 + max(l, r)
-
-            
+            return max(l, r) + 1
+        
         helper(root)
         return self.isBalanced

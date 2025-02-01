7d6d9d2821a411ffb3e22396b5b1c5ceb2a5950d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-01 14:43:29 +0000

    [LeetCode Sync] Runtime - 3 ms (63.58%), Memory - 18 MB (100.00%)

---
+++ problems/3736-find-valid-pair-of-adjacent-digits-in-string/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findValidPair(self, s: str) -> str:
+        count = Counter(s)
+
+        for i in range(1, len(s)):
+            if s[i] != s[i - 1] and count[s[i]] == int(s[i]) and count[s[i - 1]] == int(s[i - 1]):
+                ans = s[i - 1] + s[i]
+                return ans
+
+        return ""
+            
+            

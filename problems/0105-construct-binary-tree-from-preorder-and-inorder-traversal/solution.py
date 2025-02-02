df7c3f37838681597db5af15d8782beeb415e892
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-02 03:25:08 +0000

    [LeetCode Sync] Runtime - 96 ms (10.12%), Memory - 89.4 MB (38.84%)

--- problems/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
+++ problems/0105-construct-binary-tree-from-preorder-and-inorder-traversal/solution.py
@@ -8,13 +8,10 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder:
             return
-        
+
         root = TreeNode(preorder[0])
         m = inorder.index(preorder[0])
 
-        # pass the correct sub arrays in
-        # in preorder remove rot by starting from index 1 instead of 0
-        # in inorder remove root by not includeing the mid (don't +1 so the subarray doesn't capture it)
         root.left = self.buildTree(preorder[1:m+1], inorder[:m])
         root.right = self.buildTree(preorder[m+1:], inorder[m+1:])
         return root

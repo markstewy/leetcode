50de749324d73ac3d5cd426f4f685c42ab4565f6
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-03 21:16:03 +0000

    [LeetCode Sync] Runtime - 181 ms (14.89%), Memory - 18 MB (100.00%)

--- problems/0424-longest-repeating-character-replacement/solution.py
+++ problems/0424-longest-repeating-character-replacement/solution.py
@@ -5,16 +5,13 @@ class Solution:
 
         l = 0
         for r in range(len(s)):
-            c = s[r]
-            count[c] = count.get(c, 0) + 1
+            count[s[r]] = count.get(s[r], 0) + 1
 
-            while (r - l + 1 - max(count.values())) > k:
-                c = s[l]
-                count[c] -= 1
+            while r - l + 1 - max(count.values()) > k:
+                count[s[l]] -= 1
                 l += 1
-
-            maxLen = max(r - l + 1, maxLen)
-        
+            
+            maxLen = max(maxLen, r - l + 1)
+    
         return maxLen
-
 

840471bcb1b9ef4a5c84f2f3ce6b32bb4828c921
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-05 06:27:16 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- problems/0118-pascals-triangle/solution.py
+++ problems/0118-pascals-triangle/solution.py
@@ -1,18 +1,26 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        ans = [[1]]
+        tree = []
+        for i in range(1, numRows + 1):
+            tree.append([1] * i)
+        
+        for r in range(len(tree)):
+            for c in range(len(tree[r])):
+                if r == 0:
+                    tree[r][0] = 1
+                    continue
+                if c == 0:
+                    tree[r][c] = tree[r - 1][0]
+                elif c == len(tree[r]) - 1:
+                    tree[r][c] = tree[r - 1][-1]
+                else:
+                    tree[r][c] = tree[r - 1][c] + tree[r - 1][c - 1]
+        
+        return tree
+                
+            
+
+                
 
 
-        while len(ans) < numRows:
-            ans.append([0] * (len(ans[-1]) + 1))
-
-            currRow = ans[-1]
-            priorRow = ans[-2]
-            currRow[0] = priorRow[0]
-            currRow[-1] = priorRow[-1]
-
-            for i in range(1, len(currRow) - 1):
-                currRow[i] = priorRow[i] + priorRow[i - 1]
-
-        return ans            
 

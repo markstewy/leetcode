f2c63eef003b967affb8242be4d2b3a9219b2f8c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-06 23:33:22 +0000

    [LeetCode Sync] Runtime - 4 ms (48.12%), Memory - 19.9 MB (99.98%)

--- problems/0210-course-schedule-ii/solution.py
+++ problems/0210-course-schedule-ii/solution.py
@@ -1,31 +1,33 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prmap = collections.defaultdict(list)
-        for crs, pr in prerequisites:
-            prmap[crs].append(pr)
+        preMap = collections.defaultdict(list)
 
-        
-        completed, circular = set(), set()
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+
+        circular = set() 
+        completed = set()
         completedOrder = []
 
-        def dfs(crs):
+        def canComplete(crs):
             if crs in completed:
                 return True
             if crs in circular:
                 return False
-            
+
             circular.add(crs)
-            for pr in prmap[crs]:
-                if dfs(pr) == False:
+            for pr in preMap[crs]:
+                if canComplete(pr) == False:
                     return False
             circular.remove(crs)
-
+                
             completed.add(crs)
             completedOrder.append(crs)
             return True
-        
-        for i in range(numCourses):
-            if dfs(i) == False:
+    
+        for crs in range(numCourses):
+            if canComplete(crs) == False:
                 return []
-            
+        
         return completedOrder

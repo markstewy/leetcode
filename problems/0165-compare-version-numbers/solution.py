52bda7c31f6a5370dfbfa4a67fcaeb702bbcb536
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-07 21:50:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

--- problems/0165-compare-version-numbers/solution.py
+++ problems/0165-compare-version-numbers/solution.py
@@ -4,18 +4,12 @@ class Solution:
         v2 = deque([int(n) for n in version2.split(".")])
 
         while v1 or v2:
-            v1n = v1[0] if v1 else 0
-            v2n = v2[0] if v2 else 0
-            
-            if v1n > v2n:
-                return 1
-            if v1n < v2n:
+            v1Val = v1.popleft() if v1 else 0
+            v2Val = v2.popleft() if v2 else 0
+
+            if v1Val < v2Val:
                 return -1
-            if v1:
-                v1.popleft()
-            if v2:
-                v2.popleft()
-        
+            if v1Val > v2Val:
+                return 1
+                
         return 0
-
-

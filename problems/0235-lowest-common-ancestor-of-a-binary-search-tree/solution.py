c4a70857e1518f6563477b5371726a3e36c5fe99
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-09 02:03:54 +0000

    [LeetCode Sync] Runtime - 50 ms (99.00%), Memory - 21 MB (99.97%)

--- problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
+++ problems/0235-lowest-common-ancestor-of-a-binary-search-tree/solution.py
@@ -9,20 +9,18 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.l = min(p.val, q.val)
         self.r = max(p.val, q.val)
-        self.lca = None
+        self.lca = root
 
         def helper(root):
-            if not root:
-                return
             if self.l <= root.val <= self.r:
                 self.lca = root
                 return
+            
             if root.val < self.l:
                 helper(root.right)
             if root.val > self.r:
                 helper(root.left)
-
+        
         helper(root)
         return self.lca
-
 

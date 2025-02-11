519b93e639cefb176bddcbea459e51fa69ff60cd
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-11 04:56:03 +0000

    [LeetCode Sync] Runtime - 99 ms (57.39%), Memory - 19.2 MB (100.00%)

--- problems/0621-task-scheduler/solution.py
+++ problems/0621-task-scheduler/solution.py
@@ -1,26 +1,27 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # execute the highest freq tasks first
-        # use a que to set aside a task during it's gap time
-        maxheap = [-v for v in Counter(tasks).values()]
-        heapq.heapify(maxheap)
-        timeout = deque() #[time, taskcount]
-        time = 0
+        taskCounts = [-c for c in Counter(tasks).values()]
+        heapq.heapify(taskCounts) # maxheap of task (task with highest count has priority)
+        timeout = deque() # (time, count)
 
-        while maxheap or timeout:
+        time = 0
+        while taskCounts or timeout:
             time += 1
 
             while timeout and timeout[0][0] < time:
-                taskCount = timeout.popleft()[1]
-                heapq.heappush(maxheap, taskCount)
+                heapq.heappush(taskCounts, timeout.popleft()[1])
             
-            if not maxheap:
+            if taskCounts:
+                taskCount = heapq.heappop(taskCounts) + 1
+                if taskCount != 0:
+                    timeout.append((time + n, taskCount))
+            if timeout and not taskCounts:
                 time = timeout[0][0]
-            else:
-                task = heapq.heappop(maxheap) + 1
-                if task < 0:
-                    timeout.append([time + n, task])
         
         return time
             
+
+
+        
+
 

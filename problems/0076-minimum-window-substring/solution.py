03b9a1eef97f6d155e7eb8be7bb940545dcfa128
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-14 17:31:29 +0000

    [LeetCode Sync] Runtime - 64 ms (65.80%), Memory - 18.3 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,11 +1,13 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
+        if len(s) < len(t):
             return ""
 
-        minl, minr = 0, float("infinity")
         tcount = Counter(t)
         scount = {}
+        lmin, rmin = 0, float("infinity")
+        minLen = float("infinity")
+
         needed = len(tcount.keys())
         matches = 0
 
@@ -13,25 +15,26 @@ class Solution:
         for r in range(len(s)):
             c = s[r]
             scount[c] = scount.get(c, 0) + 1
-
+            
             if c in tcount and tcount[c] == scount[c]:
                 matches += 1
-            
+        
             while matches >= needed:
-                if (r - l) < (minr - minl):
-                    print("hit")
-                    minr = r
-                    minl = l
-                    print(f"minl: {minl}.   minr: {minr}")
+                if (r - l + 1) < minLen:
+                    minLen = r - l + 1
+                    lmin = l
+                    rmin = r
+
                 c = s[l]
+                l += 1
                 scount[c] -= 1
 
                 if c in tcount and tcount[c] == scount[c] + 1:
                     matches -= 1
-                l += 1
-
-        if minr == float("infinity"):
-            return ""
-        return s[minl: minr + 1] 
         
+        if rmin == float("infinity"):
+            return ""
+        return s[lmin : rmin + 1]
 
+
+            

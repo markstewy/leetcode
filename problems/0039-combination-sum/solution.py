0d677af4ad5d5a0949302a48140a7e8d7e9468ee
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-17 22:53:22 +0000

    [LeetCode Sync] Runtime - 9 ms (53.50%), Memory - 18 MB (100.00%)

--- problems/0039-combination-sum/solution.py
+++ problems/0039-combination-sum/solution.py
@@ -2,18 +2,14 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
 
-        def helper(i, sub, total):
-            if i >= len(candidates) or total > target:
-                return
+        def dfs(i, sub, total):
+            if total > target: return
             if total == target:
                 ans.append(sub.copy())
                 return
-            
-            helper(i + 1, sub, total)
-            
-            sub.append(candidates[i])
-            helper(i, sub, total + candidates[i])
-            sub.pop()
-        
-        helper(0, [], 0)
+            for j in range(i, len(candidates)):
+                sub.append(candidates[j])
+                dfs(j, sub, total + candidates[j])
+                sub.pop()
+        dfs(0, [], 0)
         return ans

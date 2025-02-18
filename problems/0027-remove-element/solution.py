637d7f0a2ae9a4624f7c03687de8a93f060ec794
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-18 07:35:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- problems/0027-remove-element/solution.py
+++ problems/0027-remove-element/solution.py
@@ -1,15 +1,15 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        i = 0
-        k = i
+        l = 0
+        r = 0
 
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
-                k += 1
+        while r < len(nums):
+            if nums[r] != val:
+                nums[l] = nums[r]
+                l += 1
+            r += 1
         
-        return k
+        return l
 
 
-
-
+            

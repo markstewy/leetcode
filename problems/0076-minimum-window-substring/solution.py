db9540501be4b4e7d84a9efae4aeddda1aaa550d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-18 07:43:59 +0000

    [LeetCode Sync] Runtime - 67 ms (60.80%), Memory - 18.5 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,11 +1,11 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(s) < len(t):
+        if len(t) > len(s):
             return ""
 
         tcount = Counter(t)
         scount = {}
-        lmin, rmin = 0, float("infinity")
+        minl, minr = None, None
         minLen = float("infinity")
 
         needed = len(tcount.keys())
@@ -15,15 +15,14 @@ class Solution:
         for r in range(len(s)):
             c = s[r]
             scount[c] = scount.get(c, 0) + 1
-            
             if c in tcount and tcount[c] == scount[c]:
                 matches += 1
-        
+            
             while matches >= needed:
-                if (r - l + 1) < minLen:
+                if r - l + 1 < minLen:
                     minLen = r - l + 1
-                    lmin = l
-                    rmin = r
+                    minl = l
+                    minr = r
 
                 c = s[l]
                 l += 1
@@ -32,9 +31,7 @@ class Solution:
                 if c in tcount and tcount[c] == scount[c] + 1:
                     matches -= 1
         
-        if rmin == float("infinity"):
+        if minl == None:
             return ""
-        return s[lmin : rmin + 1]
-
-
-            
+        return s[minl : minr + 1]
+                

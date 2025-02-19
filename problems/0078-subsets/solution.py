72d455436c79aea2eea85401530637962c27b5c3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-19 23:36:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (100.00%)

--- problems/0078-subsets/solution.py
+++ problems/0078-subsets/solution.py
@@ -1,18 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
-        self.nums = nums
+        ans = []
 
         def helper(sub, i):
-            if i == len(self.nums):
-                self.ans.append(sub.copy())
+            if i >= len(nums):
+                ans.append(sub.copy())
                 return
             
             helper(sub, i + 1)
-            sub.append(self.nums[i])
+            sub.append(nums[i])
             helper(sub, i + 1)
             sub.pop()
         
         helper([], 0)
-        return self.ans
+        return ans
 

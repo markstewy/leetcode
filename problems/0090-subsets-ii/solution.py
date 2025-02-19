d0ccb9c146b9ffc652221f1492c22f71686e07b4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-19 23:58:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

---
+++ problems/0090-subsets-ii/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+
+        def helper(sub, i):
+            if i >= len(nums):
+                ans.append(sub.copy())
+                return
+            
+            sub.append(nums[i])
+            helper(sub, i + 1)
+            sub.pop()
+
+            while i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                i += 1
+            
+            helper(sub, i + 1)
+        
+        helper([], 0)
+        return ans

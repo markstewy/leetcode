9f5ca84186a3de8eda745688ee3c2bfd12142c77
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-20 05:07:33 +0000

    [LeetCode Sync] Runtime - 3 ms (74.66%), Memory - 19.9 MB (99.95%)

--- problems/0210-course-schedule-ii/solution.py
+++ problems/0210-course-schedule-ii/solution.py
@@ -1,34 +1,32 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        self.prs = collections.defaultdict(list)
+        prs = collections.defaultdict(list)
         for crs, pr in prerequisites:
-            self.prs[crs].append(pr)
+            prs[crs].append(pr)
         
-        self.order = []
-        self.orderSet = set()
-        self.visited = set()
+        order = []
+        completed = set()
+        visiting = set()
 
-        def isCompleteable(crs):
-            if crs in self.visited:
+        def helper(crs):
+            if crs in visiting:
                 return False
-            if crs in self.orderSet:
-            # if crs not in self.prs or crs in self.orderSet:
+            if crs in completed:
                 return True
             
-            self.visited.add(crs)
-            for pr in self.prs[crs]:
-                if isCompleteable(pr) == False:
+            visiting.add(crs)
+            for pr in prs[crs]:
+                if helper(pr) == False:
                     return False
-            self.visited.remove(crs)
-
-            self.orderSet.add(crs)
-            self.order.append(crs)
-            return True
+            visiting.remove(crs)
             
+            completed.add(crs)
+            order.append(crs)
+            return True    
         
         for crs in range(numCourses):
-            if isCompleteable(crs) == False:
+            if helper(crs) == False:
                 return []
-        return self.order
             
+        return order
             

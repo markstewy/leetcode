93845f22d0c1092aca39047f2ecade6ab0c3e952
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-21 05:50:23 +0000

    [LeetCode Sync] Runtime - 134 ms (37.21%), Memory - 78.4 MB (27.83%)

--- problems/0146-lru-cache/solution.py
+++ problems/0146-lru-cache/solution.py
@@ -1,63 +1,56 @@
 class Node:
-    def __init__(self, val, key):
-        self.key = key
+    def __init__(self, val=None, key=None):
         self.val = val
+        self.key = key
         self.next = None
         self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.l = Node(-1, "")
-        self.r = Node(-1, "")
+        self.l = Node()
+        self.r = Node()
         self.l.next = self.r
         self.r.prev = self.l
 
         self.nodeMap = {}
-
+        self.capacity = capacity
+        
     def get(self, key: int) -> int:
         if key in self.nodeMap:
-            self.moveToTop(key)
+            node = self.cut(key)
+            print(node.val)
+            self.insertFront(node)
             return self.nodeMap[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.nodeMap:
-            self.moveToTop(key)
-            self.nodeMap[key].val = value
-        else:
-            # create node
+        if key not in self.nodeMap:
             self.nodeMap[key] = Node(value, key)
-            node = self.nodeMap[key]
-            # append to top of list
-            node.prev = self.r.prev
-            node.next = self.r
-            self.r.prev.next = node
-            self.r.prev = node
-        while len(self.nodeMap.keys()) > self.capacity:
-            self.delete(self.l.next.key)
-
-
-    def delete(self, key) -> None:
-        node = self.nodeMap[key]
-        node.next.prev = node.prev
-        node.prev.next = node.next
-        del self.nodeMap[key]
+            self.insertFront(self.nodeMap[key])
+        else:
+            self.nodeMap[key].val = value
+            self.insertFront(self.cut(key))
+        
+        if len(self.nodeMap.keys()) > self.capacity:
+            delNode = self.cut(self.l.next.key)
+            del self.nodeMap[delNode.key]
+        
+    def cut(self, key: int) -> Node:
+        prev = self.nodeMap[key].prev
+        next = self.nodeMap[key].next
+        prev.next = next
+        next.prev = prev
+        
+        return self.nodeMap[key]
     
-    def moveToTop(self, key) -> None:
-        node = self.nodeMap[key]
-        # remove the node
-        node.next.prev = node.prev
-        node.prev.next = node.next
-
-        # add to top of list
-        node.prev = self.r.prev
+    def insertFront(self, node: Node) -> None:
+        prev = self.r.prev
+        node.prev = prev
         node.next = self.r
-        self.r.prev.next = node
+        
+        prev.next = node
         self.r.prev = node
-
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)

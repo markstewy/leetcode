0e3b4c98de3ad50254c3e3791c13dd9b6d192424
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-21 06:19:53 +0000

    [LeetCode Sync] Runtime - 127 ms (15.55%), Memory - 34.9 MB (15.55%)

--- problems/0739-daily-temperatures/solution.py
+++ problems/0739-daily-temperatures/solution.py
@@ -1,14 +1,17 @@
 class Solution:
     def dailyTemperatures(self, temps: List[int]) -> List[int]:
         ans = [0] * len(temps)
-        minheap = [] # (temp, idx)
+        stack = []
 
         for i, t in enumerate(temps):
-            while minheap and t > minheap[0][0]:
-                idx = minheap[0][1]
-                ans[idx] = i - idx
-                heapq.heappop(minheap)
             
-            heapq.heappush(minheap, (t, i))
-        
+            while stack and t > stack[-1][0]:
+                priorIdx = stack[-1][1]
+                days = i - priorIdx
+                ans[priorIdx] = days
+                stack.pop()
+            
+            stack.append([t, i])
+
         return ans
+

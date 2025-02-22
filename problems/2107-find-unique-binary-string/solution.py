af8ac9bceee0aa3aaa9eb11394fa048953cebfda
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-22 03:38:11 +0000

    [LeetCode Sync] Runtime - 1858 ms (5.07%), Memory - 17.8 MB (100.00%)

--- problems/2107-find-unique-binary-string/solution.py
+++ problems/2107-find-unique-binary-string/solution.py
@@ -1,33 +1,25 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        # find permutation of length k from bnums
-        self.ans = None
-        
-        numSet = set(nums)
         k = len(nums)
-        permCount = {"0": k, "1": k}
+        count = {"0": k, "1": k}
+        numSet = set(nums)
         perm = []
+        self.ans = None
 
         def helper():
-            if self.ans:
-                return
             if len(perm) == k:
                 if "".join(perm) not in numSet:
                     self.ans = "".join(perm)
                 return
             
-            for d in permCount:
-                if permCount[d] > 0:
-                    permCount[d] -= 1
+            for d in count:
+                if count[d] > 0:
                     perm.append(d)
+                    count[d] -= 1
                     helper()
+                    count[d] += 1
                     perm.pop()
-                    permCount[d] += 1
-        
         helper()
         return self.ans
             
-                
-
-
 

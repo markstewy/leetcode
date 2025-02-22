a9b35a06c965ddb34028f9f86e8b58c499ae65f3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-22 01:22:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- problems/0046-permutations/solution.py
+++ problems/0046-permutations/solution.py
@@ -1,21 +1,20 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
+        perm = []
+        numSet = set()
 
-        def helper(sub, subSet):
-            if len(sub) == len(nums):
-                ans.append(sub.copy())
+        def helper():
+            if len(perm) == len(nums):
+                ans.append(perm.copy())
                 return
             
             for n in nums:
-                if n not in subSet:
-                    sub.append(n)
-                    subSet.add(n)
-                    helper(sub, subSet)
-                    subSet.remove(n)
-                    sub.pop()
-        
-        helper([], set())
+                if n not in numSet:
+                    numSet.add(n)
+                    perm.append(n)
+                    helper()
+                    perm.pop()
+                    numSet.remove(n)
+        helper()
         return ans
-            
-                

c89b3140985ea7fc94700f2e53723c61cf14f5f1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-22 03:44:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22 MB (99.99%)

--- problems/0890-lemonade-change/solution.py
+++ problems/0890-lemonade-change/solution.py
@@ -1,23 +1,25 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        register = {
-            5: 0,
-            10: 0,
-            20: 0
-        }
+        fives = 0
+        tens = 0
 
-        for b in bills:
-            register[b] += 1
-            change = b - 5
+        for bill in bills:
+            if bill == 5:
+                fives += 1
+            if bill == 10:
+                tens += 1
+            
+            change = bill - 5
 
-            while register[10] and change >= 10:
+            while change >= 10 and tens:
                 change -= 10
-                register[10] -= 1
-            while register[5] and change >= 5:
+                tens -= 1
+            while change >= 5 and fives:
                 change -= 5
-                register[5] -= 1
+                fives -= 1
             
             if change:
                 return False
-        
         return True
+        
+            

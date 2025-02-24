59b6d64844712673f6c04e3aad16d933fac2e5b0
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-24 04:34:30 +0000

    [LeetCode Sync] Runtime - 3 ms (79.85%), Memory - 18 MB (100.00%)

--- problems/0013-roman-to-integer/solution.py
+++ problems/0013-roman-to-integer/solution.py
@@ -1,6 +1,6 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        numerals = {
+        nums = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -9,13 +9,13 @@ class Solution:
             "D": 500,
             "M": 1000,
         }
-        
+            
         s = s.replace("IV", "IIII").replace("IX", "VIIII")
         s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
         s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
 
         total = 0
         for c in s:
-            total += numerals[c]
+            total += nums[c]
         
         return total

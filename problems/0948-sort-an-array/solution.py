5d61563d410688b3e7374dab0a0cfc937768ec4d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-02-27 19:40:27 +0000

    [LeetCode Sync] Runtime - 716 ms (30.61%), Memory - 25.5 MB (99.95%)

--- problems/0948-sort-an-array/solution.py
+++ problems/0948-sort-an-array/solution.py
@@ -1,32 +1,32 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-
-        def merge(lArr, rArr):
-            k = max(len(lArr), len(rArr))
-            ans = []
-            l, r = 0, 0
-
-            while l < len(lArr) or r < len(rArr):
-                lv = lArr[l] if l < len(lArr) else float("infinity")
-                rv = rArr[r] if r < len(rArr) else float("infinity")
-
-                if lv < rv:
-                    ans.append(lv)
-                    l += 1
-                else:
-                    ans.append(rv)
-                    r += 1
-            return ans
-
         
+        def merge(lArr, rArr):
+            ans = []
+            ldq = deque(lArr)
+            rdq = deque(rArr)
+            
+            while ldq or rdq:
+                if not ldq:
+                    ans.append(rdq.popleft())
+                elif not rdq:
+                    ans.append(ldq.popleft())
+                elif ldq[0] <= rdq[0]:
+                    ans.append(ldq.popleft())
+                else:
+                    ans.append(rdq.popleft())
+            
+            return ans   
+
+
         def mergeSort(arr):
             if len(arr) == 1:
                 return arr
-
+            
             m = len(arr) // 2
             l = mergeSort(arr[:m])
             r = mergeSort(arr[m:])
 
             return merge(l, r)
-        
+    
         return mergeSort(nums)

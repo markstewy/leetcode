5a62b3a682191a65ef54661e33c6afc0b743f256
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-02 03:33:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- problems/0017-letter-combinations-of-a-phone-number/solution.py
+++ problems/0017-letter-combinations-of-a-phone-number/solution.py
@@ -1,40 +1,36 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        letters = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
+        dial = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
         }
-        self.ans = []
-        self.digits = list(digits)
 
-        if not self.digits:
-            return self.ans
+        ans = []
+        
+        if digits == "":
+            return ans
 
         def helper(sub, i):
-            if len(sub) == len(self.digits):
-                if sub:
-                    self.ans.append("".join(sub))
-                    return
-            
-            for c in letters[self.digits[i]]:
+            if i == len(digits):
+                ans.append("".join(sub))
+                return
+
+            d = digits[i]
+            for c in dial[d]:
                 sub.append(c)
                 helper(sub, i + 1)
                 sub.pop()
         
         helper([], 0)
-        return self.ans
-            
-            
+        return ans
 
-            
-                
-        
 
-        
+
+
 

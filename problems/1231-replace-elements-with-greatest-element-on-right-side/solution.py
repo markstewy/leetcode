22c914fe2a9dd5fdb1323c04508b77326242f1f5
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-02 03:42:42 +0000

    [LeetCode Sync] Runtime - 63 ms (5.49%), Memory - 19.2 MB (100.00%)

--- problems/1231-replace-elements-with-greatest-element-on-right-side/solution.py
+++ problems/1231-replace-elements-with-greatest-element-on-right-side/solution.py
@@ -1,12 +1,17 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
-        rtl = [-1]
+        rtlMax = []
+        mx = 0
+        for i in range(len(arr) - 1, -1, -1):
+            if i == len(arr) - 1:
+                rtlMax.append(-1)
+                continue
 
-        mx = -1
-        for i in range(len(arr) - 2, -1, -1):
-            mx = max(arr[i + 1], mx)
-            rtl.append(mx)
-        rtl.reverse()
-        
-        return rtl
-      
+            mx = max(mx, arr[i + 1])
+            rtlMax.append(mx)
+        rtlMax.reverse()
+
+        return rtlMax
+
+            
+            

c5c06fcb8a815951d68ba0aceada422ee27b5912
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-03 00:36:09 +0000

    [LeetCode Sync] Runtime - 63 ms (5.89%), Memory - 17.8 MB (100.00%)

--- problems/0572-subtree-of-another-tree/solution.py
+++ problems/0572-subtree-of-another-tree/solution.py
@@ -6,33 +6,30 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        self.subRoot = subRoot
         self.isSub = False
 
-        def isSameTree(r1, r2) -> bool:
-            if not r1 and not r2:
-                return True
+        def helper(root, subRoot):
+            if not root:
+                return
             
-            r1val = r1.val if r1 else None
-            r2val = r2.val if r2 else None
-        
+            if isSameTree(root, subRoot):
+                self.isSub = True
+                return
+            
+            helper(root.left, subRoot)
+            helper(root.right, subRoot)
+
+        def isSameTree(root1, root2):
+            r1val = root1.val if root1 else None
+            r2val = root2.val if root2 else None
+
             if r1val != r2val:
                 return False
             
-            return isSameTree(r1.left, r2.left) and isSameTree(r1.right, r2.right)
-        
-        def bfs(root) -> None:
-            if not root:
-                return
-            if root.val == self.subRoot.val:
-                if isSameTree(root, self.subRoot):
-                    self.isSub = True
-                    return
+            if not root1 and not root2:
+                return True
             
-            bfs(root.left)
-            bfs(root.right)
+            return isSameTree(root1.left, root2.left) and isSameTree(root1.right, root2.right)
         
-        bfs(root)
+        helper(root, subRoot)
         return self.isSub
-            
-            

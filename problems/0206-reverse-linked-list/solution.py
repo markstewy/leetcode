ae798eb4be2c53449d34a69c0829ff6a72dfc5f2
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-03 00:44:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.7 MB (100.00%)

--- problems/0206-reverse-linked-list/solution.py
+++ problems/0206-reverse-linked-list/solution.py
@@ -5,16 +5,17 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
         
-        while head:
-            temp = head.next
-            head.next = prev
-            prev = head
-            head = temp
+        curr = head
+        prev = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
         
         return prev
 
 
-            
-            

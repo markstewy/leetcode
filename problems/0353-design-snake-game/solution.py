3686ed4cfa02ddfd13db62922aa786e60ff615ef
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-03 16:00:50 +0000

    [LeetCode Sync] Runtime - 641 ms (5.01%), Memory - 799.7 MB (13.72%)

--- problems/0353-design-snake-game/solution.py
+++ problems/0353-design-snake-game/solution.py
@@ -1,54 +1,60 @@
 class SnakeGame:
 
     def __init__(self, width: int, height: int, food: List[List[int]]):
-        self.board = [[""] * width for _ in range(height)]
-        self. width = width
         self.height = height
+        self.width = width
         self.food = deque(food)
-        fr, fc = self.food.popleft()
-        self.board[fr][fc] = "f"
-        
+
         self.snake = deque()
-        self.snake.append([0, 0])
-        self.board[0][0] = "s"    
-        self.eatenCount = 0    
-        
+        self.snake.append((0, 0))
+
+        self.grid = [[""] * self.width for _ in range(self.height)]
+        self.grid[0][0] = "s"
+
+        fr, fc = self.food.popleft()
+        self.grid[fr][fc] = "f"
 
     def move(self, direction: str) -> int:
-        # print(self.snake)
-        r = self.snake[-1][0]
-        c = self.snake[-1][1]
+        print(self.snake)
+        dirs = {
+            "U": (-1, 0), 
+            "D": (1, 0), 
+            "L": (0, -1), 
+            "R": (0, 1)
+            }
+        r, c = self.snake[-1]
+        nr, nc = dirs[direction]
+        nr += r
+        nc += c
 
-        if direction == "U":
-            r -= 1
-        if direction == "D":
-            r += 1
-        if direction == "L":
-            c -= 1
-        if direction == "R":
-            c += 1
-        
-        isOutOfBounds = r < 0 or r >= self.height or c < 0 or c >= self.width
-        if isOutOfBounds:
+        if nr < 0 or nc < 0 or nr >= len(self.grid) or nc >= len(self.grid[0]):
             return -1
-            
-        if self.board[r][c] == "f":
-            self.eatenCount += 1
+        
+        # "", "f", "s"
+        if self.grid[nr][nc] == "f":
+            self.snake.append([nr, nc])
+            self.grid[nr][nc] = "s"
             if self.food:
                 fr, fc = self.food.popleft()
-                self.board[fr][fc] = "f"
-        else:
+                self.grid[fr][fc] = "f"
+        elif self.grid[nr][nc] == "":
+            self.snake.append([nr, nc])
+            self.grid[nr][nc] = "s"
             tr, tc = self.snake.popleft()
-            self.board[tr][tc] = ""
+            self.grid[tr][tc] = ""
+        elif self.grid[nr][nc] == "s":
+            # print(f"{nr} {nc}. {self.snake[0]}")
+            if (nr, nc) == tuple(self.snake[0]):
+                tail = self.snake.popleft()
+                self.snake.append(tail)
+            else:
+                print("HIT")
+                return -1      
         
-        if self.board[r][c] == "s":
-            return -1
+        return len(self.snake) - 1
         
-        self.board[r][c] = "s"
-        self.snake.append([r, c])
 
-        return self.eatenCount
-
+        
 
 
 # Your SnakeGame object will be instantiated and called as such:

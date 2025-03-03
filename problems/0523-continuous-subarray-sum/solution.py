b6ff512b772f13f972ea2a02aa84173429095f53
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-03 03:30:53 +0000

    [LeetCode Sync] Runtime - 195 ms (5.14%), Memory - 47.9 MB (5.29%)

--- problems/0523-continuous-subarray-sum/solution.py
+++ problems/0523-continuous-subarray-sum/solution.py
@@ -1,26 +1,25 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        # [23, 2,  4,  6,  7]
-        # [23, 25, 29, 35, 42]
-        #  5.  1.   5
-
-
-        # [23, 2  ,4,  6,  6]
-        #. 23. 25  29  35, 41
-        #  2.  4.  1.   0.  5
-        remainderIdx = {}
-
+        sumRemainders = []
         total = 0
         for i, n in enumerate(nums):
             total += n
-            remainder = total % k
-
-            if remainder == 0 and i >= 1:
+            
+            if total % k == 0 and i > 0:
                 return True
-
-            if remainder in remainderIdx and i - remainderIdx[remainder] > 1 and i > 0:
-                return True
-            elif remainder not in remainderIdx:
-                remainderIdx[remainder] = i
+            
+            sumRemainders.append(total % k)
         
+        print(sumRemainders)
+        remainderCount = collections.defaultdict(list) # remainder: [idx]
+
+        for i, n in enumerate(sumRemainders):
+            remainderCount[n].append(i)
+        
+        for idxs in remainderCount.values():
+            if max(idxs) - min(idxs) > 1:
+                return True
+            
         return False
+
+        

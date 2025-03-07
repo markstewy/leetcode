822c97ff89a9aee8841b0bc2391551c7bb27e054
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-07 00:27:40 +0000

    [LeetCode Sync] Runtime - 150 ms (15.85%), Memory - 59.8 MB (98.00%)

--- problems/0077-combinations/solution.py
+++ problems/0077-combinations/solution.py
@@ -1,22 +1,22 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = list(range(1, n + 1))
         ans = []
+        print(nums)
 
         def helper(sub, i):
             if len(sub) == k:
                 ans.append(sub.copy())
                 return
-            if i > n:
+            if i >= len(nums):
                 return
             
-            sub.append(i)
+            sub.append(nums[i])
             helper(sub, i + 1)
             sub.pop()
 
             helper(sub, i + 1)
         
-        helper([], 1)
-
+        helper([], 0)
         return ans
-
 

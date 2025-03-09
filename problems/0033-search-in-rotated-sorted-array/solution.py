08f8d3a8dbdb3244c96eb76b0de9df2d6a6b3cea
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-09 19:33:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- problems/0033-search-in-rotated-sorted-array/solution.py
+++ problems/0033-search-in-rotated-sorted-array/solution.py
@@ -1,22 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0 
+        
+        l = 0
         r = len(nums) - 1
 
         while l <= r:
             m = l + (r - l) // 2
+
             if nums[m] == target:
                 return m
 
-            if nums[r] < nums[m]: # left side is sequential
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1
-            else: # right side is sequential
+            if nums[r] > nums[m]: # continuous
                 if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
+            else:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
         
         return -1
+

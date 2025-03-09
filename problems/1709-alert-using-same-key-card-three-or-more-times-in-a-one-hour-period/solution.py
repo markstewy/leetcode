e2d6cd37bfba08adf01e587cea464f666864b3b1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-09 19:50:11 +0000

    [LeetCode Sync] Runtime - 239 ms (16.80%), Memory - 45 MB (12.40%)

--- problems/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/solution.py
+++ problems/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/solution.py
@@ -1,22 +1,36 @@
 class Solution:
     def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
-        emps = collections.defaultdict(deque)
-        keyTime = [ int(time.replace(":", "")) for time in keyTime ]
+        keyTime = [int(n.replace(":", "")) for n in keyTime]
         times = list(zip(keyTime, keyName))
         times.sort()
+
         ans = set()
 
-        for e in times:
-            time, name = e[0], e[1]
-            
-            emps[name].append(time)
+        empCount = {}
 
-            while emps[name] and emps[name][0] < time - 100:
-                emps[name].popleft()
+        l = 0
+        for r, t in enumerate(times):
+            time = t[0]
+            name = t[1]
+            empCount[name] = empCount.get(name, 0) + 1
+
+            while times[l][0] < time - 100:
+                empCount[times[l][1]] -= 1
+                l += 1
             
-            if len(emps[name]) >= 3:
+            if empCount[name] >= 3:
                 ans.add(name)
         
-        ans = list(ans)
-        ans.sort()
-        return ans
+        names = list(ans)
+        names.sort()
+        return names
+        
+
+
+
+
+
+
+
+
+

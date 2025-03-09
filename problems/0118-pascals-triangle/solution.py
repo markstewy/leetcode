83e3479657a2096f619fdf4479caeddd8799e745
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-09 20:37:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- problems/0118-pascals-triangle/solution.py
+++ problems/0118-pascals-triangle/solution.py
@@ -1,23 +1,24 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-            if numRows == 0:
-                return []
+        ans = []
+        
+        for r in range(1, numRows + 1):
+            # print(r)
+            row = []
+            for i in range(r):
+                if i == 0:
+                    row.append(1)
+                elif i == r - 1:
+                    row.append(1)
+                else:
+                    row.append(ans[-1][i - 1] + ans[-1][i])
+            ans.append(row)
+        
+        return ans
+                
+
+
+
+                
             
-            matrix = [[1]]
-
-            for r in range(1, numRows):
-                childRow = [0] * (r + 1)
-                matrix.append(childRow)
-                parentRow = matrix[r - 1]
-                for i in range(len(childRow)):
-                    l = parentRow[i - 1] if i > 0 else 0
-                    r = parentRow[i] if i < len(parentRow) else 0
-                    childRow[i] = l + r
-            
-            return matrix
-
-
-           
-
-
 

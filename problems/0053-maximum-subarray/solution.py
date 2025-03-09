9f042aa5d24d795fbc8968d9754103f7a14f67e4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-09 06:13:16 +0000

    [LeetCode Sync] Runtime - 118 ms (5.38%), Memory - 32.4 MB (6.24%)

--- problems/0053-maximum-subarray/solution.py
+++ problems/0053-maximum-subarray/solution.py
@@ -1,19 +1,22 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # Initialize current sum and max sum with the first element
-        current_sum = max_sum = nums[0]
+        prevMins = [0]
+        totals = []
+
+        total = 0
+        prevMin = 0
+
+
+        for n in nums:
+            total += n
+            totals.append(total)
+            prevMin = min(prevMin, total)
+            prevMins.append(prevMin)
+
+        maxSum = totals[0]
+        for i in range(len(totals)):
+            summ = totals[i] - prevMins[i]
+            maxSum = max(maxSum, summ)
         
-        # Iterate through the array starting from the second element
-        for num in nums[1:]:
-            # For each element, decide whether to start a new subarray 
-            # or extend the existing subarray
-            current_sum = max(num, current_sum + num)
-            
-            # Update the maximum sum if current sum is larger
-            max_sum = max(max_sum, current_sum)
-        
-        return max_sum
-
-
-
+        return maxSum
 

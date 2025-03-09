7a72b09aa6cd5c3ae5e6a2f2ea7571cb176ed497
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-09 20:08:50 +0000

    [LeetCode Sync] Runtime - 221 ms (21.08%), Memory - 19.6 MB (97.97%)

--- problems/1171-shortest-path-in-binary-matrix/solution.py
+++ problems/1171-shortest-path-in-binary-matrix/solution.py
@@ -3,24 +3,21 @@ class Solution:
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
 
+        dq = deque()
+        dq.append([0, 0, 1])
+        directions = [(1, 1), (1, 0), (1, -1), (0,1), (0,-1), (-1,1),(-1,0),(-1,-1)]
         visited = set()
-        stack = deque([[0, 0, 1]])
-        directions = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
 
-        while stack:
-            r, c, l = stack.popleft()
-
+        while dq:
+            r, c, l = dq.popleft()
             if r == len(grid) - 1 and c == len(grid[0]) - 1:
                 return l
-            
+
             for nr, nc in directions:
-                nr = nr + r
-                nc = nc + c
-            
+                nr += r
+                nc += c
+
                 if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 0 and (nr, nc) not in visited:
                     visited.add((nr, nc))
-                    stack.append([nr, nc, l + 1])
-        
+                    dq.append([nr, nc, l + 1])
         return -1
-
-

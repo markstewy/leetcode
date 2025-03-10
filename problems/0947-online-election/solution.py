a6c9cffd6e6d5ecb3d809602aee07169b9727dce
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-10 06:04:20 +0000

    [LeetCode Sync] Runtime - 241 ms (15.66%), Memory - 23.9 MB (100.00%)

--- problems/0947-online-election/solution.py
+++ problems/0947-online-election/solution.py
@@ -1,37 +1,43 @@
 class TopVotedCandidate:
 
     def __init__(self, persons: List[int], times: List[int]):
-        self.votes = list(zip(times, persons))
+        self.votes = list(zip(times, persons))        
+        self.votes.sort()
+
+        leader = ""
+        leaderCount = 0
+        count = {}
         self.leaders = []
 
-        voteCount = {}
-        leader = None
-        leaderCount = 0
-
         for t, p in self.votes:
-            voteCount[p] = voteCount.get(p, 0) + 1
-            if voteCount[p] >= leaderCount:
-                leaderCount = voteCount[p]
+            count[p] = count.get(p, 0) + 1
+            if count[p] >= leaderCount:
                 leader = p
-            self.leaders.append([t, leader])
-
+                leaderCount = count[p]
+                self.leaders.append([t, leader])
         print(self.leaders)
 
+             
     def q(self, t: int) -> int:
         l = 0
         r = len(self.leaders) - 1
-        closestPrev = self.leaders[-1][1]
+        closestPrev = self.leaders[0]
 
         while l <= r:
             m = l + (r - l) // 2
 
-            if t >= self.leaders[m][0]:
-                closestPrev = self.leaders[m][1]
+            if self.leaders[m][0] <= t:
+                closestPrev = self.leaders[m]
                 l = m + 1
             else:
                 r = m - 1
         
-        return closestPrev
+        return closestPrev[1]
+
+
+
+
+
         
 
 

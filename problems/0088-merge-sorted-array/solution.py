24618e1faf6bc15484febfcc5b4fae9906216d14
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-13 17:55:49 +0000

    [LeetCode Sync] Runtime - 5 ms (1.47%), Memory - 18.1 MB (100.00%)

--- problems/0088-merge-sorted-array/solution.py
+++ problems/0088-merge-sorted-array/solution.py
@@ -3,11 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m
+        k = len(nums1) - len(nums2)
         for n in nums2:
-            nums1[i] = n
-            i += 1
-        nums1.sort()
-        return nums1
+            nums1[k] = n
+            k += 1
 
+        def merge(l, r, m):
+            dq1 = deque(nums1[l:m+1])
+            dq2 = deque(nums1[m+1:r+1])
+
+            while dq1 or dq2:
+                val1 = dq1[0] if dq1 else float("infinity")
+                val2 = dq2[0] if dq2 else float("infinity")
+                
+                if val1 < val2:
+                    nums1[l] = dq1.popleft() 
+                else:
+                    nums1[l] = dq2.popleft()
+                l += 1
+
+        def mergeSort(l, r):
+            if l == r:
+                return
             
+            m = l + (r - l) // 2
+            mergeSort(l, m)
+            mergeSort(m+1, r)
+
+            merge(l, r, m)
+    
+        mergeSort(0, len(nums1) - 1)

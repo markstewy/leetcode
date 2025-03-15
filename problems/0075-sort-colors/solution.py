97329719fcc48d2886bbae757dd455afbf0fc875
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-15 20:08:04 +0000

    [LeetCode Sync] Runtime - 3 ms (11.26%), Memory - 18.1 MB (100.00%)

--- problems/0075-sort-colors/solution.py
+++ problems/0075-sort-colors/solution.py
@@ -4,34 +4,31 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         def merge(l, r, m):
-            arr1 = deque(nums[l:m+1])  # Include m in the first half
-            arr2 = deque(nums[m+1:r+1])  # Include r in the second half
-            
-            while arr1 or arr2:
-                val1 = arr1[0] if arr1 else float("infinity")
-                val2 = arr2[0] if arr2 else float("infinity")
+            dq1 = deque(nums[l : m + 1])
+            dq2 = deque(nums[m + 1 : r + 1])
 
-                if val1 < val2:
-                    nums[l] = arr1.popleft()
+            while dq1 or dq2:
+                dq1val = dq1[0] if dq1 else float("infinity")
+                dq2val = dq2[0] if dq2 else float("infinity")
+
+                if dq1val < dq2val:
+                    nums[l] = dq1.popleft()
                 else:
-                    nums[l] = arr2.popleft()
+                    nums[l] = dq2.popleft()
                 l += 1
+
 
         def mergeSort(l, r):
             if l == r:
                 return
-            
+
             m = l + (r - l) // 2
             mergeSort(l, m)
-            mergeSort(m + 1, r)  # Start second half at m+1
-            merge(l, r, m)  # Pass the correct boundaries
+            mergeSort(m + 1, r)
 
-        mergeSort(0, len(nums) - 1)  # Don't forget to call the function
-
-
-            
-            
-
-
-
+            merge(l, r, m)
+        
+        mergeSort(0, len(nums) - 1)
+        
+        return nums
 

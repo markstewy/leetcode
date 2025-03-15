95300b6ce5a11b05fd70bdb4ba3d345168b167a3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-15 20:29:21 +0000

    [LeetCode Sync] Runtime - 115 ms (5.32%), Memory - 31.7 MB (9.04%)

--- problems/1849-maximum-absolute-sum-of-any-subarray/solution.py
+++ problems/1849-maximum-absolute-sum-of-any-subarray/solution.py
@@ -1,31 +1,25 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
-        prevMins = [0]
-        prevMaxs = [0]
+        
         totals = []
-
         total = 0
-        prevMin = 0
-        prevMax = 0
-
         for n in nums:
             total += n
-            prevMin = min(prevMin, total)
-            prevMax = max(prevMax, total)
-
             totals.append(total)
-            prevMins.append(prevMin)
-            prevMaxs.append(prevMax)
         
-        absMaxSum = abs(totals[0])
+        prevNeg = [0]
+        prevPos = [0]
 
-        for i, n in enumerate(totals):
-            maxNeg = n - prevMaxs[i]
-            maxPos = n - prevMins[i]
-
-            absMaxSum = max(abs(maxNeg), abs(maxPos), absMaxSum)
+        for t in totals:
+            minNeg = min(prevNeg[-1], t)
+            maxPos = max(prevPos[-1], t)
+            prevNeg.append(minNeg)
+            prevPos.append(maxPos)
         
-        return absMaxSum
+        absSum = abs(nums[0])
 
-
+        for i, t in enumerate(totals):
+            absSum = max(absSum, abs(t - prevPos[i]), abs(t - prevNeg[i]))
+        
+        return absSum
 

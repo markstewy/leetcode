395e2400cc0fa68b235d33fdabc8e716f46b5d63
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-15 20:19:52 +0000

    [LeetCode Sync] Runtime - 142 ms (5.38%), Memory - 33.2 MB (6.24%)

--- problems/0053-maximum-subarray/solution.py
+++ problems/0053-maximum-subarray/solution.py
@@ -1,22 +1,24 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        prevMins = [0]
-        totals = []
-
         total = 0
-        prevMin = 0
-
-
+        totals = []
         for n in nums:
             total += n
             totals.append(total)
-            prevMin = min(prevMin, total)
-            prevMins.append(prevMin)
-
-        maxSum = totals[0]
-        for i in range(len(totals)):
-            summ = totals[i] - prevMins[i]
-            maxSum = max(maxSum, summ)
         
-        return maxSum
+        prevMins = [0]
+        for t in totals:
+            currMin = min(prevMins[-1], t)
+            prevMins.append(currMin)
+        
+        maxSub = nums[0]
+        
+        for i, t in enumerate(totals):
+            maxSub = max(t, t - prevMins[i], maxSub)
+    
+        return maxSub
+        
 
+# [-2, 1]
+# [-2,-1]
+# [0, -2]

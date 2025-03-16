46601fde61bd2c47af98b6ea1ba46eac9ed4faff
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-16 21:27:04 +0000

    [LeetCode Sync] Runtime - 100 ms (29.73%), Memory - 24 MB (100.00%)

--- problems/1816-lowest-common-ancestor-of-a-binary-tree-iv/solution.py
+++ problems/1816-lowest-common-ancestor-of-a-binary-tree-iv/solution.py
@@ -8,25 +8,25 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
         k = len(nodes)
-        vals = [node.val for node in nodes]
-        nodeSet = set(vals)
+        nset = set(nodes)
         self.lca = None
 
         def helper(root):
             if not root:
                 return 0
-
+            
             l = helper(root.left)
             r = helper(root.right)
+
             total = l + r
 
-            if root.val in nodeSet:
+            if root in nset:
                 total += 1
-            
+
             if total == k:
                 self.lca = root
                 return 0
-            
+
             return total
         
         helper(root)

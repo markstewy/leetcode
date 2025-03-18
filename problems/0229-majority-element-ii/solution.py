8f32ae3894e27204df96370e9016a935595229c4
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-18 05:53:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (100.00%)

--- problems/0229-majority-element-ii/solution.py
+++ problems/0229-majority-element-ii/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        k = len(nums) // 3
-        count = {}
-        for n in nums:
-            count[n] = count.get(n, 0) + 1
-
         ans = []
+        k = len(nums) / 3
+        count = Counter(nums)
+        
         for n, c in count.items():
             if c > k:
                 ans.append(n)

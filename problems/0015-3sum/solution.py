c9754b5f297fab4fd784a661ce713ac47c8814bd
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-18 19:07:42 +0000

    [LeetCode Sync] Runtime - 439 ms (97.42%), Memory - 20.7 MB (99.96%)

--- problems/0015-3sum/solution.py
+++ problems/0015-3sum/solution.py
@@ -3,27 +3,27 @@ class Solution:
         nums.sort()
         ans = []
 
-        for i in range(len(nums)): 
+        for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             
+            if nums[i] > 0:
+                break
+            
+
             l = i + 1
             r = len(nums) - 1
 
             while l < r:
                 total = nums[i] + nums[l] + nums[r]
-                
                 if total > 0:
                     r -= 1
                 elif total < 0:
                     l += 1
                 else:
-                    ans.append([nums[i], nums[l], nums[r]])
+                    ans.append([nums[i], nums[l],  nums[r]])
                     l += 1
-                
-                    while nums[l] == nums[l - 1] and l < r:
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                
+        
         return ans
-
-

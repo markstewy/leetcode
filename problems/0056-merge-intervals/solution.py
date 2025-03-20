4b965c03aff3b0d9934b1085ae321703f5a02e8a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-20 22:14:26 +0000

    [LeetCode Sync] Runtime - 11 ms (24.79%), Memory - 20.9 MB (100.00%)

--- problems/0056-merge-intervals/solution.py
+++ problems/0056-merge-intervals/solution.py
@@ -1,19 +1,22 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        
-        def isOverlap(i1, i2):
-            return i1[0] <= i2[0] <= i1[1] or i1[0] <= i2[1] <= i1[1] or i2[0] <= i1[0] <= i2[1] or i2[0] <= i1[1] <= i2[1]
-        
-        def merge(i1, i2):
-            return [min(i1[0], i2[0]), max(i1[1], i2[1])]
 
+        def isOverlap(int1, int2):
+            b1, e1, = int1[0], int1[1]
+            b2, e2, = int2[0], int2[1]
+            return b1 <= b2 <= e1 or b1 <= e2 <= e1 or b2 <= b1 <= e2 or b2 <= e1 <= e2
+
+        def merge(int1, int2):
+            return [min(int1[0], int2[0]), max(int1[1], int2[1])]
+
+        
         ans = [intervals[0]]
-
         for i in intervals:
             if isOverlap(i, ans[-1]):
-                ans[-1] = merge(ans[-1], i)
+                ans[-1] = merge(i, ans[-1])
             else:
                 ans.append(i)
         
         return ans
+

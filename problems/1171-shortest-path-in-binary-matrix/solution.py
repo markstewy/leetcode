43b4cb2c3b46a4e5862a9228def67d9008ce449e
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-20 00:27:03 +0000

    [LeetCode Sync] Runtime - 345 ms (5.07%), Memory - 19.9 MB (60.11%)

--- problems/1171-shortest-path-in-binary-matrix/solution.py
+++ problems/1171-shortest-path-in-binary-matrix/solution.py
@@ -3,21 +3,25 @@ class Solution:
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
 
-        dq = deque()
-        dq.append([0, 0, 1])
-        directions = [(1, 1), (1, 0), (1, -1), (0,1), (0,-1), (-1,1),(-1,0),(-1,-1)]
+        dq = deque([(0, 0, 1)])
         visited = set()
+        directions = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
 
         while dq:
-            r, c, l = dq.popleft()
+            print(dq[0])
+            r, c, count = dq.popleft()
+
             if r == len(grid) - 1 and c == len(grid[0]) - 1:
-                return l
+                return count
 
             for nr, nc in directions:
                 nr += r
                 nc += c
 
-                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 0 and (nr, nc) not in visited:
+                if (nr, nc) not in visited and 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 0:
                     visited.add((nr, nc))
-                    dq.append([nr, nc, l + 1])
+                    dq.append((nr, nc, count + 1))
         return -1
+            
+
+

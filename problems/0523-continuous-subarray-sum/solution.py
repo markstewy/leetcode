2133ae2491872cba3b4729ff44cd246e186ff27b
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-20 00:06:36 +0000

    [LeetCode Sync] Runtime - 263 ms (5.14%), Memory - 51.6 MB (5.29%)

--- problems/0523-continuous-subarray-sum/solution.py
+++ problems/0523-continuous-subarray-sum/solution.py
@@ -1,27 +1,27 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        
         totals = []
         total = 0
-
         for n in nums:
             total += n
             totals.append(total)
+        
+        mods = []
+        for t in totals:
+            mods.append(t % k)
+        
+        modMap = collections.defaultdict(list)
 
-        mods = [t % k for t in totals]
-        print(totals)
-        print(mods)
-
-        # if the mod is 0 and not the first number
-        # if two mods are the same the sum of the array beween those idxs is divisible by k
-        modIdx = collections.defaultdict(list)
-
+        for i, m in enumerate(mods):
+            modMap[m].append(i)
+        
         for i, m in enumerate(mods):
             if m == 0 and i > 0:
                 return True
-        
-            modIdx[m].append(i)
-            if max(modIdx[m]) - min(modIdx[m]) > 1:
+            
+            if i - min(modMap[m]) > 1:
                 return True
         
         return False
+
+

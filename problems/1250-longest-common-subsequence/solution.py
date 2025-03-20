b2d6ce73c8f18bb8ed2835f54ec500d60df8f5e2
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-20 22:32:07 +0000

    [LeetCode Sync] Runtime - 506 ms (30.50%), Memory - 42.8 MB (82.36%)

--- problems/1250-longest-common-subsequence/solution.py
+++ problems/1250-longest-common-subsequence/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         matrix = [[0] * (len(text1) + 1) for _ in range(len(text2) + 1)]
-        # text1 = col
-        # text2 = row
+        # row = text1
+        # col = text2
 
         for r in range(len(text2)):
             for c in range(len(text1)):
-                if text2[r] == text1[c]:
-                    matrix[r + 1][c + 1] = matrix[r][c] + 1
+                if text1[c] == text2[r]:
+                    matrix[r+1][c+1] = matrix[r][c] + 1
                 else:
-                    matrix[r + 1][c + 1] = max(matrix[r + 1][c], matrix[r][c + 1])
-        
+                    matrix[r+1][c+1] = max(matrix[r][c], matrix[r+1][c], matrix[r][c+1])
+
+
         return matrix[-1][-1]
-
-

7f6ee85f913dce032067e6225e77660540a1cf4e
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-21 01:48:50 +0000

    [LeetCode Sync] Runtime - 3 ms (85.98%), Memory - 19.8 MB (99.99%)

--- problems/0207-course-schedule/solution.py
+++ problems/0207-course-schedule/solution.py
@@ -1,30 +1,28 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         prs = collections.defaultdict(list)
-        for crs, pr in prerequisites: 
+        for crs, pr in prerequisites:
             prs[crs].append(pr)
         
         visiting = set()
         completed = set()
 
         def helper(crs):
-            if crs in visiting:
-                return False
             if crs in completed:
                 return True
+            if crs in visiting:
+                return False
             
             visiting.add(crs)
             for pr in prs[crs]:
                 if helper(pr) == False:
                     return False
             visiting.remove(crs)
-            
+
             completed.add(crs)
-            
             return True
         
         for crs in range(numCourses):
             if helper(crs) == False:
                 return False
         return True
-

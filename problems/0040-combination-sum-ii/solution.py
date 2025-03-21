94bc3977cc4220d19996041e5c42e7d1e1091cfc
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-21 00:53:46 +0000

    [LeetCode Sync] Runtime - 31 ms (19.55%), Memory - 18 MB (100.00%)

--- problems/0040-combination-sum-ii/solution.py
+++ problems/0040-combination-sum-ii/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        ans = []
         candidates.sort()
+        ans = []
 
         def helper(sub, i, total):
             if total == target:
@@ -9,16 +9,15 @@ class Solution:
                 return
             if i >= len(candidates) or total > target:
                 return
-            
+
             sub.append(candidates[i])
             helper(sub, i + 1, total + candidates[i])
             sub.pop()
 
             while i < len(candidates) - 1 and candidates[i] == candidates[i + 1]:
                 i += 1
-            
-            helper(sub, i + 1, total)
 
+            helper(sub, i + 1, total)
+        
         helper([], 0, 0)
         return ans
-

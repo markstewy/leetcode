5f966d888405a1261c48010b779321e00753750c
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-23 06:43:28 +0000

    [LeetCode Sync] Runtime - 98 ms (79.91%), Memory - 78.7 MB (15.89%)

--- problems/0146-lru-cache/solution.py
+++ problems/0146-lru-cache/solution.py
@@ -1,62 +1,58 @@
-class Node():
-    def __init__(self, val=None):
+class Node:
+    def __init__(self, val, key):
         self.val = val
+        self.key = key
         self.next = None
         self.prev = None
-        self.key = None
-
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.left = Node()
-        self.right = Node()
-        self.right.prev = self.left
-        self.left.next = self.right
-
-        self.nodeMap = {} # key: node
         self.capacity = capacity
+        self.l = Node(-1, -1)
+        self.r = Node(-1, -1)
+        self.l.next = self.r
+        self.r.prev = self.l
+        self.nodeMap = {}
 
     def get(self, key: int) -> int:
         if key not in self.nodeMap:
             return -1
+        
         node = self.nodeMap[key]
-        self.pasteFront(self.cut(node))
+        self.insert(self.cut(node))
         return node.val
-
+        
 
     def put(self, key: int, value: int) -> None:
         if key not in self.nodeMap:
-            node = Node(value)
-            node.key = key
+            node = Node(value, key)
             self.nodeMap[key] = node
-            self.pasteFront(node)
+            self.insert(node)
         else:
             node = self.nodeMap[key]
             node.val = value
-            self.pasteFront(self.cut(node))
+            self.insert(self.cut(node))
         
-        self.trimCache()
+        if len(self.nodeMap) > self.capacity:
+            node = self.l.next
+            ky = node.key
+            self.cut(node)
+            del self.nodeMap[ky]        
 
-    def cut(self, node) -> Node:
+    def cut(self, node: Node) -> Node:
         node.prev.next = node.next
         node.next.prev = node.prev
         node.next = None
         node.prev = None
         return node
-
-    def pasteFront(self, node) -> None:
-        node.prev = self.right.prev
-        node.next = self.right
+    
+    def insert(self, node: Node) -> None:
+        node.next = self.r
+        node.prev = self.r.prev
         node.prev.next = node
         node.next.prev = node
-
-    def trimCache(self):
-        while len(self.nodeMap.keys()) > self.capacity:
-            node = self.left.next
-            key = node.key
-            self.cut(node)
-            del self.nodeMap[key]
+        
 
 
 # Your LRUCache object will be instantiated and called as such:

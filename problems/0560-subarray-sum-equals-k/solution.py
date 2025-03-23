a0f0f8bf9dd93d07bc68cbc8473f5e94640956bb
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-23 21:58:01 +0000

    [LeetCode Sync] Runtime - 9758 ms (5.19%), Memory - 23.4 MB (10.08%)

--- problems/0560-subarray-sum-equals-k/solution.py
+++ problems/0560-subarray-sum-equals-k/solution.py
@@ -1,28 +1,23 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
         totals = []
         total = 0
         for n in nums:
             total += n
             totals.append(total)
-        
-        totalsMap = collections.defaultdict(list)
+
+        totalsIdx = collections.defaultdict(list)
         for i, t in enumerate(totals):
-            totalsMap[t].append(i)
+            totalsIdx[t].append(i)
         
         count = 0
         for i, t in enumerate(totals):
-            if t == k:
+            diff = t - k
+            if diff == 0:
                 count += 1
-                
-            target = -(k - t)
-            if target in totalsMap:
-                for j in totalsMap[target]:
-                    if j < i:
+            if diff in totalsIdx:
+                for idx in totalsIdx[diff]:
+                    if idx < i:
                         count += 1
         
         return count
-        
-
-        

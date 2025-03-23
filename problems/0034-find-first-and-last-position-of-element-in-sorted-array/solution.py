31086fd1bc1c8117882007fc7217ff3fc4d10a23
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-23 05:48:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (100.00%)

--- problems/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ problems/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,41 +1,49 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        
-        def closestPrev():
+        def bs():
             l = 0
             r = len(nums) - 1
-            closestPrev = 0
 
             while l <= r:
                 m = l + (r - l) // 2
-
-                if nums[m] <= target:
-                    closestPrev = m
-                if nums[m] < target:
-                    l = m + 1
-                else:
-                    r = m - 1
-            
-            return closestPrev if nums[closestPrev] == target else closestPrev + 1
-
-        
-        def closestNext():
-            l = 0
-            r = len(nums) - 1
-            closestNext = r
-
-            while l <= r:
-                m = l + (r - l) // 2
-
-                if nums[m] >= target:
-                    closestNext = m
                 if nums[m] > target:
                     r = m - 1
+                elif nums[m] < target:
+                    l = m + 1
+                else:
+                    return True
+            return False
+        
+        def bsl():
+            l = 0
+            r = len(nums) - 1
+            closestPrev = -1
+
+            while l <= r:
+                m = l + (r - l) // 2
+                if nums[m] < target:
+                    closestPrev = m
+                    l = m + 1
+                else:
+                    r = m - 1
+            
+            return closestPrev + 1
+        
+        def bsr():
+            l = 0
+            r = len(nums) - 1
+            closestNext = len(nums)
+
+            while l <= r:
+                m = l + (r - l) // 2
+                if nums[m] > target:
+                    r = m - 1
+                    closestNext = m
                 else:
                     l = m + 1
             
-            return closestNext if nums[closestNext] == target else closestNext - 1
+            return closestNext - 1
         
-        if not nums or target not in nums:
+        if not bs():
             return [-1, -1]
-        return [closestPrev(), closestNext()]
+        return [bsl(), bsr()]

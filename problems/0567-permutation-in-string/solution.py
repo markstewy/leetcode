0d21b51de7054d28a4e19510a8be0d1b532fc570
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-23 21:19:39 +0000

    [LeetCode Sync] Runtime - 23 ms (47.28%), Memory - 18.2 MB (100.00%)

--- problems/0567-permutation-in-string/solution.py
+++ problems/0567-permutation-in-string/solution.py
@@ -1,35 +1,28 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1count = Counter(s1)
-        s2count = {}
+        count1 = Counter(s1)
+        count2 = {}
 
+        needed = len(count1)
         matches = 0
-        needed = len(s1count.keys())
 
         l = 0
         for r in range(len(s2)):
-
-            # add new char on right
-            c = s2[r]
-            s2count[c] = s2count.get(c, 0) + 1
-
-            if c in s1count and s1count[c] == s2count[c]:
-                matches += 1
-            if c in s1count and s1count[c] == s2count[c] - 1:
-                matches -=1
-            
-            # remove trailing char on left
-            if r - l + 1 > len(s1):
-                c = s2[l]
-                l += 1
-                s2count[c] -= 1
-
-                if c in s1count and s1count[c] == s2count[c]:
-                    matches += 1
-                if c in s1count and s1count[c] == s2count[c] + 1:
-                    matches -=1
             if matches == needed:
                 return True
+
+            c = s2[r]
+            count2[c] = count2.get(c, 0) + 1
+
+            if c in count1 and count1[c] == count2[c]:
+                matches += 1
+            
+            if r - l + 1 > len(s1):
+                c = s2[l]
+                count2[c] -= 1
+                
+                if c in count1 and count1[c] == count2[c] + 1:
+                    matches -= 1
+                l += 1
         
         return matches == needed
-

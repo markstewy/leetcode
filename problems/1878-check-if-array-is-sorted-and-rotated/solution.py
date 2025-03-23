9f3b57941e67f14508dcd424eb4b8456015ba6e3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-23 23:53:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- problems/1878-check-if-array-is-sorted-and-rotated/solution.py
+++ problems/1878-check-if-array-is-sorted-and-rotated/solution.py
@@ -1,19 +1,11 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        mn = min(nums)
-        mnIdx = None
+        decreaseCount = 0
 
-        for i in range(len(nums) - 1, -1, -1):
-            if i == 0 or (nums[i] == mn and nums[i-1] > nums[i]):
-                mnIdx = i
-                break
-
-        prev = -float("infinity")
-
-        for i in range(len(nums)):
-            idx = (i + mnIdx) % len(nums)
-            if nums[idx] < prev:
-                return False
-            prev = nums[idx]
+        prev = nums[-1]
+        for n in nums:
+            if n < prev:
+                decreaseCount += 1
+            prev = n
         
-        return True
+        return decreaseCount <= 1

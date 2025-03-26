553096aac14e345f84c1cc3826a57c5f7af74163
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-26 00:28:58 +0000

    [LeetCode Sync] Runtime - 59 ms (24.64%), Memory - 28.9 MB (99.52%)

--- problems/0041-first-missing-positive/solution.py
+++ problems/0041-first-missing-positive/solution.py
@@ -1,18 +1,18 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         for i in range(len(nums)):
-            if nums[i] < 0:
+            if nums[i] < 0: # [1, 2, 3, 4] worst case ans is len(nums) + 1
                 nums[i] = 0
         
         for n in nums:
             idx = abs(n) - 1
             if 0 <= idx < len(nums):
                 if nums[idx] == 0:
-                    nums[idx] = -(len(nums) + 1)
+                    nums[idx] = -(len(nums))
                 else:
                     nums[idx] = -abs(nums[idx])
         
-        for i, n in enumerate(nums):
+        for i , n in enumerate(nums):
             if n >= 0:
                 return i + 1
         

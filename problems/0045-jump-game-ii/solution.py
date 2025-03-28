bf5ebacfa7936efb2cfcfa60864025fb000fad4f
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-28 04:55:10 +0000

    [LeetCode Sync] Runtime - 6 ms (55.53%), Memory - 18.7 MB (100.00%)

--- problems/0045-jump-game-ii/solution.py
+++ problems/0045-jump-game-ii/solution.py
@@ -1,18 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        maxIdxReach = []
-        maxIdx = 0
+        maxReachIdxs = []
+        maxReach = 0
 
         for i, n in enumerate(nums):
-            maxIdx = max(maxIdx, n + i)
-            maxIdxReach.append(maxIdx)
+            maxReach = max(maxReach, i + n)
+            maxReachIdxs.append(maxReach)
         
-        print(maxIdxReach)
+        prev = -1
         i = 0
         count = 0
-        while i < len(nums) - 1:
+        while i != prev:
+            if i >= len(nums) - 1:
+                return count
+            
+            prev = i
+            i = maxReachIdxs[i]
             count += 1
-            i = maxIdxReach[i]
-
-        return count
-
+        
+        

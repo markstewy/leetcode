c90c66bf2779336dd603cfc946acd2979d35bd8d
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-03-30 22:17:58 +0000

    [LeetCode Sync] Runtime - 564 ms (5.01%), Memory - 798.4 MB (13.72%)

--- problems/0353-design-snake-game/solution.py
+++ problems/0353-design-snake-game/solution.py
@@ -3,62 +3,50 @@ class SnakeGame:
     def __init__(self, width: int, height: int, food: List[List[int]]):
         self.width = width
         self.height = height
-        
-        self.board = [[""] * self.width for _ in range(self.height)]
-        
+
+        self.grid = [[""] * self.width for _ in range(self.height)]
+        self.snake = deque([[0, 0]])
+        self.grid[0][0] = "s"
+
         self.food = deque(food)
         fr, fc = self.food.popleft()
-        self.board[fr][fc] = "f"
-
-        self.snake = deque([[0, 0]])
-        self.board[0][0] = "s"
-        
-        self.directions = {
-            "U": [-1, 0],
-            "D": [1, 0],
-            "L": [0, -1],
-            "R": [0, 1]
-        }
-
-    def isValid(self, r, c):
-        if not (0 <= r < self.height and 0 <= c < self.width):
-            return False
-        
-        tr, tc = self.snake[0]
-        isTail = r == tr and c == tc
-        isSnake = self.board[r][c] == "s" and not isTail
-
-        if isSnake:
-            return False
-
-        return True
+        self.grid[fr][fc] = "f"
 
 
     def move(self, direction: str) -> int:
-        nr, nc = self.directions[direction]
+        directions = {
+            "U": (-1, 0),
+            "D": (1, 0),
+            "L": (0, -1),
+            "R": (0, 1),
+        }
+        
+        nr, nc = directions[direction]
         nr += self.snake[-1][0]
         nc += self.snake[-1][1]
-
-        if not self.isValid(nr, nc):
+        
+        # inbounds
+        if not (0 <= nr < self.height and 0 <= nc < self.width):
             return -1
         
-        if self.board[nr][nc] != "f":
+        # is snake
+        if self.grid[nr][nc] == "s" and [nr, nc] != self.snake[0]:
+            return -1
+
+        # add food
+        if self.grid[nr][nc] != "f":
             tr, tc = self.snake.popleft()
-            self.board[tr][tc] = ""
+            self.grid[tr][tc] = ""
         else:
             if self.food:
                 fr, fc = self.food.popleft()
-                self.board[fr][fc] = "f"
+                self.grid[fr][fc] = "f"
         
+        # update board, and snake
+        self.grid[nr][nc] = "s"
         self.snake.append([nr, nc])
-        self.board[nr][nc] = "s"
+        
         return len(self.snake) - 1
-        
-            
-        
-
-
-
 
         
 

ebb96f7a3663aa62676f10a36f7f1890a13f9d17
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-02 15:49:05 +0000

    [LeetCode Sync] Runtime - 282 ms (10.80%), Memory - 28.4 MB (13.05%)

--- problems/0200-number-of-islands/solution.py
+++ problems/0200-number-of-islands/solution.py
@@ -1,27 +1,31 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islandCount = 0
+        visited = set()
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        count = 0
         
-        def delIsland(r, c):
+        def helper(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+                return
             if grid[r][c] == "0":
                 return
+            if (r, c) in visited:
+                return
             
-            grid[r][c] = "0"
+            visited.add((r, c))
 
-            if r > 0:
-                delIsland(r - 1, c)
-            if r < len(grid) - 1:
-                delIsland(r + 1, c)
-            if c > 0:
-                delIsland(r, c - 1)
-            if c < len(grid[0]) - 1:
-                delIsland(r, c + 1)
+            for nr, nc in directions:
+                nr += r
+                nc += c
+
+                helper(nr, nc)
         
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == "1":
-                    islandCount += 1
-                    delIsland(r, c)
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    count += 1
+                    helper(r, c)
         
-        return islandCount
+        return count
+
 

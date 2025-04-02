51a858505b393ee2e07fa3d4dbe8445690f5fac1
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-02 06:54:54 +0000

    [LeetCode Sync] Runtime - 19 ms (20.10%), Memory - 17.7 MB (100.00%)

--- problems/2186-count-vowel-substrings-of-a-string/solution.py
+++ problems/2186-count-vowel-substrings-of-a-string/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def countVowelSubstrings(self, word: str) -> int:
         count = 0
-        
+
         for i in range(len(word)):
             vset = set()
             j = i
+
             while j < len(word) and word[j] in "aeiou":
                 vset.add(word[j])
                 if len(vset) == 5:
@@ -12,6 +13,3 @@ class Solution:
                 j += 1
         
         return count
-                
-
-            

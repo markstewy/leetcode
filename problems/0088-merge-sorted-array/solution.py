e4511a49cb0a64f4985eedbcb064234d27ef53c3
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-02 07:00:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (100.00%)

--- problems/0088-merge-sorted-array/solution.py
+++ problems/0088-merge-sorted-array/solution.py
@@ -3,19 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-
-        dq1 = deque(nums1)
         dq2 = deque(nums2)
-        for _ in range(len(nums2)):
-            dq1.pop()
-
+        dq1 = deque(nums1[:m])
         i = 0
+
         while dq1 or dq2:
-            dq1val = dq1[0] if dq1 else float("infinity")
-            dq2val = dq2[0] if dq2 else float("infinity")
-            if dq1val < dq2val:
+            val1 = dq1[0] if dq1 else float("infinity")
+            val2 = dq2[0] if dq2 else float("infinity")
+
+            if val1 <= val2:
                 nums1[i] = dq1.popleft()
             else:
                 nums1[i] = dq2.popleft()
-            i += 1
             
+            i += 1
+        
+        
+            
+
+
+

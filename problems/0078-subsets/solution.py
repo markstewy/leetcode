ecf9c569b39dec47103bd9c9077351a919e587cf
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-02 16:49:15 +0000

    [LeetCode Sync] Runtime - 3 ms (7.28%), Memory - 17.8 MB (100.00%)

--- problems/0078-subsets/solution.py
+++ problems/0078-subsets/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-
+        ans = []        
 
         def helper(sub, i):
             if i >= len(nums):
@@ -13,6 +12,6 @@ class Solution:
             sub.pop()
 
             helper(sub, i + 1)
-        
+    
         helper([], 0)
         return ans

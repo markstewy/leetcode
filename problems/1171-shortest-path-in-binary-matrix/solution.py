e8444a64c98f5b40e7db3d391aa780abd15af7a8
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-04 04:17:19 +0000

    [LeetCode Sync] Runtime - 276 ms (9.26%), Memory - 19.5 MB (99.68%)

--- problems/1171-shortest-path-in-binary-matrix/solution.py
+++ problems/1171-shortest-path-in-binary-matrix/solution.py
@@ -2,15 +2,22 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
-
+        
         dq = deque([(0, 0, 1)])
-        visited = set()
         directions = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+        visited = set((0, 0))
 
+        def isValid(r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]):
+                return False
+            if (r, c) in visited:
+                return False
+            if grid[r][c] != 0:
+                return False
+            return True
+        
         while dq:
-            print(dq[0])
             r, c, count = dq.popleft()
-
             if r == len(grid) - 1 and c == len(grid[0]) - 1:
                 return count
 
@@ -18,10 +25,10 @@ class Solution:
                 nr += r
                 nc += c
 
-                if (nr, nc) not in visited and 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == 0:
-                    visited.add((nr, nc))
+                if isValid(nr, nc):
                     dq.append((nr, nc, count + 1))
+                    visited.add((nr, nc))
+        
         return -1
+
             
-
-

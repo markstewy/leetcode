efdd7cd9cea3976950ba17b0bda7de2e6a6e5572
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-04 05:02:51 +0000

    [LeetCode Sync] Runtime - 7 ms (39.71%), Memory - 17.9 MB (100.00%)

--- problems/0047-permutations-ii/solution.py
+++ problems/0047-permutations-ii/solution.py
@@ -1,22 +1,24 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         count = Counter(nums)
-
         perm = []
+        permSet = set()
         ans = []
 
         def helper():
             if len(perm) == len(nums):
+                # if "".join(perm) not in permSet:
                 ans.append(perm.copy())
+                    # permSet.add("".join(perm))
                 return
             
-            for n in count:
-                if count[n] > 0:
-                    perm.append(n)
-                    count[n] -= 1
+            for c in count:
+                if count[c] > 0:
+                    count[c] -= 1
+                    perm.append(c)
                     helper()
-                    count[n] += 1
                     perm.pop()
-            
+                    count[c] += 1
+        
         helper()
         return ans

70852a8880a7fc24e7c0c18fdd6aa6876c8d356a
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-06 05:19:27 +0000

    [LeetCode Sync] Runtime - 31 ms (36.15%), Memory - 30.1 MB (99.94%)

--- problems/0287-find-the-duplicate-number/solution.py
+++ problems/0287-find-the-duplicate-number/solution.py
@@ -1,19 +1,19 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         
-        f = 0
         s = 0
-
+        f = 0
         while True:
             s = nums[s]
             f = nums[nums[f]]
+
             if s == f:
                 break
         
         s2 = 0
-
         while True:
             s = nums[s]
             s2 = nums[s2]
             if s == s2:
                 return s
+

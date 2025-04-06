31a0d0754bea83835eb2e2504e1f19e14c511fdf
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-06 05:02:19 +0000

    [LeetCode Sync] Runtime - 2 ms (79.21%), Memory - 19.8 MB (99.98%)

--- problems/0210-course-schedule-ii/solution.py
+++ problems/0210-course-schedule-ii/solution.py
@@ -1,17 +1,18 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prs = collections.defaultdict(list)
+        prs = defaultdict(list)
         for crs, pr in prerequisites:
             prs[crs].append(pr)
         
-        visiting = set()
+
         completed = set()
-        order = []
+        visiting = set()
+        ans = []
 
         def helper(crs):
             if crs in completed:
                 return True
-            if crs in visiting: # circular prereq
+            if crs in visiting:
                 return False
             
             visiting.add(crs)
@@ -21,11 +22,13 @@ class Solution:
             visiting.remove(crs)
 
             completed.add(crs)
-            order.append(crs)
+            ans.append(crs)
+
             return True
         
         for crs in range(numCourses):
             if helper(crs) == False:
                 return []
         
-        return order
+        return ans
+

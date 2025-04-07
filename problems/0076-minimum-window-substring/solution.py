0c8dbfd1ca496677ff98c84a5e916ea0ab77e767
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2025-04-07 23:06:55 +0000

    [LeetCode Sync] Runtime - 61 ms (76.98%), Memory - 18.3 MB (100.00%)

--- problems/0076-minimum-window-substring/solution.py
+++ problems/0076-minimum-window-substring/solution.py
@@ -1,36 +1,39 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        tcount = Counter(t)
+        if len(t) > len(s):
+            return ""
+        
         scount = {}
-        minWin = float("infinity")
+        tcount = Counter(t)
+        minLen = float("infinity")
         minl, minr = 0, 0
 
         matches = 0
-        needed = len(tcount.keys())
+        needed = len(tcount)
 
         l = 0
         for r in range(len(s)):
-            print(f"{l}. {r}")
             c = s[r]
             scount[c] = scount.get(c, 0) + 1
-
-            if c in tcount and tcount[c] == scount[c]:
+            if c in tcount and scount[c] == tcount[c]:
                 matches += 1
-            # print(matches)
-            while matches >= needed:
-                # print(f"{matches}.  {needed}")
-                if r - l + 1 < minWin:
-                    minWin = r - l + 1
+            
+            while matches == needed:
+                if r - l + 1 < minLen:
+                    minLen = r - l + 1
                     minr = r
                     minl = l
 
                 c = s[l]
-                scount[c] -= 1
-                if c in tcount and tcount[c] == scount[c] + 1:
-                    matches -= 1
-                
                 l += 1
-        if minWin == float("infinity"):
+                scount[c] -= 1
+                if c in tcount and tcount[c] - 1 == scount[c]:
+                    matches -= 1
+        
+        if minLen == float("infinity"):
             return ""
-        return s[minl : minr + 1]
+        
+        return s[minl:minr + 1]
+
+
 

fbda51dca93bf2a6af1c8e3d87fc3a8f0f023903
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2026-03-29 05:38:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (70.77%)

--- problems/0290-word-pattern/solution.py
+++ problems/0290-word-pattern/solution.py
@@ -1,20 +1,16 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        patternToWord = {}
-        wordToPattern = {}
+        words = {}
         s = s.split(" ")
 
         if len(s) != len(pattern):
             return False
 
         for i, c in enumerate(pattern):
-            if c in patternToWord and patternToWord[c] != s[i]:
+            if c not in words:
+                if s[i] in words.values():
+                    return False
+                words[c] = s[i]
+            elif words[c] != s[i]:
                 return False
-            if s[i] in wordToPattern and wordToPattern[s[i]] != c:
-                return False
-            
-            patternToWord[c] = s[i]
-            wordToPattern[s[i]] = c
-        
         return True
-

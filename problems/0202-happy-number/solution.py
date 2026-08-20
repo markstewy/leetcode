8f19fc7cc7c1788475507862542050a264f17da8
Mark Stewart <markstewy@Marks-MacBook-Pro-2.local>
2026-08-20 05:01:32 +0000

    [LeetCode Sync] Runtime - 3 ms (32.90%), Memory - 19.4 MB (24.89%)

--- problems/0202-happy-number/solution.py
+++ problems/0202-happy-number/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        cacheSet = set()
-        while True:
-            if n == 1:
-                return True
-            if n in cacheSet:
-                return False
-            
-            cacheSet.add(n)
-            n = sum([int(char) ** 2 for char in str(n)])
+        x = set()
 
+        while n not in x:
+            print(n)
+            x.add(n)
+            n = sum(int(char) ** 2 for char in str(n))
+            if n == 1:
+                print(n)
+                return True
+        return False 
